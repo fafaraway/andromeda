f0f44ddff8cacfc1a5b94cf3b4e77e66449e92c8
Haleth <haleth@live.com>
2012-06-28 15:46:34 +0200

    Better button holy power colours, code cleanup

--- FreeUI/scripts/rangecolor.lua
+++ FreeUI/scripts/rangecolor.lua
@@ -11,33 +11,14 @@ local ActionHasRange = ActionHasRange
 local IsActionInRange = IsActionInRange
 local IsUsableAction = IsUsableAction
 local HasAction = HasAction
+local buttonsToUpdate = {}
 
-
---code for handling defaults
-local function removeDefaults(tbl, defaults)
-	for k, v in pairs(defaults) do
-		if type(tbl[k]) == 'table' and type(v) == 'table' then
-			removeDefaults(tbl[k], v)
-			if next(tbl[k]) == nil then
-				tbl[k] = nil
-			end
-		elseif tbl[k] == v then
-			tbl[k] = nil
-		end
-	end
-	return tbl
-end
-
-local function copyDefaults(tbl, defaults)
-	for k, v in pairs(defaults) do
-		if type(v) == 'table' then
-			tbl[k] = copyDefaults(tbl[k] or {}, v)
-		elseif tbl[k] == nil then
-			tbl[k] = v
-		end
-	end
-	return tbl
-end
+local colors = {
+	normal = {1, 1, 1},
+	oor = {1, 0.3, 0.1},
+	oom = {0.1, 0.3, 1},
+	ooh = {0.45, 0.45, 1}
+}
 
 local function timer_Create(parent, interval)
 	local updater = parent:CreateAnimationGroup()
@@ -103,53 +84,9 @@ do
 	end
 end
 
-
 --[[ The main thing ]]--
 
 local Range = timer_Create(CreateFrame('Frame', 'Range'), UPDATE_DELAY)
-
-function Range:Load()
-	self:SetScript('OnEvent', self.OnEvent)
-	self:RegisterEvent('PLAYER_LOGIN')
-	self:RegisterEvent('PLAYER_LOGOUT')
-end
-
-
---[[ Frame Events ]]--
-
-function Range:OnEvent(event, ...)
-	local action = self[event]
-	if action then
-		action(self, event, ...)
-	end
-end
-
---[[ Game Events ]]--
-
-function Range:PLAYER_LOGIN()
-	if not Range_COLORS then
-		Range_COLORS = {}
-	end
-	self.sets = copyDefaults(Range_COLORS, self:GetDefaults())
-
-	--add options loader
-	local f = CreateFrame('Frame', nil, InterfaceOptionsFrame)
-	f:SetScript('OnShow', function(self)
-		self:SetScript('OnShow', nil)
-		LoadAddOn('Range_Config')
-	end)
-
-	self.buttonsToUpdate = {}
-
-	hooksecurefunc('ActionButton_OnUpdate', self.RegisterButton)
-	hooksecurefunc('ActionButton_UpdateUsable', self.OnUpdateButtonUsable)
-	hooksecurefunc('ActionButton_Update', self.OnButtonUpdate)
-end
-
-function Range:PLAYER_LOGOUT()
-	removeDefaults(Range_COLORS, self:GetDefaults())
-end
-
 
 --[[ Actions ]]--
 
@@ -157,14 +94,8 @@ function Range:Update()
 	return self:UpdateButtons(UPDATE_DELAY)
 end
 
-function Range:ForceColorUpdate()
-	for button in pairs(self.buttonsToUpdate) do
-		Range.OnUpdateButtonUsable(button)
-	end
-end
-
 function Range:UpdateActive()
-	if next(self.buttonsToUpdate) then
+	if next(buttonsToUpdate) then
 		if not self:Active() then
 			self:Start()
 		end
@@ -174,8 +105,8 @@ function Range:UpdateActive()
 end
 
 function Range:UpdateButtons(elapsed)
-	if next(self.buttonsToUpdate) then
-		for button in pairs(self.buttonsToUpdate) do
+	if next(buttonsToUpdate) then
+		for button in pairs(buttonsToUpdate) do
 			self:UpdateButton(button, elapsed)
 		end
 		return true
@@ -191,14 +122,12 @@ end
 function Range:UpdateButtonStatus(button)
 	local action = ActionButton_GetPagedID(button)
 	if button:IsVisible() and action and HasAction(action) and ActionHasRange(action) then
-		self.buttonsToUpdate[button] = true
+		buttonsToUpdate[button] = true
 	else
-		self.buttonsToUpdate[button] = nil
+		buttonsToUpdate[button] = nil
 	end
 	self:UpdateActive()
 end
-
-
 
 --[[ Button Hooking ]]--
 
@@ -227,6 +156,9 @@ function Range.OnButtonUpdate(button)
 	 Range:UpdateButtonStatus(button)
 end
 
+hooksecurefunc('ActionButton_OnUpdate', Range.RegisterButton)
+hooksecurefunc('ActionButton_UpdateUsable', Range.OnUpdateButtonUsable)
+hooksecurefunc('ActionButton_Update', Range.OnButtonUpdate)
 
 --[[ Range Coloring ]]--
 
@@ -240,7 +172,7 @@ function Range.UpdateButtonUsable(button)
 		if IsActionInRange(action) == 0 then
 			Range.SetButtonColor(button, 'oor')
 		--a holy power abilty, and we're less than 3 Holy Power
-		elseif PLAYER_IS_PALADIN and isHolyPowerAbility(action) and not(UnitPower('player', SPELL_POWER_HOLY_POWER) >= Range:GetHolyPowerThreshold() or UnitBuff('player', HAND_OF_LIGHT)) then
+		elseif PLAYER_IS_PALADIN and isHolyPowerAbility(action) and not(UnitPower('player', SPELL_POWER_HOLY_POWER) >= 3 or UnitBuff('player', HAND_OF_LIGHT)) then
 			Range.SetButtonColor(button, 'ooh')
 		--in range
 		else
@@ -249,7 +181,7 @@ function Range.UpdateButtonUsable(button)
 	--out of mana
 	elseif notEnoughMana then
 		--a holy power abilty, and we're less than 3 Holy Power
-		if PLAYER_IS_PALADIN and isHolyPowerAbility(action) and not(UnitPower('player', SPELL_POWER_HOLY_POWER) >= Range:GetHolyPowerThreshold() or UnitBuff('player', HAND_OF_LIGHT)) then
+		if PLAYER_IS_PALADIN and not(UnitPower('player', SPELL_POWER_HOLY_POWER) >= 3 or UnitBuff('player', HAND_OF_LIGHT)) then
 			Range.SetButtonColor(button, 'ooh')
 		else
 			Range.SetButtonColor(button, 'oom')
@@ -264,7 +196,7 @@ function Range.SetButtonColor(button, colorType)
 	if button.RangeColor ~= colorType then
 		button.RangeColor = colorType
 
-		local r, g, b = Range:GetColor(colorType)
+		local r, g, b = colors[colorType][1], colors[colorType][2], colors[colorType][3]
 
 		local icon =  _G[button:GetName() .. 'Icon']
 		icon:SetVertexColor(r, g, b)
@@ -293,49 +225,3 @@ function Range.UpdateFlash(button, elapsed)
 		button.flashtime = flashtime
 	end
 end
-
-
---[[ Configuration ]]--
-
-function Range:GetDefaults()
-	return {
-		normal = {1, 1, 1},
-		oor = {1, 0.3, 0.1},
-		oom = {0.1, 0.3, 1},
-		ooh = {0.45, 0.45, 1},
-		holyPowerThreshold = 3
-	}
-end
-
-function Range:Reset()
-	Range_COLORS = {}
-	self.sets = copyDefaults(Range_COLORS, self:GetDefaults())
-
-	self:ForceColorUpdate()
-end
-
-function Range:SetColor(index, r, g, b)
-	local color = self.sets[index]
-	color[1] = r
-	color[2] = g
-	color[3] = b
-
-	self:ForceColorUpdate()
-end
-
-function Range:GetColor(index)
-	local color = self.sets[index]
-	return color[1], color[2], color[3]
-end
-
-function Range:SetHolyPowerThreshold(value)
-	self.sets.holyPowerThreshold = value
-end
-
-function Range:GetHolyPowerThreshold()
-	return self.sets.holyPowerThreshold
-end
-
---[[ Load The Thing ]]--
-
-Range:Load()
