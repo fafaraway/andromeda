78c2573b23a74e711414068a24c48f6d46cf6b5b
Haleth <haleth@live.com>
2012-07-07 16:21:13 +0200

    Fix an important bug.

--- FreeUI/scripts/theme.lua
+++ FreeUI/scripts/theme.lua
@@ -1989,7 +1989,7 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 			local num = #INTERFACEOPTIONS_ADDONCATEGORIES
 			for i = 1, num do
 				local bu = _G["InterfaceOptionsFrameAddOnsButton"..i.."Toggle"]
-				if not bu.reskinned then
+				if bu and not bu.reskinned then
 					F.ReskinExpandOrCollapse(bu)
 					bu:SetPushedTexture("")
 					bu.SetPushedTexture = F.dummy
