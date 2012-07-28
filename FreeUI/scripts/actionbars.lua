e88f4734b8e3f7330a90da8cf518ae93261a5531
Haleth <haleth@live.com>
2012-07-28 12:42:08 +0200

    Blizzard fixed this typo.

--- FreeUI/scripts/actionbars.lua
+++ FreeUI/scripts/actionbars.lua
@@ -181,8 +181,7 @@ MainMenuBarTexture3:SetTexture("")
 MainMenuBarLeftEndCap:SetTexture("")
 MainMenuBarRightEndCap:SetTexture("")
 
-
-local textureList = {"_BG","EndCapL","EndCapR","_Boader","Divider1","Divider2","Divider3","ExitBG","MicroBGL","MicroBGR","_MicroBGMid","ButtonBGL","ButtonBGR","_ButtonBGMid"}
+local textureList = {"_BG","EndCapL","EndCapR","_Border","Divider1","Divider2","Divider3","ExitBG","MicroBGL","MicroBGR","_MicroBGMid","ButtonBGL","ButtonBGR","_ButtonBGMid"}
 
 for _, tex in pairs(textureList) do
 	OverrideActionBar[tex]:SetAlpha(0)
