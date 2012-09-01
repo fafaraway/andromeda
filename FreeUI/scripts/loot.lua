944aebb3d667cb714825ee6ea7481a49f15b94b0
Haleth <haleth@live.com>
2012-09-01 21:02:25 +0200

    Add our own class colour to ML dropdown.

--- FreeUI/scripts/loot.lua
+++ FreeUI/scripts/loot.lua
@@ -249,3 +249,21 @@ addon:Hide()
 
 LootFrame:UnregisterAllEvents()
 table.insert(UISpecialFrames, "Butsu")
+
+LootHistoryDropDown.initialize = function(self)
+	local info = UIDropDownMenu_CreateInfo();
+	info.isTitle = 1;
+	info.text = MASTER_LOOTER;
+	info.fontObject = GameFontNormalLeft;
+	info.notCheckable = 1;
+	UIDropDownMenu_AddButton(info);
+
+	info = UIDropDownMenu_CreateInfo();
+	info.notCheckable = 1;
+	local name, class = C_LootHistory.GetPlayerInfo(self.itemIdx, self.playerIdx);
+	local classColor = C.classcolours[class];
+	local colorCode = string.format("|cFF%02x%02x%02x",  classColor.r*255,  classColor.g*255,  classColor.b*255);
+	info.text = string.format(MASTER_LOOTER_GIVE_TO, colorCode..name.."|r");
+	info.func = LootHistoryDropDown_OnClick;
+	UIDropDownMenu_AddButton(info);
+end
