437a0bc2ead4a94bff8324e6b9d7da6d8b9582e0
Haleth <haleth@live.com>
2012-09-06 10:45:07 +0200

    Colour updates.

--- FreeUI/scripts/colours.lua
+++ FreeUI/scripts/colours.lua
@@ -249,9 +249,9 @@ hooksecurefunc('GuildFrame_LoadUI', function()
 end)
 
 hooksecurefunc('LFRBrowseFrameListButton_SetData', function(button, index)
-	local name, level, areaName, className, comment, partyMembers, status, class, encountersTotal, encountersComplete, isLeader, isTank, isHealer, isDamage = SearchLFGGetResults(index)
+	local name, level, areaName, className, comment, partyMembers, status, class, encountersTotal, encountersComplete, isIneligible, isLeader, isTank, isHealer, isDamage = SearchLFGGetResults(index)
 
-	if(index and class and name and level and (name~=myName)) then
+	if class and name and level and (name~=myName) then
 		button.name:SetText(ns.classColor[class] .. name)
 		button.class:SetText(ns.classColor[class] .. className)
 		button.level:SetText(ns.diffColor[level] .. level)
