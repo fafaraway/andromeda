efa20f5cb18528e8f873cf0a8c161cd4e58dda9a
Haleth <haleth@live.com>
2012-09-22 03:42:22 +0200

    Restyle inspect talent frame.

--- FreeUI/scripts/theme.lua
+++ FreeUI/scripts/theme.lua
@@ -5003,6 +5003,8 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 		InspectPVPTeam2:DisableDrawLayer("BACKGROUND")
 		InspectPVPTeam3:DisableDrawLayer("BACKGROUND")
 
+		InspectTalentFrame:GetRegions():Hide()
+		select(2, InspectTalentFrame:GetRegions()):Hide()
 		InspectGuildFrameBG:Hide()
 		for i = 1, 5 do
 			select(i, InspectModelFrame:GetRegions()):Hide()
@@ -5010,14 +5012,6 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 		InspectPVPFrameBG:SetAlpha(0)
 		InspectPVPFrameBottom:SetAlpha(0)
 		select(9, InspectMainHandSlot:GetRegions()):Hide()
-
-		for i = 1, 4 do
-			local tab = _G["InspectFrameTab"..i]
-			F.CreateTab(tab)
-			if i ~= 1 then
-				tab:SetPoint("LEFT", _G["InspectFrameTab"..i-1], "RIGHT", -15, 0)
-			end
-		end
 
 		local slots = {
 			"Head", "Neck", "Shoulder", "Shirt", "Chest", "Waist", "Legs", "Feet", "Wrist",
@@ -5032,6 +5026,86 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 			slot:SetNormalTexture("")
 			slot:SetPushedTexture("")
 			_G["Inspect"..slots[i].."SlotIconTexture"]:SetTexCoord(.08, .92, .08, .92)
+		end
+
+		InspectTalentFrame.InspectSpec.ring:Hide()
+
+		for i = 1, 6 do
+			local row = InspectTalentFrame.InspectTalents["tier"..i]
+			for j = 1, 3 do
+				local bu = row["talent"..j]
+
+				bu.Slot:Hide()
+				bu.border:SetTexture("")
+
+				bu.icon:SetDrawLayer("ARTWORK")
+				bu.icon:SetTexCoord(.08, .92, .08, .92)
+
+				F.CreateBG(bu.icon)
+			end
+		end
+
+		InspectTalentFrame.InspectSpec.specIcon:SetTexCoord(.08, .92, .08, .92)
+		F.CreateBG(InspectTalentFrame.InspectSpec.specIcon)
+
+		local function updateIcon(self)
+			local spec = nil
+			if INSPECTED_UNIT ~= nil then
+				spec = GetInspectSpecialization(INSPECTED_UNIT)
+			end
+			if spec ~= nil and spec > 0 then
+				local role1 = GetSpecializationRoleByID(spec)
+				if role1 ~= nil then
+					local _, _, _, icon = GetSpecializationInfoByID(spec)
+					self.specIcon:SetTexture(icon)
+				end
+			end
+		end
+
+		InspectTalentFrame.InspectSpec:HookScript("OnShow", updateIcon)
+		InspectTalentFrame:HookScript("OnEvent", function(self, event, unit)
+			if not InspectFrame:IsShown() then return end
+			if event == "INSPECT_READY" and InspectFrame.unit and UnitGUID(InspectFrame.unit) == unit then
+				updateIcon(self.InspectSpec)
+			end
+		end)
+
+		local function updateGlyph(self, clear)
+			local id = self:GetID()
+			local talentGroup = PlayerTalentFrame and PlayerTalentFrame.talentGroup
+			local enabled, glyphType, glyphTooltipIndex, glyphSpell, iconFilename = GetGlyphSocketInfo(id, talentGroup, true, INSPECTED_UNIT);
+
+			if not glyphType then return end
+
+			if enabled and glyphSpell and not clear then
+				if iconFilename then
+					self.glyph:SetTexture(iconFilename)
+				else
+					self.glyph:SetTexture("Interface\\Spellbook\\UI-Glyph-Rune1")
+				end
+			end
+		end
+
+		hooksecurefunc("InspectGlyphFrameGlyph_UpdateSlot", updateGlyph)
+
+		for i = 1, 6 do
+			local glyph = InspectTalentFrame.InspectGlyphs["Glyph"..i]
+
+			glyph:HookScript("OnShow", updateGlyph)
+
+			glyph.ring:Hide()
+
+			glyph.glyph:SetDrawLayer("ARTWORK")
+			glyph.glyph:SetTexCoord(.08, .92, .08, .92)
+			F.CreateBDFrame(glyph.glyph)
+		end
+
+		for i = 1, 4 do
+			local tab = _G["InspectFrameTab"..i]
+			F.CreateTab(tab)
+			if i ~= 1 then
+				tab:SetPoint("LEFT", _G["InspectFrameTab"..i-1], "RIGHT", -15, 0)
+			end
 		end
 
 		F.ReskinPortraitFrame(InspectFrame, true)
