ce6afc66903230f0eacbfd3d1c35311aa5522ec8
Haleth <haleth@live.com>
2012-11-22 11:39:36 +0100

    Fix oudated info in profiles file.

--- FreeUI/profiles.lua
+++ FreeUI/profiles.lua
@@ -1,7 +1,7 @@
 local F, C, L = unpack(select(2, ...))
 
 --[[
-	This file allows you to override any option in options.lua, or append to it, for example to the sFilter table.
+	This file allows you to override any option in options.lua, or append to it, for example to the buffTracker table.
 	Since this file is mostly blank, it will most likely updated.
 	You can therefore simply copy and paste it every time you update the UI, and keep your settings, unless mentioned otherwise.
 
@@ -9,13 +9,13 @@ local F, C, L = unpack(select(2, ...))
 	C.general.autorepair_guild = true
 
 	To override an option in a table which uses indexes (=the table is simply a summary of values), use this:
-	C.sfilter.PALADIN[1] = {spellId = 59578, unitId = "player", isMine = 1, filter = "HELPFUL", slot = 3}
+	C.buffTracker.PALADIN[1] = {spellId = 59578, unitId = "player", isMine = 1, filter = "HELPFUL", slot = 3}
 
 	To append an option to an existing table, use this format:
-	tinsert(C.sfilter.PALADIN, {spellId = 114250, unitId = "player", isMine = 1, filter = "HELPFUL", slot = 3})
+	tinsert(C.buffTracker.PALADIN, {spellId = 114250, unitId = "player", isMine = 1, filter = "HELPFUL", slot = 3})
 
 	To remove an element from an existing table (in this case the first):
-	tremove(C.sfilter.PALADIN, 1)
+	tremove(C.buffTracker.PALADIN, 1)
 ]]
 
 local name = C.myName
