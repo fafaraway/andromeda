e6c2abfe9cccaf0eb8f461fa2244ecb4c01f6b15
Haleth <haleth@live.com>
2012-12-29 00:47:59 +0100

    Don't vendor grey items part of equipment sets.

--- FreeUI/scripts/automation.lua
+++ FreeUI/scripts/automation.lua
@@ -52,7 +52,7 @@ f:SetScript("OnEvent", function(self, event)
 		for bag = 0, 4 do
 			for slot = 0, GetContainerNumSlots(bag) do
 				local link = GetContainerItemLink(bag, slot)
-				if link and (select(3, GetItemInfo(link))==0) then
+				if link and select(3, GetItemInfo(link)) == 0 and not GetContainerItemEquipmentSetInfo(bag, slot) then
 					UseContainerItem(bag, slot)
 				end
 			end
