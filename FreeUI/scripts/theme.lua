9269fc9c2a01d8878c5a77e8f92f6c6857f84fbd
Haleth <haleth@live.com>
2013-02-18 20:33:51 +0100

    Fix a memory leak in the equipment flyout.

--- FreeUI/scripts/theme.lua
+++ FreeUI/scripts/theme.lua
@@ -1424,7 +1424,7 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 
 				button.icon:SetTexCoord(.08, .92, .08, .92)
 
-				button.reskinned = true
+				button.styled = true
 			end
 
 			local location = button.location
