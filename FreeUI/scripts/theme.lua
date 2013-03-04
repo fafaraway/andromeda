96556c4874a758b5ed4cefd2ee3fb2c02f8c727e
Haleth <haleth@live.com>
2013-03-04 17:44:58 +0100

    Restyle most of the PVP UI.

--- FreeUI/scripts/theme.lua
+++ FreeUI/scripts/theme.lua
@@ -604,14 +604,6 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 		GroupFinderFrameGroupButton2.icon:SetTexture("Interface\\Icons\\inv_helmet_06")
 		GroupFinderFrameGroupButton3.icon:SetTexture("Interface\\Icons\\Icon_Scenarios")
 
-		local function onMouseDown(self)
-			self.icon:SetSize(64, 64)
-		end
-
-		local function onMouseUp(self)
-			self.icon:SetSize(66, 66)
-		end
-
 		for i = 1, 3 do
 			local bu = GroupFinderFrame["groupButton"..i]
 
@@ -627,9 +619,6 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 			bu.icon:SetDrawLayer("OVERLAY")
 			bu.icon.bg = F.CreateBG(bu.icon)
 			bu.icon.bg:SetDrawLayer("ARTWORK")
-
-			bu:HookScript("OnMouseDown", onMouseDown)
-			bu:HookScript("OnMouseUp", onMouseUp)
 		end
 
 		hooksecurefunc("GroupFinderFrame_SelectGroupButton", function(index)
@@ -6046,6 +6035,12 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 		PetJournalListScrollFrame:HookScript("OnVerticalScroll", ColourPetQuality)
 		PetJournalListScrollFrame:HookScript("OnMouseWheel", ColourPetQuality)
 	elseif addon == "Blizzard_PVPUI" then
+		local PVPUIFrame = PVPUIFrame
+		local PVPQueueFrame = PVPQueueFrame
+		local HonorFrame = HonorFrame
+		local ConquestFrame = ConquestFrame
+		local WarGamesFrame = WarGamesFrame
+
 		PVPUIFrame:DisableDrawLayer("ARTWORK")
 		PVPUIFrame.LeftInset:DisableDrawLayer("BORDER")
 		PVPUIFrame.Background:Hide()
@@ -6056,12 +6051,251 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 
 		PVPUIFrameTab2:SetPoint("LEFT", PVPUIFrameTab1, "RIGHT", -15, 0)
 
+		-- Category buttons
+
+		for i = 1, 3 do
+			local bu = PVPQueueFrame["CategoryButton"..i]
+			local icon = bu.Icon
+			local cu = bu.CurrencyIcon
+
+			bu.Background:Hide()
+			bu.Ring:Hide()
+
+			bu.bg = F.CreateBG(bu)
+			bu.bg:SetAllPoints()
+			bu.bg:SetVertexColor(r, g, b, .2)
+
+			F.Reskin(bu, true)
+
+			icon:SetTexCoord(.08, .92, .08, .92)
+			icon:SetPoint("LEFT", bu, "LEFT")
+			icon:SetDrawLayer("OVERLAY")
+			icon.bg = F.CreateBG(icon)
+			icon.bg:SetDrawLayer("ARTWORK")
+
+			if cu then
+				cu:SetSize(16, 16)
+				cu:SetPoint("TOPLEFT", bu.Name, "BOTTOMLEFT", 0, -8)
+				bu.CurrencyAmount:SetPoint("LEFT", cu, "RIGHT", 4, 0)
+
+				cu:SetTexCoord(.08, .92, .08, .92)
+				cu.bg = F.CreateBG(cu)
+				cu.bg:SetDrawLayer("BACKGROUND", 1)
+			end
+		end
+
+		PVPQueueFrame.CategoryButton1.Icon:SetTexture("Interface\\Icons\\achievement_bg_winwsg")
+		PVPQueueFrame.CategoryButton2.Icon:SetTexture("Interface\\Icons\\achievement_bg_killxenemies_generalsroom")
+		PVPQueueFrame.CategoryButton3.Icon:SetTexture("Interface\\Icons\\ability_warrior_offensivestance")
+
+		local englishFaction = UnitFactionGroup("player")
+		PVPQueueFrame.CategoryButton1.CurrencyIcon:SetTexture("Interface\\Icons\\PVPCurrency-Honor-"..englishFaction)
+		PVPQueueFrame.CategoryButton2.CurrencyIcon:SetTexture("Interface\\Icons\\PVPCurrency-Conquest-"..englishFaction)
+
+		hooksecurefunc("PVPQueueFrame_SelectButton", function(index)
+			local self = PVPQueueFrame
+			for i = 1, 3 do
+				local bu = self["CategoryButton"..i]
+				if i == index then
+					bu.bg:Show()
+				else
+					bu.bg:Hide()
+				end
+			end
+		end)
+
+		-- Honor frame
+
+		local Inset = HonorFrame.Inset
+		local BonusFrame = HonorFrame.BonusFrame
+
+		for i = 1, 9 do
+			select(i, Inset:GetRegions()):Hide()
+		end
+		BonusFrame.BattlegroundTexture:Hide()
+		BonusFrame.WorldBattlesTexture:Hide()
+		BonusFrame.BattlegroundHeader:Hide()
+		BonusFrame.WorldPVPHeader:Hide()
+		BonusFrame.ShadowOverlay:Hide()
+
+		--BonusFrame.DiceButton:SetNormalTexture("")
+		--BonusFrame.DiceButton:SetPushedTexture("")
+		F.Reskin(BonusFrame.DiceButton)
+
+		for _, bu in pairs({BonusFrame.RandomBGButton, BonusFrame.CallToArmsButton, BonusFrame.WorldPVP1Button, BonusFrame.WorldPVP2Button}) do
+			F.Reskin(bu, true)
+
+			bu.SelectedTexture:SetDrawLayer("BACKGROUND")
+			bu.SelectedTexture:SetTexture(r, g, b, .2)
+			bu.SelectedTexture:SetAllPoints()
+		end
+
+		BonusFrame.CallToArmsButton:SetPoint("TOP", BonusFrame.RandomBGButton, "BOTTOM", 0, -1)
+		BonusFrame.WorldPVP2Button:SetPoint("TOP", BonusFrame.WorldPVP1Button, "BOTTOM", 0, -1)
+
+		-- Honor frame specific
+
+		for _, bu in pairs(HonorFrame.SpecificFrame.buttons) do
+			bu.Bg:Hide()
+			bu.Border:Hide()
+
+			bu:SetNormalTexture("")
+			bu:SetHighlightTexture("")
+
+			local bg = CreateFrame("Frame", nil, bu)
+			bg:SetPoint("TOPLEFT", 2, 0)
+			bg:SetPoint("BOTTOMRIGHT", 0, 2)
+			F.CreateBD(bg, 0)
+			bg:SetFrameLevel(bu:GetFrameLevel()-1)
+
+			bu.tex = F.CreateGradient(bu)
+			bu.tex:SetDrawLayer("BACKGROUND")
+			bu.tex:SetPoint("TOPLEFT", 3, -1)
+			bu.tex:SetPoint("BOTTOMRIGHT", -1, 3)
+
+			bu.SelectedTexture:SetDrawLayer("BACKGROUND")
+			bu.SelectedTexture:SetTexture(r, g, b, .2)
+			bu.SelectedTexture:SetPoint("TOPLEFT", 2, 0)
+			bu.SelectedTexture:SetPoint("BOTTOMRIGHT", 0, 2)
+
+			bu.Icon:SetTexCoord(.08, .92, .08, .92)
+			bu.Icon.bg = F.CreateBG(bu.Icon)
+			bu.Icon.bg:SetDrawLayer("BACKGROUND", 1)
+			bu.Icon:SetPoint("TOPLEFT", 5, -3)
+		end
+
+		-- if scroll frames aren't bugged then they are terribly implemented
+		local bu1 = HonorFrame.SpecificFrame.buttons[1]
+		bu1.tex:SetPoint("TOPLEFT", 3, 0)
+		bu1.tex:SetPoint("BOTTOMRIGHT", -1, 3)
+		bu1.Icon:SetPoint("TOPLEFT", 4, -2)
+		bu1.SelectedTexture:SetPoint("BOTTOMRIGHT", 0, 3)
+
+		-- Conquest Frame
+
+		local Inset = ConquestFrame.Inset
+
+		for i = 1, 9 do
+			select(i, Inset:GetRegions()):Hide()
+		end
+		ConquestFrame.ArenaTexture:Hide()
+		ConquestFrame.RatedBGTexture:Hide()
+		ConquestFrame.ArenaHeader:Hide()
+		ConquestFrame.RatedBGHeader:Hide()
+		ConquestFrame.ShadowOverlay:Hide()
+
+		for _, bu in pairs({ConquestFrame.Arena2v2, ConquestFrame.Arena3v3, ConquestFrame.Arena5v5, ConquestFrame.RatedBG}) do
+			F.Reskin(bu, true)
+
+			bu.SelectedTexture:SetDrawLayer("BACKGROUND")
+			bu.SelectedTexture:SetTexture(r, g, b, .2)
+			bu.SelectedTexture:SetAllPoints()
+		end
+
+		ConquestFrame.Arena3v3:SetPoint("TOP", ConquestFrame.Arena2v2, "BOTTOM", 0, -1)
+		ConquestFrame.Arena5v5:SetPoint("TOP", ConquestFrame.Arena3v3, "BOTTOM", 0, -1)
+
+		ConquestFrame.RatedBG.TeamNameText:SetText(UnitName("player"))
+		ConquestFrame.RatedBG.TeamNameText:SetTextColor(r, g, b)
+
+		-- War games
+
+		local Inset = WarGamesFrame.RightInset
+
+		for i = 1, 9 do
+			select(i, Inset:GetRegions()):Hide()
+		end
+		WarGamesFrame.InfoBG:Hide()
+		WarGamesFrame.HorizontalBar:Hide()
+		WarGamesFrameInfoScrollFrame.scrollBarBackground:Hide()
+		WarGamesFrameInfoScrollFrame.scrollBarArtTop:Hide()
+		WarGamesFrameInfoScrollFrame.scrollBarArtBottom:Hide()
+
+		WarGamesFrameDescription:SetTextColor(.9, .9, .9)
+
+		local function onSetNormalTexture(self, texture)
+			if texture:find("Plus") then
+				self.plus:Show()
+			else
+				self.plus:Hide()
+			end
+		end
+
+		for _, button in pairs(WarGamesFrame.scrollFrame.buttons) do
+			local bu = button.Entry
+			local SelectedTexture = bu.SelectedTexture
+
+			bu.Bg:Hide()
+			bu.Border:Hide()
+
+			bu:SetNormalTexture("")
+			bu:SetHighlightTexture("")
+
+			local bg = CreateFrame("Frame", nil, bu)
+			bg:SetPoint("TOPLEFT", 2, 0)
+			bg:SetPoint("BOTTOMRIGHT", -1, 2)
+			F.CreateBD(bg, 0)
+			bg:SetFrameLevel(bu:GetFrameLevel()-1)
+
+			local tex = F.CreateGradient(bu)
+			tex:SetDrawLayer("BACKGROUND")
+			tex:SetPoint("TOPLEFT", 3, -1)
+			tex:SetPoint("BOTTOMRIGHT", -2, 3)
+
+			SelectedTexture:SetDrawLayer("BACKGROUND")
+			SelectedTexture:SetTexture(r, g, b, .2)
+			SelectedTexture:SetPoint("TOPLEFT", 2, 0)
+			SelectedTexture:SetPoint("BOTTOMRIGHT", -1, 2)
+
+			bu.Icon:SetTexCoord(.08, .92, .08, .92)
+			bu.Icon.bg = F.CreateBG(bu.Icon)
+			bu.Icon.bg:SetDrawLayer("BACKGROUND", 1)
+			bu.Icon:SetPoint("TOPLEFT", 5, -3)
+
+			local header = button.Header
+
+			header:GetNormalTexture():SetAlpha(0)
+			header:SetHighlightTexture("")
+			header:SetPushedTexture("")
+
+			local headerBg = CreateFrame("Frame", nil, header)
+			headerBg:SetSize(13, 13)
+			headerBg:SetPoint("LEFT", 4, 0)
+			headerBg:SetFrameLevel(header:GetFrameLevel()-1)
+			F.CreateBD(headerBg, 0)
+
+			local headerTex = F.CreateGradient(header)
+			headerTex:SetAllPoints(headerBg)
+
+			local minus = header:CreateTexture(nil, "OVERLAY")
+			minus:SetSize(7, 1)
+			minus:SetPoint("CENTER", headerBg)
+			minus:SetTexture(C.media.backdrop)
+			minus:SetVertexColor(1, 1, 1)
+
+			local plus = header:CreateTexture(nil, "OVERLAY")
+			plus:SetSize(1, 7)
+			plus:SetPoint("CENTER", headerBg)
+			plus:SetTexture(C.media.backdrop)
+			plus:SetVertexColor(1, 1, 1)
+			header.plus = plus
+
+			hooksecurefunc(header, "SetNormalTexture", onSetNormalTexture)
+		end
+
+		-- Main style
+
 		F.ReskinPortraitFrame(PVPUIFrame)
 		F.ReskinTab(PVPUIFrame.Tab1)
 		F.ReskinTab(PVPUIFrame.Tab2)
 		F.Reskin(HonorFrame.SoloQueueButton)
 		F.Reskin(HonorFrame.GroupQueueButton)
+		F.Reskin(ConquestFrame.JoinButton)
+		F.Reskin(WarGameStartButton)
 		F.ReskinDropDown(HonorFrameTypeDropDown)
+		F.ReskinScroll(HonorFrameSpecificFrameScrollBar)
+		F.ReskinScroll(WarGamesFrameScrollFrameScrollBar)
+		F.ReskinScroll(WarGamesFrameInfoScrollFrameScrollBar)
 	elseif addon == "Blizzard_QuestChoice" then
 		for i = 1, 18 do
 			select(i, QuestChoiceFrame:GetRegions()):Hide()
