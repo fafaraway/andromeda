eecf5c017ee05d1f13307e4fceb5feda69d11d6d
Haleth <haleth@live.com>
2013-03-10 02:23:31 +0100

    Slightly more efficient DBM code.

--- FreeUI/scripts/dbm.lua
+++ FreeUI/scripts/dbm.lua
@@ -34,23 +34,50 @@ local function InitStyle()
 		end
 	end)
 
+	local firstInfo = true
+	hooksecurefunc(DBM.InfoFrame, "Show", function()
+		if firstInfo then
+			DBMInfoFrame:SetBackdrop(nil)
+			local bd = CreateFrame("Frame", nil, DBMInfoFrame)
+			bd:SetPoint("TOPLEFT")
+			bd:SetPoint("BOTTOMRIGHT")
+			bd:SetFrameLevel(DBMInfoFrame:GetFrameLevel()-1)
+			F.CreateBD(bd)
+
+			firstInfo = false
+		end
+	end)
+
+	local firstRange = true
+	hooksecurefunc(DBM.RangeCheck, "Show", function()
+		if firstRange then
+			DBMRangeCheck:SetBackdrop(nil)
+			local bd = CreateFrame("Frame", nil, DBMRangeCheck)
+			bd:SetPoint("TOPLEFT")
+			bd:SetPoint("BOTTOMRIGHT")
+			bd:SetFrameLevel(DBMRangeCheck:GetFrameLevel()-1)
+			F.CreateBD(bd)
+
+			firstRange = false
+		end
+	end)
+
 	hooksecurefunc(DBM.BossHealth, "Show", function()
 		local anchor = DBMBossHealthDropdown:GetParent()
 		if not anchor.styled then
 			local header = anchor:GetRegions()
-			if header:IsObjectType("FontString") then
-				header:SetFont(C.media.font, 8, "OUTLINEMONOCHROME")
-				header:SetTextColor(1, 1, 1)
-				header:SetShadowOffset(0, 0)
-				anchor.styled = true
-			end
+			header:SetFont(C.media.font, 8, "OUTLINEMONOCHROME")
+			header:SetTextColor(1, 1, 1)
+			header:SetShadowOffset(0, 0)
+
+			anchor.styled = true
 		end
 	end)
 
 	local count = 1
 
 	local styleBar = function()
-		local bar = _G[format("DBM_BossHealth_Bar_%d", count)]
+		local bar = _G["DBM_BossHealth_Bar_"..count]
 
 		while bar do
 			if not bar.styled then
@@ -58,8 +85,6 @@ local function InitStyle()
 				local sb = _G[name.."Bar"]
 				local text = _G[name.."BarName"]
 				local timer = _G[name.."BarTimer"]
-
-				local prev = _G[format("DBM_BossHealth_Bar_%d", count-1)]
 
 				_G[name.."BarBackground"]:Hide()
 				_G[name.."BarBorder"]:SetNormalTexture("")
@@ -77,88 +102,12 @@ local function InitStyle()
 			end
 
 			count = count + 1
-			bar = _G[format("DBM_BossHealth_Bar_%d", count)]
+			bar = _G["DBM_BossHealth_Bar_"..count]
 		end
 	end
 
 	hooksecurefunc(DBM.BossHealth, "AddBoss", styleBar)
 	hooksecurefunc(DBM.BossHealth, "UpdateSettings", styleBar)
-
-	local load = CreateFrame("Frame")
-	load:RegisterEvent("ADDON_LOADED")
-	load:SetScript("OnEvent", function(self, _, addon)
-		if addon ~= "DBM-GUI" then return end
-		self:UnregisterEvent("ADDON_LOADED")
-
-		DBM_GUI_OptionsFrameHeader:SetTexture(nil)
-		DBM_GUI_OptionsFramePanelContainer:SetBackdrop(nil)
-		DBM_GUI_OptionsFrameBossMods:DisableDrawLayer("BACKGROUND")
-		DBM_GUI_OptionsFrameDBMOptions:DisableDrawLayer("BACKGROUND")
-
-		for i = 1, 2 do
-			_G["DBM_GUI_OptionsFrameTab"..i.."Left"]:SetAlpha(0)
-			_G["DBM_GUI_OptionsFrameTab"..i.."Middle"]:SetAlpha(0)
-			_G["DBM_GUI_OptionsFrameTab"..i.."Right"]:SetAlpha(0)
-			_G["DBM_GUI_OptionsFrameTab"..i.."LeftDisabled"]:SetAlpha(0)
-			_G["DBM_GUI_OptionsFrameTab"..i.."MiddleDisabled"]:SetAlpha(0)
-			_G["DBM_GUI_OptionsFrameTab"..i.."RightDisabled"]:SetAlpha(0)
-		end
-
-		local count = 1
-
-		local function styleDBM()
-			local option = _G["DBM_GUI_Option_"..count]
-			while option do
-				local objType = option:GetObjectType()
-				if objType == "CheckButton" then
-					F.ReskinCheck(option)
-				elseif objType == "Slider" then
-					F.ReskinSlider(option)
-				elseif objType == "EditBox" then
-					F.ReskinInput(option)
-				elseif option:GetName():find("DropDown") then
-					F.ReskinDropDown(option)
-				elseif objType == "Button" then
-					F.Reskin(option)
-				elseif objType == "Frame" then
-					option:SetBackdrop(nil)
-				end
-
-				count = count + 1
-				option = _G["DBM_GUI_Option_"..count]
-				if not option then
-					option = _G["DBM_GUI_DropDown"..count]
-				end
-			end
-		end
-
-		DBM:RegisterOnGuiLoadCallback(function()
-			styleDBM()
-			hooksecurefunc(DBM_GUI, "UpdateModList", styleDBM)
-			DBM_GUI_OptionsFrameBossMods:HookScript("OnShow", styleDBM)
-		end)
-
-		hooksecurefunc(DBM_GUI_OptionsFrame, "DisplayButton", function(button, element)
-			-- bit of a hack, can't get the API to work
-			local pushed = element.toggle:GetPushedTexture():GetTexture()
-
-			if not element.styled then
-				F.ReskinExpandOrCollapse(element.toggle)
-				element.toggle:GetPushedTexture():SetAlpha(0)
-
-				element.styled = true
-			end
-
-			element.toggle.plus:SetShown(pushed and pushed:find("Plus"))
-		end)
-
-		F.CreateBD(DBM_GUI_OptionsFrame)
-		F.CreateSD(DBM_GUI_OptionsFrame)
-		F.Reskin(DBM_GUI_OptionsFrameOkay)
-		F.ReskinScroll(DBM_GUI_OptionsFramePanelContainerFOVScrollBar)
-
-		load = nil
-	end)
 end
 
 if IsAddOnLoaded("DBM-Core") then
--- FreeUI/scripts/theme.lua
+++ FreeUI/scripts/theme.lua
@@ -1787,38 +1787,6 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 			F.CreateBG(bu)
 		end
 
-		-- DBM
-
-		if IsAddOnLoaded("DBM-Core") then
-			local firstInfo = true
-			hooksecurefunc(DBM.InfoFrame, "Show", function()
-				if firstInfo then
-					DBMInfoFrame:SetBackdrop(nil)
-					local bd = CreateFrame("Frame", nil, DBMInfoFrame)
-					bd:SetPoint("TOPLEFT")
-					bd:SetPoint("BOTTOMRIGHT")
-					bd:SetFrameLevel(DBMInfoFrame:GetFrameLevel()-1)
-					F.CreateBD(bd)
-
-					firstInfo = false
-				end
-			end)
-
-			local firstRange = true
-			hooksecurefunc(DBM.RangeCheck, "Show", function()
-				if firstRange then
-					DBMRangeCheck:SetBackdrop(nil)
-					local bd = CreateFrame("Frame", nil, DBMRangeCheck)
-					bd:SetPoint("TOPLEFT")
-					bd:SetPoint("BOTTOMRIGHT")
-					bd:SetFrameLevel(DBMRangeCheck:GetFrameLevel()-1)
-					F.CreateBD(bd)
-
-					firstRange = false
-				end
-			end)
-		end
-
 		-- Achievement popup
 
 		local function fixBg(f)
@@ -7224,33 +7192,72 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 		F.Reskin(VoidStorageTransferButton)
 		F.ReskinClose(VoidStorageBorderFrame:GetChildren(), nil)
 		F.ReskinInput(VoidItemSearchBox)
-	elseif addon == "DBM-Core" then
-		local firstInfo = true
-		hooksecurefunc(DBM.InfoFrame, "Show", function()
-			if firstInfo then
-				DBMInfoFrame:SetBackdrop(nil)
-				local bd = CreateFrame("Frame", nil, DBMInfoFrame)
-				bd:SetPoint("TOPLEFT")
-				bd:SetPoint("BOTTOMRIGHT")
-				bd:SetFrameLevel(DBMInfoFrame:GetFrameLevel()-1)
-				F.CreateBD(bd)
+	elseif addon == "DBM-GUI" then
+		DBM_GUI_OptionsFrameHeader:SetTexture(nil)
+		DBM_GUI_OptionsFramePanelContainer:SetBackdrop(nil)
+		DBM_GUI_OptionsFrameBossMods:DisableDrawLayer("BACKGROUND")
+		DBM_GUI_OptionsFrameDBMOptions:DisableDrawLayer("BACKGROUND")
 
-				firstInfo = false
+		for i = 1, 2 do
+			_G["DBM_GUI_OptionsFrameTab"..i.."Left"]:SetAlpha(0)
+			_G["DBM_GUI_OptionsFrameTab"..i.."Middle"]:SetAlpha(0)
+			_G["DBM_GUI_OptionsFrameTab"..i.."Right"]:SetAlpha(0)
+			_G["DBM_GUI_OptionsFrameTab"..i.."LeftDisabled"]:SetAlpha(0)
+			_G["DBM_GUI_OptionsFrameTab"..i.."MiddleDisabled"]:SetAlpha(0)
+			_G["DBM_GUI_OptionsFrameTab"..i.."RightDisabled"]:SetAlpha(0)
+		end
+
+		local count = 1
+
+		local function styleDBM()
+			local option = _G["DBM_GUI_Option_"..count]
+			while option do
+				local objType = option:GetObjectType()
+				if objType == "CheckButton" then
+					F.ReskinCheck(option)
+				elseif objType == "Slider" then
+					F.ReskinSlider(option)
+				elseif objType == "EditBox" then
+					F.ReskinInput(option)
+				elseif option:GetName():find("DropDown") then
+					F.ReskinDropDown(option)
+				elseif objType == "Button" then
+					F.Reskin(option)
+				elseif objType == "Frame" then
+					option:SetBackdrop(nil)
+				end
+
+				count = count + 1
+				option = _G["DBM_GUI_Option_"..count]
+				if not option then
+					option = _G["DBM_GUI_DropDown"..count]
+				end
 			end
+		end
+
+		DBM:RegisterOnGuiLoadCallback(function()
+			styleDBM()
+			hooksecurefunc(DBM_GUI, "UpdateModList", styleDBM)
+			DBM_GUI_OptionsFrameBossMods:HookScript("OnShow", styleDBM)
 		end)
 
-		local firstRange = true
-		hooksecurefunc(DBM.RangeCheck, "Show", function()
-			if firstRange then
-				DBMRangeCheck:SetBackdrop(nil)
-				local bd = CreateFrame("Frame", nil, DBMRangeCheck)
-				bd:SetPoint("TOPLEFT")
-				bd:SetPoint("BOTTOMRIGHT")
-				bd:SetFrameLevel(DBMRangeCheck:GetFrameLevel()-1)
-				F.CreateBD(bd)
+		hooksecurefunc(DBM_GUI_OptionsFrame, "DisplayButton", function(button, element)
+			-- bit of a hack, can't get the API to work
+			local pushed = element.toggle:GetPushedTexture():GetTexture()
 
-				firstRange = false
+			if not element.styled then
+				F.ReskinExpandOrCollapse(element.toggle)
+				element.toggle:GetPushedTexture():SetAlpha(0)
+
+				element.styled = true
 			end
+
+			element.toggle.plus:SetShown(pushed and pushed:find("Plus"))
 		end)
+
+		F.CreateBD(DBM_GUI_OptionsFrame)
+		F.CreateSD(DBM_GUI_OptionsFrame)
+		F.Reskin(DBM_GUI_OptionsFrameOkay)
+		F.ReskinScroll(DBM_GUI_OptionsFramePanelContainerFOVScrollBar)
 	end
 end)
