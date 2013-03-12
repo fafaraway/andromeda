932f52e1d948d8b90b06643c2e27179c43608c74
Haleth <haleth@live.com>
2013-03-12 18:58:58 +0100

    Improve interrupt module (options will follow).

--- FreeUI/scripts/interrupt.lua
+++ FreeUI/scripts/interrupt.lua
@@ -2,23 +2,56 @@ local F, C = unpack(select(2, ...))
 
 if not C.general.interrupt then return end
 
+-- it will always be enabled in arena and raid, mention this. Only included necessary options
+local enableInParty = true -- 5 man groups / scenarios
+local enableOutdoors = true
+local enableInLFG = true -- raid finder/battleground (must be enabled for home made battleground groups too)
+	local enableInBGs = false
+
 local playerName = UnitName("player")
+local LE_PARTY_CATEGORY_INSTANCE, LE_PARTY_CATEGORY_HOME = LE_PARTY_CATEGORY_INSTANCE, LE_PARTY_CATEGORY_HOME
 
-local interrupt = CreateFrame("Frame")
-interrupt:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-interrupt:SetScript("OnEvent", function(_, _, _, subevent, _, _, sourceName, _, _, _, destName, _, _, _, _, _, spellID)
-	if subevent == "SPELL_INTERRUPT" then
-		if sourceName == playerName and GetNumGroupMembers() > 5 then
-			local isInstance, instanceType = IsInInstance()
-			if isInstance and instanceType ~= "pvp" then
-				local channel
-				if IsInGroup(LE_PARTY_CATEGORY_INSTANCE) then
-					channel = "INSTANCE_CHAT"
-				else
-					channel = "RAID"
+F.RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED", function(_, _, subEvent, _, _, sourceName, _, _, _, destName, _, _, _, _, _, spellID)
+	if subEvent == "SPELL_INTERRUPT" then
+		if sourceName == playerName then
+			local channel
+
+			if IsInGroup(LE_PARTY_CATEGORY_INSTANCE) then
+				if enableInLFG then
+					local isInstance, instanceType = IsInInstance()
+
+					if isInstance then
+						if instanceType == "pvp" then
+							if enableInBGs then
+								channel = "INSTANCE_CHAT"
+							end
+						elseif instanceType == "raid" or instanceType == "arena" or enableInParty then -- if not raid or arena (which are always enabled), it can only be scenario or party
+							channel = "INSTANCE_CHAT"
+						end
+					end
 				end
+			elseif IsInGroup(LE_PARTY_CATEGORY_HOME) then
+				local isInstance, instanceType = IsInInstance()
 
-				SendChatMessage("Interrupted: "..destName.."'s "..GetSpellLink(spellID)..".", channel)
+				if isInstance then
+					if (instanceType == "party" and enableInParty) then
+						channel = "PARTY"
+					elseif (instanceType == "raid" and enableInRaids) then
+						channel = "RAID"
+					end
+				elseif enableOutdoors then
+					local num = GetNumGroupMembers()
+
+					if num > 5 and enableInRaids then
+						channel = "RAID"
+					elseif num > 0 and enableInParty then -- party works
+						channel = "PARTY"
+					end
+				end
+			end
+
+			if channel then
+				SendChatMessage("Interrupted "..destName.."'s "..GetSpellLink(spellID)..".", channel)
 			end
 		end
 	end
