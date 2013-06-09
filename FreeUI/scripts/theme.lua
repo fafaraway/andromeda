072d1d3e8412a2ceee073f5bdb9b4c42bde8ad67
Haleth <haleth@telenet.be>
2013-06-09 23:58:47 +0200

    Fix item upgrade currency icon bg always showing.

--- FreeUI/scripts/theme.lua
+++ FreeUI/scripts/theme.lua
@@ -6078,6 +6078,9 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 		F.Reskin(ItemSocketingSocketButton)
 		F.ReskinScroll(ItemSocketingScrollFrameScrollBar)
 	elseif addon == "Blizzard_ItemUpgradeUI" then
+		local ItemUpgradeFrame = ItemUpgradeFrame
+		local ItemButton = ItemUpgradeFrame.ItemButton
+
 		ItemUpgradeFrame:DisableDrawLayer("BACKGROUND")
 		ItemUpgradeFrame:DisableDrawLayer("BORDER")
 		ItemUpgradeFrameMoneyFrameLeft:Hide()
@@ -6086,40 +6089,42 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 		ItemUpgradeFrame.ButtonFrame:GetRegions():Hide()
 		ItemUpgradeFrame.ButtonFrame.ButtonBorder:Hide()
 		ItemUpgradeFrame.ButtonFrame.ButtonBottomBorder:Hide()
-		ItemUpgradeFrame.ItemButton.Frame:Hide()
-		ItemUpgradeFrame.ItemButton.Grabber:Hide()
-		ItemUpgradeFrame.ItemButton.TextFrame:Hide()
-		ItemUpgradeFrame.ItemButton.TextGrabber:Hide()
+		ItemButton.Frame:Hide()
+		ItemButton.Grabber:Hide()
+		ItemButton.TextFrame:Hide()
+		ItemButton.TextGrabber:Hide()
 
-		F.CreateBD(ItemUpgradeFrame.ItemButton, .25)
-		ItemUpgradeFrame.ItemButton:SetHighlightTexture("")
-		ItemUpgradeFrame.ItemButton:SetPushedTexture("")
-		ItemUpgradeFrame.ItemButton.IconTexture:SetPoint("TOPLEFT", 1, -1)
-		ItemUpgradeFrame.ItemButton.IconTexture:SetPoint("BOTTOMRIGHT", -1, 1)
+		F.CreateBD(ItemButton, .25)
+		ItemButton:SetHighlightTexture("")
+		ItemButton:SetPushedTexture("")
+		ItemButton.IconTexture:SetPoint("TOPLEFT", 1, -1)
+		ItemButton.IconTexture:SetPoint("BOTTOMRIGHT", -1, 1)
 
-		local bg = CreateFrame("Frame", nil, ItemUpgradeFrame.ItemButton)
+		local bg = CreateFrame("Frame", nil, ItemButton)
 		bg:SetSize(341, 50)
-		bg:SetPoint("LEFT", ItemUpgradeFrame.ItemButton, "RIGHT", -1, 0)
-		bg:SetFrameLevel(ItemUpgradeFrame.ItemButton:GetFrameLevel()-1)
+		bg:SetPoint("LEFT", ItemButton, "RIGHT", -1, 0)
+		bg:SetFrameLevel(ItemButton:GetFrameLevel()-1)
 		F.CreateBD(bg, .25)
 
-		ItemUpgradeFrame.ItemButton:HookScript("OnEnter", function(self)
+		ItemButton:HookScript("OnEnter", function(self)
 			self:SetBackdropBorderColor(1, .56, .85)
 		end)
-		ItemUpgradeFrame.ItemButton:HookScript("OnLeave", function(self)
+		ItemButton:HookScript("OnLeave", function(self)
 			self:SetBackdropBorderColor(0, 0, 0)
 		end)
 
+		ItemButton.Cost.Icon:SetTexCoord(.08, .92, .08, .92)
+		ItemButton.Cost.Icon.bg = F.CreateBG(ItemButton.Cost.Icon)
+
 		hooksecurefunc("ItemUpgradeFrame_Update", function()
 			if GetItemUpgradeItemInfo() then
-				ItemUpgradeFrame.ItemButton.IconTexture:SetTexCoord(.08, .92, .08, .92)
+				ItemButton.IconTexture:SetTexCoord(.08, .92, .08, .92)
+				ItemButton.Cost.Icon.bg:Show()
 			else
-				ItemUpgradeFrame.ItemButton.IconTexture:SetTexture("")
+				ItemButton.IconTexture:SetTexture("")
+				ItemButton.Cost.Icon.bg:Hide()
 			end
 		end)
-
-		ItemUpgradeFrame.ItemButton.Cost.Icon:SetTexCoord(.08, .92, .08, .92)
-		F.CreateBG(ItemUpgradeFrame.ItemButton.Cost.Icon)
 
 		local currency = ItemUpgradeFrameMoneyFrame.Currency
 		currency.icon:SetPoint("LEFT", currency.count, "RIGHT", 1, 0)
