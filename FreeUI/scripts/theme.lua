3856197879c6f8cde4fb5a3dddcd48dce6cdd516
Haleth <haleth@telenet.be>
2013-09-22 21:05:01 +0200

    Adjust world state score frame tab positions.

--- FreeUI/scripts/theme.lua
+++ FreeUI/scripts/theme.lua
@@ -3244,16 +3244,18 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 		select(2, WorldStateScoreScrollFrame:GetRegions()):Hide()
 		select(3, WorldStateScoreScrollFrame:GetRegions()):Hide()
 
+		WorldStateScoreFrameTab1:ClearAllPoints()
+		WorldStateScoreFrameTab1:SetPoint("TOPLEFT", WorldStateScoreFrame, "BOTTOMLEFT", 5, 2)
 		WorldStateScoreFrameTab2:SetPoint("LEFT", WorldStateScoreFrameTab1, "RIGHT", -15, 0)
 		WorldStateScoreFrameTab3:SetPoint("LEFT", WorldStateScoreFrameTab2, "RIGHT", -15, 0)
-
-		F.ReskinPortraitFrame(WorldStateScoreFrame, true)
-		F.Reskin(WorldStateScoreFrameLeaveButton)
-		F.ReskinScroll(WorldStateScoreScrollFrameScrollBar)
 
 		for i = 1, 3 do
 			F.ReskinTab(_G["WorldStateScoreFrameTab"..i])
 		end
+
+		F.ReskinPortraitFrame(WorldStateScoreFrame, true)
+		F.Reskin(WorldStateScoreFrameLeaveButton)
+		F.ReskinScroll(WorldStateScoreScrollFrameScrollBar)
 
 		-- Item text
 
