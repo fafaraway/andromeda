8e22460235eb1b62e5320bf2c8f15edffd22e1e4
Haleth <haleth@telenet.be>
2013-10-13 17:48:01 +0200

    Remove (broken) group disband slash command.

--- FreeUI/core/slash.lua
+++ FreeUI/core/slash.lua
@@ -22,26 +22,6 @@ SlashCmdList.ROLECHECK = InitiateRolePoll
 SLASH_ROLECHECK1 = "/rolecheck"
 SLASH_ROLECHECK2 = "/rolepoll"
 
-SlashCmdList.GROUPDISBAND = function()
-		SendChatMessage("Disbanding group.", "RAID" or "PARTY")
-		if UnitInRaid("player") then
-			for i = 1, GetNumRaidMembers() do
-				local name, _, _, _, _, _, _, online = GetRaidRosterInfo(i)
-				if online and name ~= UnitName("player") then
-					UninviteUnit(name)
-				end
-			end
-		else
-			for i = MAX_PARTY_MEMBERS, 1, -1 do
-				if GetPartyMember(i) then
-					UninviteUnit(UnitName("party"..i))
-				end
-			end
-		end
-		LeaveParty()
-end
-SLASH_GROUPDISBAND1 = "/rd"
-
 SlashCmdList.TICKET = ToggleHelpFrame
 SLASH_TICKET1 = "/gm"
 
