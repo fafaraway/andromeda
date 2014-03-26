c7a970b2dd05b7f560ca296d38ae5af8c07c978a
Haleth <haleth@telenet.be>
2014-03-26 18:25:34 +0100

    Restyle spec icon on loot won alert frame.
    Even though it doesn't even work properly in the default UI.

--- FreeUI/scripts/theme.lua
+++ FreeUI/scripts/theme.lua
@@ -2691,10 +2691,17 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 				frame.IconBorder:Hide()
 				frame.glow:SetTexture("")
 				frame.shine:SetTexture("")
+				frame.SpecRing:SetTexture("")
 
 				frame.Icon:SetTexCoord(.08, .92, .08, .92)
 				F.CreateBG(frame.Icon)
+
+				frame.SpecIcon:SetTexCoord(.08, .92, .08, .92)
+				frame.SpecIcon.bg = F.CreateBG(frame.SpecIcon)
+				frame.SpecIcon.bg:SetDrawLayer("BORDER", 2)
 			end
+
+			frame.SpecIcon.bg:SetShown(frame.SpecIcon:IsShown())
 		end)
 
 		-- Money won alert
