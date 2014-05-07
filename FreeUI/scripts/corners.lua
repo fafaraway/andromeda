be4d13dcd13954d0c0900dbf292507051bad7fd7
Haleth <haleth@telenet.be>
2014-05-07 19:08:07 +0200

    Swap mouse buttons for micro and chat menu.
    Also clean up code a bit.

--- FreeUI/scripts/corners.lua
+++ FreeUI/scripts/corners.lua
@@ -2,22 +2,54 @@ local F, C, L = unpack(select(2, ...))
 
 local r, g, b = unpack(C.class)
 
-F.menuShown = false
+-- Left corner
 
-local function onMouseUp(self)
-	if IsAddOnLoaded("alDamageMeter") then
-		DisableAddOn("alDamageMeter")
-		DEFAULT_CHAT_FRAME:AddMessage("FreeUI: |cffffffffalDamageMeter disabled. Type|r /rl |cfffffffffor the changes to apply.|r", r, g, b)
-	else
-		EnableAddOn("alDamageMeter")
-		LoadAddOn("alDamageMeter")
-		if IsAddOnLoaded("alDamageMeter") then
-			DEFAULT_CHAT_FRAME:AddMessage("FreeUI: |cffffffffalDamageMeter loaded.|r", r, g, b)
+local left = CreateFrame("Frame")
+left:SetBackdrop({
+	bgFile = C.media.backdrop,
+	edgeFile = C.media.glow,
+	edgeSize = 3,
+	insets = {left = 3, right = 3, top = 3, bottom = 3},
+})
+left:SetBackdropColor(.4, .6, 1)
+left:SetBackdropBorderColor(.4, .6, 1)
+left:SetAlpha(0)
+left:SetSize(8, 8)
+left:SetPoint("BOTTOMLEFT")
+left:EnableMouse(true)
+left:SetScript("OnMouseDown", function(self, button)
+	if button == "LeftButton" then
+		if DropDownList1:IsShown() then
+			ToggleFrame(DropDownList1)
 		else
-			DEFAULT_CHAT_FRAME:AddMessage("FreeUI: |cffffffffalDamageMeter not found!|r", r, g, b)
+			F.MicroMenu()
 		end
+	elseif button == "RightButton" then
+		ToggleFrame(ChatMenu)
 	end
-end
+end)
+
+left:SetScript("OnEnter", function(self)
+	self:SetAlpha(1)
+	F.CreatePulse(self)
+	if not InCombatLockdown() then
+		GameTooltip:SetOwner(self, "ANCHOR_NONE")
+		GameTooltip:SetPoint("BOTTOMLEFT", self, "BOTTOMLEFT", 14, 14)
+		GameTooltip:AddLine("FreeUI", r, g, b)
+		GameTooltip:AddLine(" ")
+		GameTooltip:AddDoubleLine("Left-click:", "Toggle micro menu", r, g, b, 1, 1, 1)
+		GameTooltip:AddDoubleLine("Right-click:", "Toggle chat menu", r, g, b, 1, 1, 1)
+		GameTooltip:Show()
+	end
+end)
+
+left:SetScript("OnLeave", function()
+	left:SetScript("OnUpdate", nil)
+	left:SetAlpha(0)
+	GameTooltip:Hide()
+end)
+
+-- Right corner
 
 local right = CreateFrame("Frame")
 right:SetBackdrop({
@@ -79,50 +111,5 @@ end)
 right:SetScript("OnLeave", function(self)
 	self:SetScript("OnUpdate", nil)
 	self:SetAlpha(0)
-	GameTooltip:Hide()
-end)
-
-local left = CreateFrame("Frame")
-left:SetBackdrop({
-	bgFile = C.media.backdrop,
-	edgeFile = C.media.glow,
-	edgeSize = 3,
-	insets = {left = 3, right = 3, top = 3, bottom = 3},
-})
-left:SetBackdropColor(.4, .6, 1)
-left:SetBackdropBorderColor(.4, .6, 1)
-left:SetAlpha(0)
-left:SetSize(8, 8)
-left:SetPoint("BOTTOMLEFT")
-left:EnableMouse(true)
-left:SetScript("OnMouseDown", function(self, button)
-	if button == "LeftButton" then
-		ToggleFrame(ChatMenu)
-	elseif button == "RightButton" then
-		if DropDownList1:IsShown() then
-			ToggleFrame(DropDownList1)
-		else
-			F.MicroMenu()
-		end
-	end
-end)
-
-left:SetScript("OnEnter", function(self)
-	self:SetAlpha(1)
-	F.CreatePulse(self)
-	if not InCombatLockdown() then
-		GameTooltip:SetOwner(self, "ANCHOR_NONE")
-		GameTooltip:SetPoint("BOTTOMLEFT", self, "BOTTOMLEFT", 14, 14)
-		GameTooltip:AddLine("FreeUI", r, g, b)
-		GameTooltip:AddLine(" ")
-		GameTooltip:AddDoubleLine("Left-click:", "Toggle chat menu", r, g, b, 1, 1, 1)
-		GameTooltip:AddDoubleLine("Right-click:", "Toggle micro menu", r, g, b, 1, 1, 1)
-		GameTooltip:Show()
-	end
-end)
-
-left:SetScript("OnLeave", function()
-	left:SetScript("OnUpdate", nil)
-	left:SetAlpha(0)
 	GameTooltip:Hide()
 end)
--- FreeUI_Install/install.lua
+++ FreeUI_Install/install.lua
@@ -325,7 +325,7 @@ end
 local tut3 = function()
 	sb:SetValue(300)
 	header:SetText("3. Features")
-	body:SetText("There's a couple of small tools in this UI you might be interested in as well. For example, there are two ways to track buffs; an embedded buff tracker, to monitor buffs; and a selfbuff reminder, which will display an icon if the buff is missing while in combat.\n\nYou can set up these buff trackers in the options. Examples are included. Selfbuffs are already set up, so no worries.\n\nYou can mark mobs by alt+shift-clicking mobs in the game world and selecting an icon from the dropdown menu.\n\nLeft/right-clicking the bottom left corner of the screen toggles the chat menu and the micro menu respectively. Left/right-clicking the bottom right corner toggles alDamageMeter and DBM respectively.")
+	body:SetText("There's a couple of small tools in this UI you might be interested in as well. For example, there are two ways to track buffs; an embedded buff tracker, to monitor buffs; and a selfbuff reminder, which will display an icon if the buff is missing while in combat.\n\nYou can set up these buff trackers in the options. Examples are included. Selfbuffs are already set up, so no worries.\n\nYou can mark mobs by alt+shift-clicking mobs in the game world and selecting an icon from the dropdown menu.\n\nLeft/right-clicking the bottom left corner of the screen toggles the micro menu and the chat menu respectively. Left/right-clicking the bottom right corner toggles alDamageMeter and DBM respectively.")
 
 	sbt:SetText("3/6")
 
