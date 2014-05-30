eb57b45998fd5c9dc52dd6a3b19611a42c5def8a
Haleth <haleth@telenet.be>
2014-05-31 00:50:50 +0200

    Reduce combat text font size.

--- FreeUI/scripts/combattext.lua
+++ FreeUI/scripts/combattext.lua
@@ -8,7 +8,7 @@ local frames = {}
 
 for i = 1, 2 do
 	local f = CreateFrame("ScrollingMessageFrame", "LightCT"..i, UIParent)
-	F.SetFS(f, C.FONT_SIZE_LARGE)
+	F.SetFS(f)
 	f:SetShadowColor(0, 0, 0, 0)
 	f:SetFadeDuration(0.2)
 	f:SetTimeVisible(3)
@@ -18,10 +18,10 @@ for i = 1, 2 do
 	f:SetHeight(150)
 
 	if i == 1 then
-		f:SetJustifyH"RIGHT"
+		f:SetJustifyH("RIGHT")
 		f:SetPoint("RIGHT", UIParent, "CENTER", -175, 80)
 	else
-		f:SetJustifyH"LEFT"
+		f:SetJustifyH("LEFT")
 		f:SetPoint("LEFT", UIParent, "CENTER", -375, 80)
 	end
 
@@ -32,20 +32,20 @@ local tbl = {
 	["DAMAGE"] = 			{frame = 1, prefix =  "-", 		arg2 = true, 	r = 1, 		g = 0.1, 	b = 0.1},
 	["DAMAGE_CRIT"] = 		{frame = 1, prefix = "c-", 		arg2 = true, 	r = 1, 		g = 0.1, 	b = 0.1},
 	["SPELL_DAMAGE"] = 		{frame = 1, prefix =  "-", 		arg2 = true, 	r = 0.79, 	g = 0.3, 	b = 0.85},
-	["SPELL_DAMAGE_CRIT"] = 	{frame = 1, prefix = "c-", 		arg2 = true, 	r = 0.79, 	g = 0.3, 	b = 0.85},
-	["HEAL"] = 			{frame = 2, prefix =  "+", 		arg3 = true, 	r = 0.1, 	g = 1, 		b = 0.1},
+	["SPELL_DAMAGE_CRIT"] = {frame = 1, prefix = "c-", 		arg2 = true, 	r = 0.79, 	g = 0.3, 	b = 0.85},
+	["HEAL"] = 				{frame = 2, prefix =  "+", 		arg3 = true, 	r = 0.1, 	g = 1, 		b = 0.1},
 	["HEAL_CRIT"] = 		{frame = 2, prefix = "c+", 		arg3 = true, 	r = 0.1, 	g = 1, 		b = 0.1},
-	["PERIODIC_HEAL"] = 		{frame = 2, prefix =  "+", 		arg3 = true, 	r = 0.1, 	g = 1, 		b = 0.1},
-	["MISS"] = 			{frame = 1, prefix = "Miss", 				r = 1, 		g = 0.1, 	b = 0.1},
-	["SPELL_MISS"] = 		{frame = 1, prefix = "Miss", 				r = 0.79, 	g = 0.3, 	b = 0.85},
-	["SPELL_REFLECT"] = 		{frame = 1, prefix = "Reflect", 			r = 1, 		g = 1, 		b = 1},
-	["DODGE"] = 			{frame = 1, prefix = "Dodge", 				r = 1, 		g = 0.1, 	b = 0.1},
-	["PARRY"] = 			{frame = 1, prefix = "Parry", 				r = 1, 		g = 0.1, 	b = 0.1},
-	["BLOCK"] = 			{frame = 1, prefix = "Block", 	spec = true,		r = 1, 		g = 0.1, 	b = 0.1},
-	["RESIST"] = 			{frame = 1, prefix = "Resist", 	spec = true, 		r = 1, 		g = 0.1, 	b = 0.1},
-	["SPELL_RESIST"] = 		{frame = 1, prefix = "Resist", 	spec = true, 		r = 0.79, 	g = 0.3, 	b = 0.85},
-	["ABSORB"] = 			{frame = 1, prefix = "Absorb", 	spec = true, 		r = 1, 		g = 0.1, 	b = 0.1},
-	["SPELL_ABSORBED"] = 		{frame = 1, prefix = "Absorb", 	spec = true, 		r = 0.79, 	g = 0.3, 	b = 0.85},
+	["PERIODIC_HEAL"] = 	{frame = 2, prefix =  "+", 		arg3 = true, 	r = 0.1, 	g = 1, 		b = 0.1},
+	["MISS"] = 				{frame = 1, prefix = "Miss", 					r = 1, 		g = 0.1, 	b = 0.1},
+	["SPELL_MISS"] = 		{frame = 1, prefix = "Miss", 					r = 0.79, 	g = 0.3, 	b = 0.85},
+	["SPELL_REFLECT"] = 	{frame = 1, prefix = "Reflect", 				r = 1, 		g = 1, 		b = 1},
+	["DODGE"] = 			{frame = 1, prefix = "Dodge", 					r = 1, 		g = 0.1, 	b = 0.1},
+	["PARRY"] = 			{frame = 1, prefix = "Parry", 					r = 1, 		g = 0.1, 	b = 0.1},
+	["BLOCK"] = 			{frame = 1, prefix = "Block", 	spec = true,	r = 1, 		g = 0.1, 	b = 0.1},
+	["RESIST"] = 			{frame = 1, prefix = "Resist", 	spec = true, 	r = 1, 		g = 0.1, 	b = 0.1},
+	["SPELL_RESIST"] = 		{frame = 1, prefix = "Resist", 	spec = true, 	r = 0.79, 	g = 0.3, 	b = 0.85},
+	["ABSORB"] = 			{frame = 1, prefix = "Absorb", 	spec = true, 	r = 1, 		g = 0.1, 	b = 0.1},
+	["SPELL_ABSORBED"] = 	{frame = 1, prefix = "Absorb", 	spec = true, 	r = 0.79, 	g = 0.3, 	b = 0.85},
 }
 
 local info
