919fbb03dd38ec2201b8fbfcdefeee980d1d4a1c
Haleth <haleth@telenet.be>
2014-11-08 14:57:50 +0100

    Fix bag slot outlines disappearing sometimes.

--- FreeUI/scripts/bags.lua
+++ FreeUI/scripts/bags.lua
@@ -2,6 +2,8 @@
 
 local F, C, L = unpack(select(2, ...))
 
+local _G = _G
+
 if C.bags.style ~= 1 then
 	local function toggleBagSlots()
 		local parent = C.bags.hideSlots and FreeUIHider or MainMenuBarArtFrame
@@ -17,8 +19,6 @@ if C.bags.style ~= 1 then
 
 	return
 end
-
-local _G = _G
 
 local r, g, b = unpack(C.class)
 
@@ -616,7 +616,7 @@ local function bagOnMouseover(self, isEnter)
 		for j = 1, GetContainerNumSlots(0) do
 			local bu = _G["ContainerFrame1Item"..j]
 			bu.bg:SetBackdropBorderColor(bgR, bgG, bgB)
-			bu.bg:SetShown(isEnter)
+			bu.bg:SetShown(isEnter or C.bags.slotsShowAlways)
 			bu.IconBorder:SetAlpha(isEnter and 0 or 1)
 		end
 	else
@@ -628,7 +628,7 @@ local function bagOnMouseover(self, isEnter)
 				for j = 1, GetContainerNumSlots(frame:GetID()) do
 					local bu = _G["ContainerFrame"..i.."Item"..j]
 					bu.bg:SetBackdropBorderColor(bgR, bgG, bgB)
-					bu.bg:SetShown(isEnter)
+					bu.bg:SetShown(isEnter or C.bags.slotsShowAlways)
 					bu.IconBorder:SetAlpha(isEnter and 0 or 1)
 				end
 
@@ -725,7 +725,7 @@ local function bankBagOnMouseover(self, isEnter)
 			for j = 1, GetContainerNumSlots(frame:GetID()) do
 				local bu = _G["ContainerFrame"..i.."Item"..j]
 				bu.bg:SetBackdropBorderColor(bgR, bgG, bgB)
-				bu.bg:SetShown(isEnter)
+				bu.bg:SetShown(isEnter or C.bags.slotsShowAlways)
 				bu.IconBorder:SetAlpha(isEnter and 0 or 1)
 			end
 
