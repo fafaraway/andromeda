672c81f751c88acf2080f619ecdbaac5ec776b5f
Haleth <haleth@telenet.be>
2016-06-05 02:05:48 +0200

    Disable interface category hiding for now

--- FreeUI/scripts/disable.lua
+++ FreeUI/scripts/disable.lua
@@ -1,11 +1,11 @@
 local F, C = unpack(select(2, ...))
 
 -- disable Blizzard UF options
-if C.unitframes.enable then
-	InterfaceOptionsFrameCategoriesButton10:SetParent(FreeUIHider)
-	InterfaceOptionsFrameCategoriesButton11:SetParent(FreeUIHider)
-	InterfaceOptionsFrameCategoriesButton12:SetPoint("TOPLEFT", InterfaceOptionsFrameCategoriesButton9, "BOTTOMLEFT")
-end
+-- if C.unitframes.enable then
+-- 	InterfaceOptionsFrameCategoriesButton10:SetParent(FreeUIHider)
+-- 	InterfaceOptionsFrameCategoriesButton11:SetParent(FreeUIHider)
+-- 	InterfaceOptionsFrameCategoriesButton12:SetPoint("TOPLEFT", InterfaceOptionsFrameCategoriesButton9, "BOTTOMLEFT")
+-- end
 
 -- disable store button
 GameMenuFrame:HookScript("OnShow", function(self)
