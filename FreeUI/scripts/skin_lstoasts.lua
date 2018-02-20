42cf14f213ed9bbbcc0b617389b52d435b90789a
solor <solor@users.noreply.github.com>
2018-02-20 20:56:46 +0800

    Fix ls_toasts skin.

--- FreeUI/scripts/skin_lstoasts.lua
+++ FreeUI/scripts/skin_lstoasts.lua
@@ -7,41 +7,54 @@ Delay:SetScript("OnEvent", function()
 	if IsAddOnLoaded("ls_Toasts") then
 
 		local LST = unpack(ls_Toasts)
-		LST:RegisterSkin("FreeUI.Fluffy", function(toast)
 
-			toast.Border:SetAlpha(0)
-			toast.TextBG:SetAlpha(.5)
-			toast.BG:SetAlpha(.7)
-
-			if not toast.skinned then
-				local bg = toast:CreateTexture(nil, 'BACKGROUND', nil, -1)
-				bg:SetPoint('TOPLEFT', toast, 1, -1)
-				bg:SetPoint('BOTTOMRIGHT', toast, -1, 1)
-				bg:SetColorTexture(0, 0, 0, .7)
-
-				-- local tr = toast:CreateTexture(nil, 'BACKGROUND', nil, -2)
-				-- tr:SetPoint('TOPLEFT', toast, 1, -1)
-				-- tr:SetPoint('BOTTOMRIGHT', toast, -1, 1)
-				-- tr:SetColorTexture(0, 0, 0, 1)
-
-				F.CreateSD(toast)
-
-				toast.skinned = true
-			end
-
-			toast.Icon:SetTexCoord(.08, .92, .08, .92)
-
-			-- toast.Title:SetFont([[]], 11)
-
-			if toast.IconBorder then
-				local r, g, b = toast.IconBorder:GetVertexColor()
-				if r > 0.99 and g > 0.99 and b > 0.99 then
-					toast.IconBorder:SetVertexColor(0.1, 0.1, 0.1, 1)
-				end
-				toast.IconBorder:SetTexture([[Interface\AddOns\FreeUI\media\border.tga]])
-				toast.IconBorder:SetTexCoord(0, 1, 0, 1)
-			end
-		end)
-		LST:SetSkin("FreeUI.Fluffy")
+		LST:RegisterSkin("FreeUI", {
+			name = "FreeUI",
+			border = {
+				color = {0, 0, 0},
+				offset = 0,
+				size = 1,
+				texture = {1, 1, 1, 1},
+			},
+			title = {
+				flags = "OUTLINE",
+				shadow = false,
+			},
+			text = {
+				flags = "OUTLINE",
+				shadow = false,
+			},
+			bonus = {
+				hidden = false,
+			},
+			dragon = {
+				hidden = false,
+			},
+			icon = {
+				tex_coords = {5 / 64, 59 / 64, 5 / 64, 59 / 64},
+			},
+			icon_border = {
+				color = {0, 0, 0},
+				offset = 0,
+				size = 1,
+				texture = {1, 1, 1, 1},
+			},
+			icon_highlight = {
+				hidden = false,
+			},
+			icon_text_1 = {
+				flags = "OUTLINE",
+				shadow = false,
+			},
+			icon_text_2 = {
+				flags = "OUTLINE",
+				shadow = false,
+			},
+			skull = {
+				hidden = false,
+			},
+		})
+		LST:SetSkin("FreeUI")
 	end
+
 end)
