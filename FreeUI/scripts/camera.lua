5fbeaf35a8bb71bcbdcf27ed7402bc226948bb97
solor <solor@users.noreply.github.com>
2018-07-23 04:31:23 +0800

    Update camera module.

--- FreeUI/scripts/camera.lua
+++ FreeUI/scripts/camera.lua
@@ -1,66 +1,43 @@
 local F, C, L = unpack(select(2, ...))
+local module = F:RegisterModule("camera")
 
--- Author: Ketho (EU-Boulderfist)
--- License: Public Domain
-
-local db
-
-db = {
-	["speed"] = C.camera.speed,
-	["increment"] = C.camera.increment,
-	["distance"] = C.camera.distance,
-}
-
-	----------------
-	--- Prehooks ---
-	----------------
+-- FasterCamera by Ketho
+-- Increases camera zoom speed
 
 local oldZoomIn = CameraZoomIn
 local oldZoomOut = CameraZoomOut
 
 function CameraZoomIn(distance)
-	oldZoomIn(db.increment)
+	oldZoomIn(C.camera.increment)
 end
 
 function CameraZoomOut(distance)
-	oldZoomOut(db.increment)
+	oldZoomOut(C.camera.increment)
 end
 
--- tried this out in the SotA demolishers
--- the normal camera zoom functions seemed to be used instead
--- I suppose both can be used interchangeably
+
 local oldVehicleZoomIn = VehicleCameraZoomIn
 local oldVehicleZoomOut = VehicleCameraZoomOut
 
 function VehicleCameraZoomIn(distance)
-	oldVehicleZoomIn(db.increment)
+	oldVehicleZoomIn(C.camera.increment)
 end
 
 function VehicleCameraZoomOut(distance)
-	oldVehicleZoomOut(db.increment)
+	oldVehicleZoomOut(C.camera.increment)
 end
 
-	---------------
-	--- Options ---
-	---------------
 
 local cvar = {
 	"cameraDistanceMoveSpeed",
 	"cameraDistanceMaxZoomFactor",
 }
 
-	----------------------
-	--- Initialization ---
-	----------------------
 
--- delay setting/overriding the CVars because it's either
--- not yet ready or is being reverted/overridden by something else
--- not sure if there is any event to wait for instead
 C_Timer.After(1, function()
-	-- not actually necessary to override from savedvars
-	-- but better to do this if other addons also set it
-	SetCVar("cameraDistanceMaxZoomFactor", db.distance)
-	--SetCVar("cameraDistanceMoveSpeed", db.speed)
+
+	SetCVar("cameraDistanceMaxZoomFactor", C.camera.distance)
+
 end)
 
 local f = CreateFrame("Frame")
@@ -72,3 +49,11 @@ end
 
 f:RegisterEvent("ADDON_LOADED")
 f:SetScript("OnEvent", f.OnEvent)
+
+
+function module:OnLogin()
+
+	if tonumber(GetCVar("cameraDistanceMaxZoomFactor")) ~= 2.6 then
+		SetCVar("cameraDistanceMaxZoomFactor", 2.6)
+	end
+end
