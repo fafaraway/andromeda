d0d60f7d6703b0df0329ce9715e4066471fffcee
solor <solor@users.noreply.github.com>
2018-07-22 22:13:13 +0800

    提醒禁用独立版本的 Aurora

--- FreeUI/theme/theme.lua
+++ FreeUI/theme/theme.lua
@@ -1,7 +1,12 @@
---local _, ns = ...
---local B, C, L, DB, F = unpack(ns)
+
 
 local F, C, L = unpack(select(2, ...))
+
+if IsAddOnLoaded("Aurora") then
+	print("FreeUI includes an efficient built-in version of Aurora.")
+	print("It's highly recommended that you disable Aurora.")
+	return
+end
 
 
 
@@ -21,29 +26,13 @@ C.themes["FreeUI"] = {}
 local Skin = CreateFrame("Frame")
 Skin:RegisterEvent("ADDON_LOADED")
 Skin:SetScript("OnEvent", function(_, _, addon)
-	--[[if addon == "FreeUI" then
 
-
-		useButtonGradientColour = C.themeconfig.useButtonGradientColour
-
-		if useButtonGradientColour then
-			buttonR, buttonG, buttonB, buttonA = unpack(C.themeconfig.buttonGradientColour)
-		else
-			buttonR, buttonG, buttonB, buttonA = unpack(C.themeconfig.buttonSolidColour)
-		end
-
-		if C.themeconfig.useCustomColour then
-			r, g, b = C.themeconfig.customColour.r, C.themeconfig.customColour.g, C.themeconfig.customColour.b
-		end
-
-		-- for modules
-		C.r, C.g, C.b = r, g, b
-	end]]
 
 	-- [[ Load modules ]]
 
 	-- check if the addon loaded is supported by Aurora, and if it is, execute its module
 	local addonModule = C.themes[addon]
+
 	if addonModule then
 		if type(addonModule) == "function" then
 			addonModule()
@@ -53,6 +42,7 @@ Skin:SetScript("OnEvent", function(_, _, addon)
 			end
 		end
 	end
+
 end)
 
 
