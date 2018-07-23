0c1ed3b7718a46ab33969e72de0cee29ca04e87b
solor <solor@users.noreply.github.com>
2018-07-23 20:31:31 +0800

    Cleanup.

--- FreeUI/theme/theme.lua
+++ FreeUI/theme/theme.lua
@@ -1,5 +1,3 @@
-
-
 local F, C, L = unpack(select(2, ...))
 
 if IsAddOnLoaded("Aurora") then
@@ -7,16 +5,6 @@ if IsAddOnLoaded("Aurora") then
 	print("It's highly recommended that you disable Aurora.")
 	return
 end
-
-
-
-
-local useButtonGradientColour
-local _, class = UnitClass("player")
-
-local r, g, b = C.classcolours[class].r, C.classcolours[class].g, C.classcolours[class].b
-
-
 
 C.themes = {}
 C.themes["FreeUI"] = {}
@@ -27,10 +15,8 @@ local Skin = CreateFrame("Frame")
 Skin:RegisterEvent("ADDON_LOADED")
 Skin:SetScript("OnEvent", function(_, _, addon)
 
-
 	-- [[ Load modules ]]
 
-	-- check if the addon loaded is supported by Aurora, and if it is, execute its module
 	local addonModule = C.themes[addon]
 
 	if addonModule then
