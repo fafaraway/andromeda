83d5b15c90f416a27e19121dd4da1f08c8ad7b33
solor <solor@users.noreply.github.com>
2018-08-04 17:14:24 +0800

    AuroraClassic

---
+++ AuroraClassic/AddOns/Blizzard_ChallengesUI.lua
@@ -0,0 +1,74 @@
+local F, C = unpack(select(2, ...))
+
+C.themes["Blizzard_ChallengesUI"] = function()
+	local function AffixesSetup(self)
+		for _, frame in ipairs(self.Affixes) do
+			frame.Border:SetTexture(nil)
+			frame.Portrait:SetTexture(nil)
+			if not frame.bg then
+				frame.bg = F.ReskinIcon(frame.Portrait)
+			end
+
+			if frame.info then
+				frame.Portrait:SetTexture(CHALLENGE_MODE_EXTRA_AFFIX_INFO[frame.info.key].texture)
+			elseif frame.affixID then
+				local _, _, filedataid = C_ChallengeMode.GetAffixInfo(frame.affixID)
+				frame.Portrait:SetTexture(filedataid)
+			end
+		end
+	end
+
+	ChallengesFrameInset:DisableDrawLayer("BORDER")
+	ChallengesFrameInsetBg:Hide()
+	for i = 1, 2 do
+		select(i, ChallengesFrame:GetRegions()):Hide()
+	end
+
+	local angryStyle
+	local function UpdateIcons(self)
+		for i = 1, #self.maps do
+			local bu = self.DungeonIcons[i]
+			if bu and not bu.styled then
+				bu:GetRegions():SetAlpha(0)
+				bu.Icon:SetTexCoord(.08, .92, .08, .92)
+				F.CreateBDFrame(bu.Icon)
+
+				bu.styled = true
+			end
+		end
+
+		if IsAddOnLoaded("AngryKeystones") and not angryStyle then
+			local scheduel = select(4, self:GetChildren())
+			scheduel:GetRegions():SetAlpha(0)
+			select(3, scheduel:GetRegions()):SetAlpha(0)
+			F.CreateBD(scheduel, .3)
+			if scheduel.Entries then
+				for i = 1, 4 do
+					AffixesSetup(scheduel.Entries[i])
+				end
+			end
+
+			angryStyle = true
+		end
+	end
+	hooksecurefunc("ChallengesFrame_Update", UpdateIcons)
+
+	hooksecurefunc(ChallengesFrame.WeeklyInfo, "SetUp", function(self)
+		local affixes = C_MythicPlus.GetCurrentAffixes()
+		if affixes then
+			AffixesSetup(self.Child)
+		end
+	end)
+
+	local keystone = ChallengesKeystoneFrame
+	F.SetBD(keystone)
+	F.ReskinClose(keystone.CloseButton)
+	F.Reskin(keystone.StartButton)
+
+	hooksecurefunc(keystone, "Reset", function(self)
+		self:GetRegions():SetAlpha(0)
+		self.InstructionBackground:SetAlpha(0)
+	end)
+
+	hooksecurefunc(keystone, "OnKeystoneSlotted", AffixesSetup)
+end
---
+++ AuroraClassic/FrameXML/ColorPickerFrame.lua
@@ -0,0 +1,47 @@
+local F, C = unpack(select(2, ...))
+
+tinsert(C.themes["AuroraClassic"], function()
+	ColorPickerFrameHeader:SetAlpha(0)
+	ColorPickerFrameHeader:ClearAllPoints()
+	ColorPickerFrameHeader:SetPoint("TOP", ColorPickerFrame, 0, 0)
+	F.CreateBD(ColorPickerFrame)
+	F.CreateSD(ColorPickerFrame)
+	--F.Reskin(ColorPickerOkayButton)
+	--F.Reskin(ColorPickerCancelButton)
+	--F.ReskinSlider(OpacitySliderFrame, true)
+
+
+	--_G.ColorPickerFrameHeader:Hide()
+	--local header = select(3, _G.ColorPickerFrame:GetRegions())
+	--header:SetPoint("TOP", _G.ColorPickerFrame, 0, -4)
+	--F.CreateBD(_G.ColorPickerFrame)
+
+	F.Reskin(_G.ColorPickerCancelButton)
+	_G.ColorPickerCancelButton:SetWidth(100)
+
+	F.Reskin(_G.ColorPickerOkayButton)
+	_G.ColorPickerOkayButton:SetWidth(100)
+	_G.ColorPickerOkayButton:SetPoint("RIGHT", _G.ColorPickerCancelButton, "LEFT", -10, 0)
+
+	_G.OpacitySliderFrame:ClearAllPoints()
+	_G.OpacitySliderFrame:SetPoint("TOPRIGHT", -30, -30)
+	F.ReskinSlider(_G.OpacitySliderFrame, true)
+
+	_G.ColorPickerWheel:SetPoint("TOPLEFT", 10, -30)
+
+	local ColorValue = _G.ColorPickerFrame:GetColorValueTexture()
+	ColorValue:SetPoint("LEFT", _G.ColorPickerWheel, "RIGHT", 13, 0)
+
+	_G.ColorSwatch:SetPoint("TOPLEFT", 205, -30)
+
+	_G.ColorPickerFrame:HookScript("OnShow", function(self)
+		if self.hasOpacity then
+			self:SetWidth(400)
+		else
+			self:SetWidth(360)
+		end
+	end)
+
+	F.CreateBD(_G.OpacityFrame)
+	F.ReskinSlider(_G.OpacityFrameSlider, true)
+end)
