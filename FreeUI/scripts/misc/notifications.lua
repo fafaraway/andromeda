f644103cfd605062ed3bbecedcfdc68024b0e549
solor <solor@users.noreply.github.com>
2018-08-07 21:03:31 +0800

    Update interrupt alert.

--- FreeUI/scripts/misc/notifications.lua
+++ FreeUI/scripts/misc/notifications.lua
@@ -11,11 +11,8 @@ end
 
 
 -- rare mob/event alert
-
 function module:RareAlert()
 	if not C.misc.rareAlert then return end
-
-
 
 	local cache = {}
 	local function updateAlert(_, id)
@@ -31,16 +28,11 @@ function module:RareAlert()
 			local tex = string.format("|T%s:%d:%d:0:0:%d:%d:%d:%d:%d:%d|t", filename, 0, 0, atlasWidth, atlasHeight, atlasWidth*txLeft, atlasWidth*txRight, atlasHeight*txTop, atlasHeight*txBottom)
 			--UIErrorsFrame:AddMessage(C.infoColor.."Rare Found"..tex..(info.name or ""))
 
-
-
 			RaidNotice_AddMessage(RaidWarningFrame, tex.." "..(info.name or "Unknown").." ".."spotted!", ChatTypeInfo["RAID_WARNING"])
 			
-
 			if C.misc.rareAlertNotify then
-
 				--print("  -> "..C.infoColor.."Rare Found"..tex..(info.name or ""))
 				print(info.name, "spotted!")
-
 			end
 			PlaySoundFile("Sound\\Interface\\PVPFlagTakenMono.ogg", "master")
 			cache[id] = true
@@ -52,52 +44,41 @@ function module:RareAlert()
 end
 
 
-
-
-
--- interrupt alert
-
-
+-- interrupt/stolen/dispel alert
 function module:InterruptAlert()
-
 	if not C.misc.interruptAlert then return end
 
 	local interruptSound = "Interface\\AddOns\\FreeUI\\assets\\sound\\Shutupfool.ogg"
 	local dispelSound = "Interface\\AddOns\\FreeUI\\assets\\sound\\buzz.ogg"
 
+	local channel
+	if IsPartyLFG() then
+		channel = "INSTANCE_CHAT"
+	elseif IsInRaid() then
+		channel = "RAID"
+	elseif IsInGroup() and not IsInRaid() then
+		channel = "PARTY"
+	end
 
 	local frame = CreateFrame("Frame")
-
 	frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-
 	frame:SetScript("OnEvent", function(self)
 		local _, event, _, sourceGUID, _, _, _, _, destName, _, _, _, _, _, spellID = CombatLogGetCurrentEventInfo()
-
 		if ((sourceGUID == UnitGUID("player")) or (sourceGUID == UnitGUID("pet"))) then
-
 			if (event == "SPELL_INTERRUPT") then
-
-				PlaySoundFile(interruptSound, "Master")
-
-				if IsInGroup() and C.misc.interruptNotify then
-					if C.client == "zhCN" or C.client == "zhTW" then
-						SendChatMessage("已打断: "..destName.."'s "..GetSpellLink(spellID)..".")
-					else
-						SendChatMessage("Interrupted: "..destName.."'s "..GetSpellLink(spellID)..".")
-					end
+				if C.misc.interruptSound then
+					PlaySoundFile(interruptSound, "Master")
 				end
-				
-			elseif ((event == "SPELL_STOLEN") or (event == "SPELL_DISPEL")) then
-
-				PlaySoundFile(dispelSound, "Master")
-
+				if C.misc.interruptNotify then
+					SendChatMessage("Interrupted: "..destName.."'s "..GetSpellLink(spellID)..".", channel)
+				end
+			elseif ((event == "SPELL_STOLEN") or (event == "SPELL_DISPEL") or (event == "SPELL_MISSED")) then
+				if C.misc.dispelSound then
+					PlaySoundFile(dispelSound, "Master")
+				end
 			end
-
 		end
-
-
 	end)
-
 end
 
 
