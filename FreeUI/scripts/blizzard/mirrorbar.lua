dcad79b832df8ff91b20cfd702d67a108fdb80a4
solor <solor@users.noreply.github.com>
2018-08-09 10:37:51 +0800

    Update mirror bar.

--- FreeUI/scripts/blizzard/mirrorbar.lua
+++ FreeUI/scripts/blizzard/mirrorbar.lua
@@ -2,34 +2,36 @@ local F, C, L = unpack(select(2, ...))
 
 -- Mirror bar
 local function MBSkin(timer, value, maxvalue, scale, paused, label)
+	local previous
 	for i = 1, MIRRORTIMER_NUMTIMERS, 1 do
 		local frame = _G["MirrorTimer"..i]
 		if not frame.isSkinned then
-			local statusbar = _G[frame:GetName().."StatusBar"]
-			local border = _G[frame:GetName().."Border"]
-			local text = _G[frame:GetName().."Text"]
-			
-			statusbar:ClearAllPoints()
-			statusbar:SetPoint("TOPLEFT", frame, 2, -2)
-			statusbar:SetPoint("BOTTOMRIGHT", frame, -2, 8)
+			F.StripTextures(frame, true)
+			frame:SetSize(200, 16)
+
+			local bg = F.CreateBDFrame(frame)
+			F.CreateTex(bg)
+			F.CreateSD(bg)
+
+			local statusbar = _G["MirrorTimer"..i.."StatusBar"]
+			statusbar:SetAllPoints()
 			statusbar:SetStatusBarTexture(C.media.texture)
-			statusbar:SetHeight(16)
-			statusbar:SetWidth(200)
-			
-			local region = frame:GetRegions()
-			if region:GetObjectType() == "Texture" then
-				region:SetAlpha(0)
-			end
-		
-			statusbar.backdrop = F.CreateBDFrame(statusbar, .5)
-			statusbar.backdrop:SetPoint("BOTTOMRIGHT", statusbar, 1, -2)
-			F.CreateSD(statusbar.backdrop, .5)
-			
+			statusbar:SetAlpha(.75)
+
+			local text = _G["MirrorTimer"..i.."Text"]
 			text:ClearAllPoints()
-			text:SetFont(C.font.normal, 11, "OUTLINE")
 			text:SetPoint("CENTER", statusbar)
 			
-			border:SetTexture(nil)
+			if C.client == 'zhCN' then
+				text:SetFont(C.font.normal, 11, "OUTLINE")
+			else
+				F.SetFS(text)
+			end
+
+			if previous then
+				frame:SetPoint("TOP", previous, "BOTTOM", 0, -5)
+			end
+			previous = frame
 			
 			frame.isSkinned = true
 		end
