572be86d14c4af45093acf83ea47ac583ce86f3d
solor <solor@users.noreply.github.com>
2018-08-13 04:42:50 +0800

    Cleanup.

--- FreeUI/scripts/maps/minimap.lua
+++ FreeUI/scripts/maps/minimap.lua
@@ -214,7 +214,7 @@ function module:ReskinRegions()
 	if TicketStatusFrame then
 		TicketStatusFrame:ClearAllPoints()
 		TicketStatusFrame:SetPoint("TOPLEFT", UIParent, 100, -100)
-		TicketStatusFrame.SetPoint = F.Dummy
+		TicketStatusFrame.SetPoint = F.dummy
 	end
 
 	-- durability
