c0802b916f2920e28b15843d7db24a8e6cb1a794
solor <solor@users.noreply.github.com>
2018-08-18 06:06:55 +0800

    Update cooldown module.

--- FreeUI/scripts/misc/cooldown.lua
+++ FreeUI/scripts/misc/cooldown.lua
@@ -6,7 +6,7 @@ local module = F:RegisterModule("cooldown")
 function module:OnLogin()
 	--if not C.actionbars.cooldown then return end
 
-	local FONT_SIZE = 16
+	local FONT_SIZE = 18
 	local MIN_DURATION = 2.5                    -- the minimum duration to show cooldown text for
 	local MIN_SCALE = 0.5                       -- the minimum scale we want to show cooldown counts at, anything below this will be hidden
 	local ICON_SIZE = 36
@@ -33,9 +33,10 @@ function module:OnLogin()
 		if fontScale < MIN_SCALE then
 			self:Hide()
 		else
-			--self.text:SetFont(C.font.normal, fontScale * FONT_SIZE, "OUTLINE")
+			self.text:SetFont("Interface\\AddOns\\FreeUI\\assets\\font\\supereffective.ttf", 16, "OUTLINEMONOCHROME")
 			--self.text:SetShadowColor(0, 0, 0, 0)
-			F.SetFS(self.text)
+			--F.SetFS(self.text)
+			self.text:SetPoint("BOTTOM", 2, 2)
 
 			if self.enabled then
 				Timer_ForceUpdate(self)
