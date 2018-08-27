6f212b91e39f20cbc9740bd4376799ef99c562a3
solor <solor@users.noreply.github.com>
2018-08-27 14:29:02 +0800

    Cleanup ui scale module.

--- FreeUI/core/uiscale.lua
+++ FreeUI/core/uiscale.lua
@@ -3,8 +3,8 @@ local module = F:RegisterModule("uiscale")
 
 -- UI scale
 local function ForceUIScale()
-	--F.HideOption(Advanced_UseUIScale)
-	--F.HideOption(Advanced_UIScaleSlider)
+	F.HideOption(Advanced_UseUIScale)
+	F.HideOption(Advanced_UIScaleSlider)
 
 	local scale = C.misc.uiScale
 	local pysWidth, pysHeight = _G.GetPhysicalScreenSize()
@@ -16,22 +16,6 @@ local function ForceUIScale()
 	_G.SetCVar("useUiScale", 0)
 	_G.SetCVar("uiScale", scale)
 	_G.UIParent:SetScale(scale)
-
-	local function RestoreUIScale(scale)
-		_G.UIParent:SetScale(scale)
-		if C.chat.lockPosition then
-			ChatFrame1:ClearAllPoints()
-			ChatFrame1:SetPoint(unpack(C.chat.position))
-		end
-	end
-
-	F:RegisterEvent("UI_SCALE_CHANGED", function()
-		C_Timer.After(1, function()
-			if UIParent:GetScale() ~= scale then
-				RestoreUIScale(scale)
-			end
-		end)
-	end)
 
 
 	--local cvarScale, parentScale = _G.GetCVar("uiscale"), _G.UIParent:GetScale()
