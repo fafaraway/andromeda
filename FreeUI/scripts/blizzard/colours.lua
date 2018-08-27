2c470d601b74fd50b0cae4bf6618eab0dbe9d5d6
solor <solor@users.noreply.github.com>
2018-08-27 14:28:20 +0800

    Remove outdated stuff.

--- FreeUI/scripts/blizzard/colours.lua
+++ FreeUI/scripts/blizzard/colours.lua
@@ -278,18 +278,6 @@ loaded:SetScript("OnEvent", function(self, event, addon)
 end)
 
 
--- lfr
-hooksecurefunc("LFRBrowseFrameListButton_SetData", function(button, index)
-	local name, level, _, className, _, _, _, class = SearchLFGGetResults(index)
-	if name == ycc.myName then return end
-	if class and name and level then
-		button.name:SetText(ycc.classColor[class]..name)
-		button.class:SetText(ycc.classColor[class]..className)
-		button.level:SetText(ycc.diffColor[level]..level)
-	end
-end)
-
-
 -- who
 local columnTable
 hooksecurefunc("WhoList_Update", function()
