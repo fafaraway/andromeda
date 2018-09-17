217f05d9b8b8d66641bc92c3624382b852770417
solor <solor@users.noreply.github.com>
2018-09-18 05:08:10 +0800

    Update Aurora.

--- AuroraClassic/AddOns/Blizzard_IslandsPartyPoseUI.lua
+++ AuroraClassic/AddOns/Blizzard_IslandsPartyPoseUI.lua
@@ -5,10 +5,23 @@ C.themes["Blizzard_IslandsPartyPoseUI"] = function()
 	F.SetBD(IslandsPartyPoseFrame)
 	F.Reskin(IslandsPartyPoseFrame.LeaveButton)
 	F.StripTextures(IslandsPartyPoseFrame.ModelScene)
-	F.CreateBDFrame(IslandsPartyPoseFrame.ModelScene, .25)
+	--F.CreateBDFrame(IslandsPartyPoseFrame.ModelScene, .25)
+
+	IslandsPartyPoseFrame.ModelScene:SetAlpha(.8)
 
 	IslandsPartyPoseFrame.Topper:Hide()
 	IslandsPartyPoseFrame.Background:Hide()
+
+	IslandsPartyPoseFrame.RightBorder:Hide()
+	IslandsPartyPoseFrame.LeftBorder:Hide()
+	IslandsPartyPoseFrame.TopRightCorner:Hide()
+	IslandsPartyPoseFrame.TopLeftCorner:Hide()
+	IslandsPartyPoseFrame.BotRightCorner:Hide()
+	IslandsPartyPoseFrame.BotLeftCorner:Hide()
+	IslandsPartyPoseFrame.TitleBg:Hide()
+	IslandsPartyPoseFrame.TopBorder:Hide()
+	IslandsPartyPoseFrame.BottomBorder:Hide()
+
 
 	local rewardFrame = IslandsPartyPoseFrame.RewardAnimations.RewardFrame
 	local bg = F.CreateBDFrame(rewardFrame)
