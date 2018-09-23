4daadcc5fdecc0b8bf23f0a60a1bcc1d3ace62eb
solor <solor@users.noreply.github.com>
2018-09-23 17:08:11 +0800

    Prevents spells from being automatically added to action bar.

--- FreeUI/scripts/blizzard/blizzard.lua
+++ FreeUI/scripts/blizzard/blizzard.lua
@@ -11,7 +11,23 @@ function module:OnLogin()
 end
 
 
+-- Prevents spells from being automatically added to your action bar
+-- This prevents icons from being animated onto the main action bar
+IconIntroTracker.RegisterEvent = function() end
+IconIntroTracker:UnregisterEvent('SPELL_PUSHED_TO_ACTIONBAR')
 
+-- In the unlikely event that you're looking at a different action page while switching talents
+-- the spell is automatically added to your main bar. This takes it back off.
+local f = CreateFrame('frame')
+f:SetScript('OnEvent', function(self, event, spellID, slotIndex, slotPos)
+	-- This event should never fire in combat, but check anyway
+	if not InCombatLockdown() then
+		ClearCursor()
+		PickupAction(slotIndex)
+		ClearCursor()
+	end
+end)
+f:RegisterEvent('SPELL_PUSHED_TO_ACTIONBAR')
 
 
 
