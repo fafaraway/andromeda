5fbf4d5bbc9396fcfa2a1f24b9d96258c43e8353
solor <solor@users.noreply.github.com>
2018-09-25 23:57:08 +0800

    Add more info on tooltip.

--- FreeUI/scripts/tooltips/extrainfo.lua
+++ FreeUI/scripts/tooltips/extrainfo.lua
@@ -9,7 +9,6 @@ function module:extraInfo()
 	local types = {
 		spell = SPELLS.."ID:",
 		item = ITEMS.."ID:",
-		unit = "NPCID",
 	}
 
 	local function addLine(self, id, type, noadd)
@@ -105,7 +104,55 @@ function module:extraInfo()
 	hooksecurefunc(GameTooltip, "SetUnitAura", SetCaster)
 
 
-	-- NPCs id
+	-- took from idTip by silverwind
+	local hooksecurefunc, select, tonumber, strfind = hooksecurefunc, select, tonumber, strfind
+
+	local kinds = {
+		item = "ItemID",
+		unit = "NPCID",
+		visual = "VisualID",
+		source = "SourceID",
+	}
+
+	local function contains(table, element)
+		for _, value in pairs(table) do
+			if value == element then return true end
+		end
+		return false
+	end
+
+	local function addLine(tooltip, id, kind)
+		if not id or id == "" then return end
+		if type(id) == "table" and #id == 1 then id = id[1] end
+
+		local frame, text
+		for i = 1,15 do
+			frame = _G[tooltip:GetName() .. "TextLeft" .. i]
+			if frame then text = frame:GetText() end
+			if text and string.find(text, kind .. ":") then return end
+		end
+
+		local left, right
+		if type(id) == "table" then
+			left = NORMAL_FONT_COLOR_CODE .. kind .. "s:" .. FONT_COLOR_CODE_CLOSE
+			right = HIGHLIGHT_FONT_COLOR_CODE .. table.concat(id, ", ") .. FONT_COLOR_CODE_CLOSE
+		else
+			left = NORMAL_FONT_COLOR_CODE .. kind .. ":" .. FONT_COLOR_CODE_CLOSE
+			right = HIGHLIGHT_FONT_COLOR_CODE .. id .. FONT_COLOR_CODE_CLOSE
+		end
+
+		tooltip:AddDoubleLine(left, right)
+		tooltip:Show()
+	end
+
+	local function addLineByKind(self, id, kind)
+		if not kind or not id then return end
+		if kind == "visual" then
+			addLine(self, id, kinds.visual)
+		end
+	end
+
+	-- NPCs
 	GameTooltip:HookScript("OnTooltipSetUnit", function(self)
 		if C_PetBattles.IsInBattle() then return end
 		local unit = select(2, self:GetUnit())
@@ -113,11 +160,33 @@ function module:extraInfo()
 			local guid = UnitGUID(unit) or ""
 			local id = tonumber(guid:match("-(%d+)-%x+$"), 10)
 			if id and guid:match("%a+") ~= "Player" then
-				if IsShiftKeyDown() then addLine(self, id, types.unit) end
+				if IsShiftKeyDown() then addLine(GameTooltip, id, kinds.unit) end
 			end
 		end
 	end)
 
+	-- item ids on wardrobe, so we can easily enjoy LucidMorph :)
+	local f = CreateFrame("frame")
+	f:RegisterEvent("ADDON_LOADED")
+	f:SetScript("OnEvent", function(_, _, what)
+		if what == "Blizzard_Collections" then
+			hooksecurefunc("WardrobeCollectionFrame_SetAppearanceTooltip", function(self, sources)
+				local visualIDs = {}
+				local sourceIDs = {}
+				local itemIDs = {}
+
+				for i = 1, #sources do
+					if sources[i].visualID and not contains(visualIDs, sources[i].visualID) then table.insert(visualIDs, sources[i].visualID) end
+					if sources[i].sourceID and not contains(visualIDs, sources[i].sourceID) then table.insert(sourceIDs, sources[i].sourceID) end
+					if sources[i].itemID and not contains(visualIDs, sources[i].itemID) then table.insert(itemIDs, sources[i].itemID) end
+				end
+
+				if #visualIDs ~= 0 then addLine(GameTooltip, visualIDs, kinds.visual) end
+				if #sourceIDs ~= 0 then addLine(GameTooltip, sourceIDs, kinds.source) end
+				if #itemIDs ~= 0 then addLine(GameTooltip, itemIDs, kinds.item) end
+			end)
+		end
+	end)
 
 end
 
