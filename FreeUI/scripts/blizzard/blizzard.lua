fe9021b83816ae59428424f3831f572ae11ab75b
solor <solor@users.noreply.github.com>
2018-09-26 23:43:01 +0800

    Proper way to alter reputation/reaction colors.

--- FreeUI/scripts/blizzard/blizzard.lua
+++ FreeUI/scripts/blizzard/blizzard.lua
@@ -1,6 +1,6 @@
 local F, C, L = unpack(select(2, ...))
 
-local module = F:RegisterModule("blizzard")
+local module = F:RegisterModule('blizzard')
 
 function module:OnLogin()
 	self:FontStyle()
@@ -28,6 +28,34 @@ f:SetScript('OnEvent', function(self, event, spellID, slotIndex, slotPos)
 	end
 end)
 f:RegisterEvent('SPELL_PUSHED_TO_ACTIONBAR')
+
+
+-- custom reputation color
+hooksecurefunc('ReputationFrame_Update', function(showLFGPulse)
+	local numFactions = GetNumFactions()
+	local factionOffset = FauxScrollFrame_GetOffset(ReputationListScrollFrame)
+
+	for i = 1, NUM_FACTIONS_DISPLAYED, 1 do
+		local factionIndex = factionOffset + i
+		local factionBar = _G['ReputationBar'..i..'ReputationBar']
+
+		if factionIndex <= numFactions then
+			local name, description, standingID = GetFactionInfo(factionIndex)
+			local colorIndex = standingID
+
+			local color = C.reactioncolours[colorIndex]
+			factionBar:SetStatusBarColor(color[1], color[2], color[3])
+		end
+	end
+end)
+
+hooksecurefunc(ReputationBarMixin, 'Update', function(self)
+	local name, reaction, minBar, maxBar, value, factionID = GetWatchedFactionInfo();
+	local colorIndex = reaction;
+
+	local color = C.reactioncolours[colorIndex];
+	self:SetBarColor(color[1], color[2], color[3], 1);
+end)
 
 
 
