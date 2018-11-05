95657bbabe5decc7c3ee9f2988d19dd04531f8e8
solor <solor@users.noreply.github.com>
2018-11-05 22:03:00 +0800

    Refactor class colors.

--- FreeUI/scripts/blizzard/colours.lua
+++ FreeUI/scripts/blizzard/colours.lua
@@ -1,185 +1,93 @@
 local F, C = unpack(select(2, ...))
 
--- yClassColors by yleaf
--- colorize player names by their class in friend list, who list, guild list, etc..
+-- based on yClassColors by yleaf
 
+local function classColor(class, showRGB)
+	local color = C.classColors[C.classList[class] or class]
+	if showRGB then
+		return color.r, color.g, color.b
+	else
+		return "|c"..color.colorStr
+	end
+end
 
-local _, ns = ...
-local ycc = {}
-ns.ycc = ycc
+local function diffColor(level)
+	return F.HexRGB(GetQuestDifficultyColor(level))
+end
 
-local GUILD_INDEX_MAX = 12
-local SMOOTH = {
+local rankColor = {
 	1, 0, 0,
 	1, 1, 0,
-	0, 1, 0,
+	0, 1, 0
 }
-ycc.myName = UnitName"player"
 
-local BC = {}
-for k, v in pairs(LOCALIZED_CLASS_NAMES_MALE) do
-	BC[v] = k
-end
-for k, v in pairs(LOCALIZED_CLASS_NAMES_FEMALE) do
-	BC[v] = k
-end
-
-local RAID_CLASS_COLORS = C.classcolours or RAID_CLASS_COLORS
-local WHITE_HEX = "|cffffffff"
-
-local function Hex(r, g, b)
-	if type(r) == "table" then
-		if r.r then
-			r, g, b = r.r, r.g, r.b
-		else
-			r, g, b = unpack(r)
-		end
-	end
-
-	if not r or not g or not b then
-		r, g, b = 1, 1, 1
-	end
-
-	return format("|cff%02x%02x%02x", r*255, g*255, b*255)
-end
-
--- http://www.wowwiki.com/ColorGradient
-local function ColorGradient(perc, ...)
-	if perc >= 1 then
-		local r, g, b = select(select("#", ...) - 2, ...)
-		return r, g, b
-	elseif perc <= 0 then
-		local r, g, b = ...
-		return r, g, b
-	end
-	
-	local num = select("#", ...) / 3
-	local segment, relperc = math.modf(perc*(num-1))
-	local r1, g1, b1, r2, g2, b2 = select((segment*3)+1, ...)
-
-	return r1 + (r2-r1)*relperc, g1 + (g2-g1)*relperc, b1 + (b2-b1)*relperc
-end
-
-ycc.guildRankColor = setmetatable({}, {
-	__index = function(t, i)
-		if i then
-			local c = Hex(ColorGradient(i/GUILD_INDEX_MAX, unpack(SMOOTH)))
-			if c then
-				t[i] = c
-				return c
-			else
-				t[i] = t[0]
-			end
-		end
-	end
-})
-ycc.guildRankColor[0] = WHITE_HEX
-
-ycc.diffColor = setmetatable({}, {
-	__index = function(t,i)
-		local c = i and GetQuestDifficultyColor(i)
-		t[i] = c and Hex(c) or t[0]
-
-		return t[i]
-	end
-})
-ycc.diffColor[0] = WHITE_HEX
-
-ycc.classColor = setmetatable({}, {
-	__index = function(t,i)
-		local c = i and RAID_CLASS_COLORS[BC[i] or i]
-		if c then
-			t[i] = Hex(c)
-			return t[i]
-		else
-			return WHITE_HEX
-		end
-	end
-})
-
-local GUILDREP = {
+local repColor = {
 	1, 0, 0,
 	1, 1, 0,
 	0, 1, 0,
 	0, 1, 1,
 	0, 0, 1,
 }
-ycc.repColor = setmetatable({}, {
-	__index = function(t, i)
-		if i then
-			local c = Hex(ColorGradient(i/5, unpack(GUILDREP)))
-			if c then
-				t[i] = c
-				return c
-			else
-				t[i] = t[0]
-			end
-		end
-	end
-})
 
-local WHITE = {1, 1, 1}
-ycc.classColorRaw = setmetatable({}, {
-	__index = function(t, i)
-		local c = i and RAID_CLASS_COLORS[BC[i] or i]
-		if not c then return WHITE end
-		t[i] = c
-
-		return c
-	end
-})
-
-if CUSTOM_CLASS_COLORS then
-	CUSTOM_CLASS_COLORS:RegisterCallback(function()
-		wipe(ycc.classColorRaw)
-		wipe(ycc.classColor)
-	end)
+local function smoothColor(cur, max, color)
+	local r, g, b = oUF:RGBColorGradient(cur, max, unpack(color))
+	return F.HexRGB(r, g, b)
 end
 
 
--- battlefield
---[[hooksecurefunc("WorldStateScoreFrame_Update", function()
-	local inArena = IsActiveBattlefieldArena()
-	local offset = FauxScrollFrame_GetOffset(WorldStateScoreScrollFrame)
+-- Guild
+local function updateGuildView()
+	currentView = currentView or GetCVar("guildRosterView")
 
-	for i = 1, 20 do
-		local index = offset + i
-		local name, _, _, _, _, faction, _, _, class = GetBattlefieldScore(index)
-		-- faction: Battlegrounds: Horde = 0, Alliance = 1 / Arenas: Green Team = 0, Yellow Team = 1
-		if name then
-			local n, r = strsplit("-", name, 2)
-			n = ycc.classColor[class]..n.."|r"
-			if name == ycc.myName then n = "> "..n.." <" end
+	local playerArea = GetRealZoneText()
+	local buttons = GuildRosterContainer.buttons
 
-			if r then
-				local color
-				if inArena then
-					if faction == 1 then
-						color = "|cffffd100"
-					else
-						color = "|cff19ff19"
+	for _, button in ipairs(buttons) do
+		if button:IsShown() and button.online and button.guildIndex then
+			if currentView == "tradeskill" then
+				local _, _, _, headerName, _, _, _, _, _, _, _, zone = GetGuildTradeSkillInfo(button.guildIndex)
+				if not headerName and zone == playerArea then
+					button.string2:SetText("|cff00ff00"..zone)
+				end
+			else
+				local _, rank, rankIndex, level, _, zone, _, _, _, _, _, _, _, _, _, repStanding = GetGuildRosterInfo(button.guildIndex)
+				if currentView == "playerStatus" then
+					button.string1:SetText(diffColor(level)..level)
+					if zone == playerArea then
+						button.string3:SetText("|cff00ff00"..zone)
 					end
-				else
-					if faction == 1 then
-						color = "|cff00adf0"
-					else
-						color = "|cffff1919"
+				elseif currentView == "guildStatus" then
+					if rankIndex and rank then
+						button.string2:SetText(smoothColor(rankIndex, 10, rankColor)..rank)
+					end
+				elseif currentView == "achievement" then
+					button.string1:SetText(diffColor(level)..level)
+				elseif currentView == "reputation" then
+					button.string1:SetText(diffColor(level)..level)
+					if repStanding then
+						button.string3:SetText(smoothColor(repStanding-4, 5, repColor).._G["FACTION_STANDING_LABEL"..repStanding])
 					end
 				end
-				r = color..r.."|r"
-				n = n.."|cffffffff - |r"..r
 			end
-
-			local button = _G["WorldStateScoreButton"..i]
-			button.name.text:SetText(n)
 		end
 	end
-end)
-]]
+end
 
--- friends
+local function updateGuildUI(event, addon)
+	if addon ~= "Blizzard_GuildUI" then return end
+	hooksecurefunc("GuildRoster_SetView", setView)
+	hooksecurefunc("GuildRoster_Update", updateGuildView)
+	hooksecurefunc(GuildRosterContainer, "update", updateGuildView)
+
+	F:UnregisterEvent(event, updateGuildUI)
+end
+F:RegisterEvent("ADDON_LOADED", updateGuildUI)
+
+
+-- Friends
 local FRIENDS_LEVEL_TEMPLATE = FRIENDS_LEVEL_TEMPLATE:gsub("%%d", "%%s")
 FRIENDS_LEVEL_TEMPLATE = FRIENDS_LEVEL_TEMPLATE:gsub("%$d", "%$s")
+
 local function friendsFrame()
 	local scrollFrame = FriendsFrameFriendsScrollFrame
 	local buttons = scrollFrame.buttons
@@ -190,9 +98,9 @@ local function friendsFrame()
 		local button = buttons[i]
 		if button:IsShown() then
 			if button.buttonType == FRIENDS_BUTTON_TYPE_WOW then
-				local name, level, class, area, connected, _, _ = GetFriendInfo(button.id)
+				local name, level, class, area, connected = GetFriendInfo(button.id)
 				if connected then
-					nameText = ycc.classColor[class]..name.."|r, "..format(FRIENDS_LEVEL_TEMPLATE, ycc.diffColor[level]..level.."|r", class)
+					nameText = classColor(class)..name.."|r, "..format(FRIENDS_LEVEL_TEMPLATE, diffColor(level)..level.."|r", class)
 					if area == playerArea then
 						infoText = format("|cff00ff00%s|r", area)
 					end
@@ -202,7 +110,7 @@ local function friendsFrame()
 				if isOnline and client == BNET_CLIENT_WOW then
 					local _, charName, _, _, _, faction, _, class, _, zoneName = BNGetGameAccountInfo(gameID)
 					if presenceName and charName and class and faction == UnitFactionGroup("player") then
-						nameText = presenceName.." "..FRIENDS_WOW_NAME_COLOR_CODE.."("..ycc.classColor[class]..charName..FRIENDS_WOW_NAME_COLOR_CODE..")"
+						nameText = presenceName.." "..FRIENDS_WOW_NAME_COLOR_CODE.."("..classColor(class)..charName..FRIENDS_WOW_NAME_COLOR_CODE..")"
 						if zoneName == playerArea then
 							infoText = format("|cff00ff00%s|r", zoneName)
 						end
@@ -219,68 +127,9 @@ hooksecurefunc(FriendsFrameFriendsScrollFrame, "update", friendsFrame)
 hooksecurefunc("FriendsFrame_UpdateFriends", friendsFrame)
 
 
--- guild
-local _VIEW
-
-local function setview(view)
-	_VIEW = view
-end
-
-local function update()
-	_VIEW = _VIEW or GetCVar"guildRosterView"
-	local playerArea = GetRealZoneText()
-	local buttons = GuildRosterContainer.buttons
-
-	for _, button in ipairs(buttons) do
-		-- why the fuck no continue?
-		if button:IsShown() and button.online and button.guildIndex then
-			if _VIEW == "tradeskill" then
-				local _, _, _, headerName, _, _, _, _, _, _, _, zone = GetGuildTradeSkillInfo(button.guildIndex)
-				if (not headerName) and UnitName("player") then
-					if zone == playerArea then
-						button.string2:SetText("|cff00ff00"..zone)
-					end
-				end
-			else
-				local _, rank, rankIndex, level, _, zone, _, _, _, _, _, _, _, _, _, repStanding = GetGuildRosterInfo(button.guildIndex)
-				if _VIEW == "playerStatus" then
-					button.string1:SetText(ycc.diffColor[level]..level)
-					if zone == playerArea then
-						button.string3:SetText("|cff00ff00"..zone)
-					end
-				elseif _VIEW == "guildStatus" then
-					if rankIndex and rank then
-						button.string2:SetText(ycc.guildRankColor[rankIndex]..rank)
-					end
-				elseif _VIEW == "achievement" then
-					button.string1:SetText(ycc.diffColor[level]..level)
-				elseif _VIEW == "reputation" then
-					button.string1:SetText(ycc.diffColor[level]..level)
-					if repStanding then
-						button.string3:SetText(ycc.repColor[repStanding-4].._G["FACTION_STANDING_LABEL"..repStanding])
-					end
-				end
-			end
-		end
-	end
-end
-
-local loaded = CreateFrame("Frame")
-loaded:RegisterEvent("ADDON_LOADED")
-loaded:SetScript("OnEvent", function(self, event, addon)
-	if addon ~= "Blizzard_GuildUI" then return end
-
-	hooksecurefunc("GuildRoster_SetView", setview)
-	hooksecurefunc("GuildRoster_Update", update)
-	hooksecurefunc(GuildRosterContainer, "update", update)
-
-	self:UnregisterEvent(event)
-end)
-
-
--- who
-local columnTable
-hooksecurefunc("WhoList_Update", function()
+-- Whoframe
+local columnTable = {}
+local function updateWhoList()
 	local whoOffset = FauxScrollFrame_GetOffset(WhoListScrollFrame)
 	local playerZone = GetRealZoneText()
 	local playerGuild = GetGuildInfo("player")
@@ -288,21 +137,85 @@ hooksecurefunc("WhoList_Update", function()
 
 	for i = 1, WHOS_TO_DISPLAY, 1 do
 		local index = whoOffset + i
-		local nameText = getglobal("WhoFrameButton"..i.."Name")
-		local levelText = getglobal("WhoFrameButton"..i.."Level")
-		local variableText = getglobal("WhoFrameButton"..i.."Variable")
+		local nameText = _G["WhoFrameButton"..i.."Name"]
+		local levelText = _G["WhoFrameButton"..i.."Level"]
+		local variableText = _G["WhoFrameButton"..i.."Variable"]
 
-		local name, guild, level, race, _, zone, classFileName = GetWhoInfo(index)
+		local name, guild, level, race, _, zone, class = GetWhoInfo(index)
 		if name then
 			if zone == playerZone then zone = "|cff00ff00"..zone end
 			if guild == playerGuild then guild = "|cff00ff00"..guild end
 			if race == playerRace then race = "|cff00ff00"..race end
 
-			columnTable = {zone, guild, race}
-			local c = ycc.classColorRaw[classFileName]
-			nameText:SetTextColor(c.r, c.g, c.b)
-			levelText:SetText(ycc.diffColor[level]..level)
+			wipe(columnTable)
+			tinsert(columnTable, zone)
+			tinsert(columnTable, guild)
+			tinsert(columnTable, race)
+
+			nameText:SetTextColor(classColor(class, true))
+			levelText:SetText(diffColor(level)..level)
 			variableText:SetText(columnTable[UIDropDownMenu_GetSelectedID(WhoFrameDropDown)])
 		end
 	end
-end)
+end
+hooksecurefunc("WhoList_Update", updateWhoList)
+
+
+-- chat
+local strfind, format, gsub, strmatch, strsub = string.find, string.format, string.gsub, string.match, string.sub
+local pairs, type = pairs, type
+
+local blizzHexColors = {}
+for class, color in pairs(C.classColors) do
+	blizzHexColors[color.colorStr] = class
+end
+
+function GetColoredName(event, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12)
+	if not arg2 then
+		return arg2
+	end
+
+	local chatType = strsub(event, 10)
+	if strsub(chatType, 1, 7) == "WHISPER" then
+		chatType = "WHISPER"
+	elseif strsub(chatType, 1, 7) == "CHANNEL" then
+		chatType = "CHANNEL"..arg8
+	end
+
+	if chatType == "GUILD" then
+		arg2 = Ambiguate(arg2, "guild")
+	else
+		arg2 = Ambiguate(arg2, "none")
+	end
+
+	local info = ChatTypeInfo[chatType]
+	if info and info.colorNameByClass and arg12 and arg12 ~= "" and arg12 ~= 0 then
+		local _, class = GetPlayerInfoByGUID(arg12)
+		local color = class and C.classColors[class]
+		if color then
+			return format("|c%s%s|r", color.colorStr, arg2)
+		end
+	end
+
+	return arg2
+end
+
+do
+	local AddMessage = {}
+
+	local function FixClassColors(frame, message, ...) -- 3174
+		if type(message) == "string" and strfind(message, "|cff") then -- type check required for shitty addons that pass nil or non-string values
+			for hex, class in pairs(blizzHexColors) do
+				local color = C.classColors[class]
+				message = color and gsub(message, hex, color.colorStr) or message -- color check required for Warmup, maybe others
+			end
+		end
+		return AddMessage[frame](frame, message, ...)
+	end
+
+	for i = 1, NUM_CHAT_WINDOWS do
+		local frame = _G["ChatFrame"..i]
+		AddMessage[frame] = frame.AddMessage
+		frame.AddMessage = FixClassColors
+	end
+end
