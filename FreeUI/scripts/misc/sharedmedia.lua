231a31cfa1085c2f8459554aff6304b12714d052
solor <solor@users.noreply.github.com>
2018-11-16 16:17:26 +0800

    New font.

--- FreeUI/scripts/misc/sharedmedia.lua
+++ FreeUI/scripts/misc/sharedmedia.lua
@@ -6,23 +6,24 @@ local LSM = _G.LibStub("LibSharedMedia-3.0")
 local koKR, ruRU, zhCN, zhTW, western = LSM.LOCALE_BIT_koKR, LSM.LOCALE_BIT_ruRU, LSM.LOCALE_BIT_zhCN, LSM.LOCALE_BIT_zhTW, LSM.LOCALE_BIT_western
 
 -- BACKGROUND
-LSM:Register("background", "FreeUI_BG", 				C.media.backdrop)
+LSM:Register("background", "FreeUI_BG", 			C.media.backdrop)
 
 -- STATUSBAR
-LSM:Register("statusbar", "FreeUI_SB",  				C.media.sbTex)
+LSM:Register("statusbar", "FreeUI_SB",  			C.media.sbTex)
 
 -- FONT
-LSM:Register("font", "FreeUI_ExocetBlizzardLight", 		[[Interface\Addons\FreeUI\assets\font\ExocetBlizzardLight.ttf]], zhCN + zhTW + western)
-LSM:Register("font", "FreeUI_ExocetBlizzardMedium", 	[[Interface\Addons\FreeUI\assets\font\ExocetBlizzardMedium.ttf]], zhCN + zhTW + western)
-LSM:Register("font", "FreeUI_supereffective", 			[[Interface\Addons\FreeUI\assets\font\supereffective.ttf]], zhCN + zhTW + western)
-LSM:Register("font", "FreeUI_pixel", 					[[Interface\Addons\FreeUI\assets\font\pixel.ttf]], zhCN + zhTW + western)
-LSM:Register("font", "FreeUI_pixel_bold", 				[[Interface\Addons\FreeUI\assets\font\pixel_bold.ttf]], zhCN + zhTW + western)
+LSM:Register("font", "FreeUI_ExocetBlizzardLight", 	[[Interface\Addons\FreeUI\assets\font\ExocetBlizzardLight.ttf]], zhCN + zhTW + western)
+LSM:Register("font", "FreeUI_ExocetBlizzardMedium", [[Interface\Addons\FreeUI\assets\font\ExocetBlizzardMedium.ttf]], zhCN + zhTW + western)
+LSM:Register("font", "FreeUI_supereffective", 		[[Interface\Addons\FreeUI\assets\font\supereffective.ttf]], zhCN + zhTW + western)
+LSM:Register("font", "FreeUI_pixel", 				[[Interface\Addons\FreeUI\assets\font\pixel.ttf]], zhCN + zhTW + western)
+LSM:Register("font", "FreeUI_pixel_bold", 			[[Interface\Addons\FreeUI\assets\font\pixel_bold.ttf]], zhCN + zhTW + western)
 
-LSM:Register("font", "FreeUI_PixfontCN", 				[[Fonts\pixfontCN.ttf]], zhCN + zhTW + western)
-LSM:Register("font", "FreeUI_Normal", 					C.font.normal, zhCN + zhTW + western)
-LSM:Register("font", "FreeUI_Header", 					C.font.header, zhCN + zhTW + western)
-LSM:Register("font", "FreeUI_Chat", 					C.font.chat, zhCN + zhTW + western)
+LSM:Register("font", "FreeUI_PixfontCN", 			[[Fonts\pixfontCN.ttf]], zhCN + zhTW + western)
+LSM:Register("font", "FreeUI_Normal", 				C.font.normal, zhCN + zhTW + western)
+LSM:Register("font", "FreeUI_Header", 				C.font.header, zhCN + zhTW + western)
+LSM:Register("font", "FreeUI_Chat", 				C.font.chat, zhCN + zhTW + western)
 
+LSM:Register("font", "ADAM.CG PRO", 				[[Interface\Addons\FreeUI\assets\font\ADAM.CG PRO.otf]], zhCN + zhTW + western)
 
 -- SOUND
 LSM:Register("sound", "FreeUI_bell", 				[[Interface\Addons\FreeUI\assets\sound\bell.ogg]])
