7c30497d5885e198f62467499c60f96963bb95ac
solor <solor@users.noreply.github.com>
2018-11-24 08:11:41 +0800

    Cleanup.

--- FreeUI/scripts/misc/misc.lua
+++ FreeUI/scripts/misc/misc.lua
@@ -18,40 +18,43 @@ function module:OnLogin()
 	self:ReadyCheckEnhancement()
 	self:AutoRepair()
 	self:AutoSetRole()
+	self:BuyStack()
 end
 
 
 
 -- ALT + Right Click to buy a stack
-local old_MerchantItemButton_OnModifiedClick = MerchantItemButton_OnModifiedClick
-local cache = {}
-function MerchantItemButton_OnModifiedClick(self, ...)
-	if IsAltKeyDown() then
-		local id = self:GetID()
-		local itemLink = GetMerchantItemLink(id)
-		if not itemLink then return end
-		local name, _, quality, _, _, _, _, maxStack, _, texture = GetItemInfo(itemLink)
-		if ( maxStack and maxStack > 1 ) then
-			if not cache[itemLink] then
-				StaticPopupDialogs["BUY_STACK"] = {
-					text = "Stack Buying Check",
-					button1 = YES,
-					button2 = NO,
-					OnAccept = function()
-						BuyMerchantItem(id, GetMerchantItemMaxStack(id))
-						cache[itemLink] = true
-					end,
-					hideOnEscape = 1,
-					hasItemFrame = 1,
-				}
-				local r, g, b = GetItemQualityColor(quality or 1)
-				StaticPopup_Show("BUY_STACK", " ", " ", {["texture"] = texture, ["name"] = name, ["color"] = {r, g, b, 1}, ["link"] = itemLink, ["index"] = id, ["count"] = maxStack})
-			else
-				BuyMerchantItem(id, GetMerchantItemMaxStack(id))
+function module:BuyStack()
+	local old_MerchantItemButton_OnModifiedClick = MerchantItemButton_OnModifiedClick
+	local cache = {}
+	function MerchantItemButton_OnModifiedClick(self, ...)
+		if IsAltKeyDown() then
+			local id = self:GetID()
+			local itemLink = GetMerchantItemLink(id)
+			if not itemLink then return end
+			local name, _, quality, _, _, _, _, maxStack, _, texture = GetItemInfo(itemLink)
+			if ( maxStack and maxStack > 1 ) then
+				if not cache[itemLink] then
+					StaticPopupDialogs["BUY_STACK"] = {
+						text = "Stack Buying Check",
+						button1 = YES,
+						button2 = NO,
+						OnAccept = function()
+							BuyMerchantItem(id, GetMerchantItemMaxStack(id))
+							cache[itemLink] = true
+						end,
+						hideOnEscape = 1,
+						hasItemFrame = 1,
+					}
+					local r, g, b = GetItemQualityColor(quality or 1)
+					StaticPopup_Show("BUY_STACK", " ", " ", {["texture"] = texture, ["name"] = name, ["color"] = {r, g, b, 1}, ["link"] = itemLink, ["index"] = id, ["count"] = maxStack})
+				else
+					BuyMerchantItem(id, GetMerchantItemMaxStack(id))
+				end
 			end
 		end
+		old_MerchantItemButton_OnModifiedClick(self, ...)
 	end
-	old_MerchantItemButton_OnModifiedClick(self, ...)
 end
 
 
