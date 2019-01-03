9e31ac8cbf5605d531c64925acb3e9273dc5647e
solor <solor@users.noreply.github.com>
2019-01-03 21:16:55 +0800

    Cleanup.

--- FreeUI/core/slash.lua
+++ FreeUI/core/slash.lua
@@ -1,16 +1,5 @@
 local F, C, L = unpack(select(2, ...))
 
-SlashCmdList.FRAMENAME = function() print(GetMouseFocus():GetName()) end
-SLASH_FRAMENAME1 = "/gn"
-
-SlashCmdList.GETPARENT = function() print(GetMouseFocus():GetParent():GetName()) end
-SLASH_GETPARENT1 = "/gp"
-
-SlashCmdList.DISABLE_ADDON = function(s) DisableAddOn(s) end
-SLASH_DISABLE_ADDON1 = "/dis"
-
-SlashCmdList.ENABLE_ADDON = function(s) EnableAddOn(s) end
-SLASH_ENABLE_ADDON1 = "/en"
 
 SlashCmdList.RELOADUI = ReloadUI
 SLASH_RELOADUI1 = "/rl"
@@ -25,62 +14,66 @@ SLASH_ROLECHECK2 = "/rolepoll"
 SlashCmdList.TICKET = ToggleHelpFrame
 SLASH_TICKET1 = "/gm"
 
-SlashCmdList.VOLUME = function(value)
-	local numValue = tonumber(value)
-	if numValue and 0 <= numValue and numValue <= 1 then
-		SetCVar("Sound_MasterVolume", numValue)
-	end
-end
-SLASH_VOLUME1 = "/vol"
-
-SlashCmdList.GPOINT = function(f)
-	if f ~= "" then
-		f = _G[f]
-	else
-		f = GetMouseFocus()
-	end
-
-	if f ~= nil then
-		local a1, p, a2, x, y = f:GetPoint()
-		print("|cffFFD100"..a1.."|r "..p:GetName().."|cffFFD100 "..a2.."|r "..x.." "..y)
-	end
-end
-
-SLASH_GPOINT1 = "/gpoint"
-
-SlashCmdList.FPS = function(value)
-	local numValue = tonumber(value)
-	if numValue and 0 <= numValue then
-		SetCVar("maxFPS", numValue)
-	end
-end
-SLASH_FPS1 = "/fps"
-
-SlashCmdList["INSTANCEID"] = function()
-	local name, _, _, _, _, _, _, id = GetInstanceInfo()
-	print(name, id)
-end
-SLASH_INSTANCEID1 = "/getid"
-
-SlashCmdList["NPCID"] = function()
-	local name = UnitName("target")
-	local guid = UnitGUID("target")
-		if name and guid then
-		local npcID = F.GetNPCID(guid)
-		print(name, C.InfoColor..(npcID or "nil"))
-	end
-end
-SLASH_NPCID1 = "/getnpc"
-
-SlashCmdList["GETFONT"] = function(msg)
-	local font = _G[msg]
-	if not font then print(msg, "not found.") return end
-	local a, b, c = font:GetFont()
-	print(msg,a,b,c)
-end
-SLASH_GETFONT1 = "/getfont"
-
 SlashCmdList["LEAVEPARTY"] = function()
 	LeaveParty();
 end
 SLASH_LEAVEPARTY1 = "/lg"
+
+
+
+
+SlashCmdList["INSTANCEID"] = function()
+	local name, instanceType, difficultyID, difficultyName, _, _, _, instanceMapID = GetInstanceInfo()
+	print(C.LineString)
+	print(C.InfoColor..'Name: '..C.RedColor..name)
+	print(C.InfoColor..'instanceType: '..C.RedColor..instanceType)
+	print(C.InfoColor..'difficultyID: '..C.RedColor..difficultyID)
+	print(C.InfoColor..'difficultyName: '..C.RedColor..difficultyName)
+	print(C.InfoColor..'instanceMapID: '..C.RedColor..instanceMapID)
+	print(C.LineString)
+end
+SLASH_INSTANCEID1 = "/getinstid"
+
+
+
+
+
+
+SlashCmdList.FREEUI = function(cmd)
+	--[[local cmd, args = strsplit(" ", cmd:lower(), 2)
+	if C.unitframes.enable then
+		if cmd == "dps" then
+			FreeUIConfig.layout = 1
+			ReloadUI()
+		elseif(cmd == "heal" or cmd == "healer") then
+			FreeUIConfig.layout = 2
+			ReloadUI()
+		end
+	end
+
+	if cmd == "install" then
+		if IsAddOnLoaded("FreeUI_Install") then
+			FreeUI_InstallFrame:Show()
+		else
+			EnableAddOn("FreeUI_Install")
+			LoadAddOn("FreeUI_Install")
+		end
+	elseif cmd == "reset" then
+		FreeUIGlobalConfig = {}
+		FreeUIConfig = {}
+		ReloadUI()
+	else
+		if not BankFrame:IsShown() and FreeUIOptionsPanel then
+			FreeUIOptionsPanel:Show()
+		end
+		DEFAULT_CHAT_FRAME:AddMessage("FreeUI |cffffffff"..GetAddOnMetadata("FreeUI", "Version"), unpack(C.class))
+		if C.unitframes.enable then
+			DEFAULT_CHAT_FRAME:AddMessage("|cffffffff/freeui|r [dps/healer]|cffffffff: Select a unitframe layout|r", unpack(C.class))
+		end
+		DEFAULT_CHAT_FRAME:AddMessage("|cffffffff/freeui|r install|cffffffff: Load the intaller|r", unpack(C.class))
+		DEFAULT_CHAT_FRAME:AddMessage("|cffffffff/freeui|r reset|cffffffff: Clear saved settings|r", unpack(C.class))
+	end]]
+
+	FreeUIOptionsPanel:Show()
+end
+SLASH_FREEUI1 = "/freeui"
