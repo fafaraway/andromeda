7550be56b85da9d8b9521576373a8a4ac10cccf3
solor <solor@users.noreply.github.com>
2019-01-14 09:43:42 +0800

    Get ready for options panel.

--- FreeUI/core/gui.lua
+++ FreeUI/core/gui.lua
@@ -1,3 +1,44 @@
 local F, C = unpack(select(2, ...))
 
+if not IsAddOnLoaded("FreeUI_Options") then return end
 
+local realm = GetRealmName()
+local name = UnitName("player")
+
+-- create the profile boolean
+if not FreeUIOptionsGlobal then FreeUIOptionsGlobal = {} end
+if FreeUIOptionsGlobal[realm] == nil then FreeUIOptionsGlobal[realm] = {} end
+if FreeUIOptionsGlobal[realm][name] == nil then FreeUIOptionsGlobal[realm][name] = false end
+
+-- create the main options table
+if FreeUIOptions == nil then FreeUIOptions = {} end
+
+-- determine which settings to use
+local profile
+if FreeUIOptionsGlobal[realm][name] == true then
+	if FreeUIOptionsPerChar == nil then
+		FreeUIOptionsPerChar = {}
+	end
+	profile = FreeUIOptionsPerChar
+else
+	profile = FreeUIOptions
+end
+
+-- apply or remove saved settings as needed
+for group, options in pairs(profile) do
+	if C[group] then
+		for option, value in pairs(options) do
+			if C[group][option] == nil or C[group][option] == value then
+				-- remove saved vars if they do not exist in lua config anymore, or are the same as the lua config
+				profile[group][option] = nil
+			else
+				C[group][option] = value
+			end
+		end
+	else
+		profile[group] = nil
+	end
+end
+
+-- add global options variable
+C.options = profile
