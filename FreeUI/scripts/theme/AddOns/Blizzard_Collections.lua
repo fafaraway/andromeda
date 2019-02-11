e9a0a962eb116ac4a0f3dd286870625b1ae71173
Solor <156806+Solor@users.noreply.github.com>
2019-02-11 20:30:43 +0800

    Minor fix.

--- FreeUI/scripts/theme/AddOns/Blizzard_Collections.lua
+++ FreeUI/scripts/theme/AddOns/Blizzard_Collections.lua
@@ -596,6 +596,7 @@ C.themes["Blizzard_Collections"] = function()
 	F.ReskinPortraitFrame(WardrobeFrame)
 	F.Reskin(WardrobeTransmogFrame.ApplyButton)
 	F.StripTextures(WardrobeTransmogFrame.SpecButton)
+	F.ReskinArrow(WardrobeTransmogFrame.SpecButton, "down")
 	WardrobeTransmogFrame.SpecButton:SetPoint("RIGHT", WardrobeTransmogFrame.ApplyButton, "LEFT", -3, 0)
 	for i = 1, 10 do
 		select(i, WardrobeTransmogFrame.Model.ClearAllPendingButton:GetRegions()):Hide()
