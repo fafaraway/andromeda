ea7a9165e16c31689f1147db069ed806c9350129
Solor <156806+Solor@users.noreply.github.com>
2019-03-07 07:14:22 +0800

    Skip azerite animation.

--- FreeUI/scripts/blizzard/blizzard.lua
+++ FreeUI/scripts/blizzard/blizzard.lua
@@ -12,6 +12,7 @@ function module:OnLogin()
 	self:PositionAlert()
 	self:RemoveTalkingHead()
 	self:RemoveBossBanner()
+	self:SkipAzeriteAnimation()
 end
 
 -- reposition alert popup
@@ -21,24 +22,24 @@ function module:PositionAlert()
 		_G.AlertFrame:ClearAllPoints()
 		_G.AlertFrame:SetPoint(unpack(C.general.alert_Position))
 	end
-	hooksecurefunc(_G.AlertFrame, "UpdateAnchors", alertFrameMover)
+	hooksecurefunc(_G.AlertFrame, 'UpdateAnchors', alertFrameMover)
 end
 
 -- remove talking head
 function module:RemoveTalkingHead()
-	local f = CreateFrame("Frame")
+	local f = CreateFrame('Frame')
 	function f:OnEvent(event, addon)
 		if C.general.hideTalkingHead then
-			if addon == "Blizzard_TalkingHeadUI" then
-				hooksecurefunc("TalkingHeadFrame_PlayCurrent", function()
+			if addon == 'Blizzard_TalkingHeadUI' then
+				hooksecurefunc('TalkingHeadFrame_PlayCurrent', function()
 					TalkingHeadFrame:Hide()
 				end)
 				self:UnregisterEvent(event)
 			end
 		end
 	end
-	f:RegisterEvent("ADDON_LOADED")
-	f:SetScript("OnEvent", f.OnEvent)
+	f:RegisterEvent('ADDON_LOADED')
+	f:SetScript('OnEvent', f.OnEvent)
 end
 
 -- Remove Boss Banner
@@ -50,21 +51,21 @@ end
 
 function module:PositionUIWidgets()
 	_G.UIWidgetTopCenterContainerFrame:ClearAllPoints()
-	_G.UIWidgetTopCenterContainerFrame:SetPoint("TOP", UIParent, "TOP", 0, -30)
+	_G.UIWidgetTopCenterContainerFrame:SetPoint('TOP', UIParent, 'TOP', 0, -30)
 	_G.UIWidgetTopCenterContainerFrame.ignoreFramePositionManager = true
 
-	local belowMiniMapcontainer = _G["UIWidgetBelowMinimapContainerFrame"]
+	local belowMiniMapcontainer = _G['UIWidgetBelowMinimapContainerFrame']
 
-	local belowMiniMapHolder = CreateFrame("Frame", "BelowMinimapContainerHolder", UIParent)
-	belowMiniMapHolder:SetPoint("TOP", UIParent, "TOP", 0, -120)
+	local belowMiniMapHolder = CreateFrame('Frame', 'BelowMinimapContainerHolder', UIParent)
+	belowMiniMapHolder:SetPoint('TOP', UIParent, 'TOP', 0, -120)
 	belowMiniMapHolder:SetSize(170, 20)
 
 	belowMiniMapcontainer:ClearAllPoints()
-	belowMiniMapcontainer:SetPoint("CENTER", belowMiniMapHolder, "CENTER")
+	belowMiniMapcontainer:SetPoint('CENTER', belowMiniMapHolder, 'CENTER')
 	belowMiniMapcontainer:SetParent(belowMiniMapHolder)
 	belowMiniMapcontainer.ignoreFramePositionManager = true
 
-	hooksecurefunc(_G["UIWidgetManager"].registeredWidgetSetContainers[2], "layoutFunc", function(widgetContainer, sortedWidgets, ...)
+	hooksecurefunc(_G['UIWidgetManager'].registeredWidgetSetContainers[2], 'layoutFunc', function(widgetContainer, sortedWidgets, ...)
 		widgetContainer:ClearAllPoints()
 
 		if widgetContainer:GetWidth() ~= belowMiniMapHolder:GetWidth() then
@@ -72,8 +73,8 @@ function module:PositionUIWidgets()
 		end
 	end)
 
-	hooksecurefunc(belowMiniMapcontainer, "ClearAllPoints", function(self)
-		self:SetPoint("CENTER", belowMiniMapHolder, "CENTER")
+	hooksecurefunc(belowMiniMapcontainer, 'ClearAllPoints', function(self)
+		self:SetPoint('CENTER', belowMiniMapHolder, 'CENTER')
 	end)
 end
 
@@ -82,10 +83,10 @@ end
 do
 	local function fixRaidGroupButton()
 		for i = 1, 40 do
-			local bu = _G["RaidGroupButton"..i]
+			local bu = _G['RaidGroupButton'..i]
 			if bu and bu.unit and not bu.clickFixed then
-				bu:SetAttribute("type", "target")
-				bu:SetAttribute("unit", bu.unit)
+				bu:SetAttribute('type', 'target')
+				bu:SetAttribute('unit', bu.unit)
 
 				bu.clickFixed = true
 			end
@@ -93,20 +94,35 @@ do
 	end
 
 	local EventFrame = CreateFrame( 'Frame' )
-	EventFrame:RegisterEvent("ADDON_LOADED")
-	EventFrame:SetScript("OnEvent", function(self, event, addon)
-		if event == "ADDON_LOADED" and addon == "Blizzard_RaidUI" then
+	EventFrame:RegisterEvent('ADDON_LOADED')
+	EventFrame:SetScript('OnEvent', function(self, event, addon)
+		if event == 'ADDON_LOADED' and addon == 'Blizzard_RaidUI' then
 			if not InCombatLockdown() then
 				fixRaidGroupButton()
 				self:UnregisterAllEvents()
 			else
-				self:RegisterEvent("PLAYER_REGEN_ENABLED")
+				self:RegisterEvent('PLAYER_REGEN_ENABLED')
 			end
-		elseif event == "PLAYER_REGEN_ENABLED" then
-			if RaidGroupButton1 and RaidGroupButton1:GetAttribute("type") ~= "target" then
+		elseif event == 'PLAYER_REGEN_ENABLED' then
+			if RaidGroupButton1 and RaidGroupButton1:GetAttribute('type') ~= 'target' then
 				fixRaidGroupButton()
 				self:UnregisterAllEvents()
 			end
+		end
+	end)
+end
+
+
+function module:SkipAzeriteAnimation()
+	if not (IsAddOnLoaded('Blizzard_AzeriteUI')) then
+    	UIParentLoadAddOn('Blizzard_AzeriteUI')
+    end
+	hooksecurefunc(AzeriteEmpoweredItemUI,'OnItemSet',function(self)
+		local itemLocation = self.azeriteItemDataSource:GetItemLocation()
+		if self:IsAnyTierRevealing() then
+			C_Timer.After(0.7,function() 
+				OpenAzeriteEmpoweredItemUIFromItemLocation(itemLocation)
+			end)
 		end
 	end)
 end
