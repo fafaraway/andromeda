8ca5605c3901b1c335a173a9d75ca79bd1e7408f
Solor <156806+Solor@users.noreply.github.com>
2020-08-22 04:41:26 +1000

    Update profiles.lua

--- FreeUI/core/profiles.lua
+++ FreeUI/core/profiles.lua
@@ -4,45 +4,54 @@ local F, C = unpack(select(2, ...))
 C.DevsList = {
 	['呂碧城-白银之手'] = true,
 }
+
 local function isDeveloper()
 	return C.DevsList[C.MyName..'-'..C.MyRealm]
 end
+
 C.isDeveloper = isDeveloper()
 
 
-if not C.isDeveloper then return end
-
-C.Actionbar.bar1_visibility = '[mod:shift][@vehicle,exists][overridebar][shapeshift][vehicleui][possessbar,@vehicle,exists] show; hide'
-C.Actionbar.bar2 = false
-C.Actionbar.bar3 = false
-
-C.Unitframe.player_hide_tags = true
-C.Unitframe.enable_pet = false
-C.Unitframe.player_height = 7
-C.Unitframe.target_height = 7
-
-
--- Override fonts
-C.Assets.Fonts.Normal = 'Fonts\\FreeUI\\sarasa_newjune_semibold.ttf'
-C.Assets.Fonts.Header = 'Fonts\\FreeUI\\header.ttf'
-C.Assets.Fonts.Chat   = 'Fonts\\FreeUI\\sarasa_newjune_semibold.ttf'
-C.Assets.Fonts.Number = 'Fonts\\FreeUI\\sarasa_tccc.ttf'
-
-STANDARD_TEXT_FONT = 'Fonts\\FreeUI\\sarasa_newjune_semibold.ttf'
-UNIT_NAME_FONT     = 'Fonts\\FreeUI\\header.ttf'
-DAMAGE_TEXT_FONT   = 'Fonts\\FreeUI\\damage.ttf'
-
-
--- disbale talent alert
-function MainMenuMicroButton_AreAlertsEffectivelyEnabled()
-	return false
+if C.isDeveloper then
+	C.Actionbar.bar1_visibility = '[mod:shift][@vehicle,exists][overridebar][shapeshift][vehicleui][possessbar,@vehicle,exists] show; hide'
 end
 
+if C.isDeveloper then
+	C.Assets.Fonts.Normal = 'Fonts\\FreeUI\\sarasa_newjune_semibold.ttf'
+	C.Assets.Fonts.Header = 'Fonts\\FreeUI\\庞门正道粗书体_猫啃网.ttf'
+	C.Assets.Fonts.Chat   = 'Fonts\\FreeUI\\sarasa_aleo_bold.ttf'
+	C.Assets.Fonts.Number = 'Fonts\\FreeUI\\sarasa_tccc_semibold.ttf'
+	C.Assets.Fonts.Combat = 'Fonts\\FreeUI\\combat.ttf'
+elseif C.Client == 'zhCN' then
+	C.Assets.Fonts.Normal = 'Fonts\\ARKai_T.ttf'
+	C.Assets.Fonts.Header = 'Fonts\\ARKai_T.ttf'
+	C.Assets.Fonts.Chat   = 'Fonts\\ARKai_T.ttf'
+	C.Assets.Fonts.Number = 'Fonts\\ARKai_T.ttf'
+	C.Assets.Fonts.Combat = 'Fonts\\ARKai_C.ttf'
+elseif C.Client == 'zhTW' then
+	C.Assets.Fonts.Normal = 'Fonts\\blei00d.ttf'
+	C.Assets.Fonts.Header = 'Fonts\\blei00d.ttf'
+	C.Assets.Fonts.Chat   = 'Fonts\\blei00d.ttf'
+	C.Assets.Fonts.Number = 'Fonts\\blei00d.ttf'
+	C.Assets.Fonts.Combat = 'Fonts\\bKAI00M.ttf'
+elseif C.Client == 'koKR' then
+	C.Assets.Fonts.Normal = 'Fonts\\2002.ttf'
+	C.Assets.Fonts.Header = 'Fonts\\2002B.ttf'
+	C.Assets.Fonts.Chat   = 'Fonts\\2002.ttf'
+	C.Assets.Fonts.Number = 'Fonts\\2002.ttf'
+	C.Assets.Fonts.Combat = 'Fonts\\K_Damage.ttf'
+elseif C.Client == 'ruRU' then
+	C.Assets.Fonts.Normal = 'Fonts\\FRIZQT___CYR.ttf'
+	C.Assets.Fonts.Header = 'Fonts\\FRIZQT___CYR.ttf'
+	C.Assets.Fonts.Chat   = 'Fonts\\FRIZQT___CYR.ttf'
+	C.Assets.Fonts.Number = 'Fonts\\FRIZQT___CYR.ttf'
+	C.Assets.Fonts.Combat = 'Fonts\\FRIZQT___CYR.ttf'
+end
+
+
+
+
 F:RegisterEvent('PLAYER_ENTERING_WORLD', function()
-	--[[ F.Print('C.ScreenWidth '..C.ScreenWidth)
-	F.Print('C.ScreenHeight '..C.ScreenHeight)
-	F.Print('C.Mult '..C.Mult)
-	F.Print('uiScale '..C.General.uiScale)
-	F.Print('UIParentScale '..UIParent:GetScale()) ]]
+
 end)
 
