e5d04ef438d5db69c888134cb092dbd44cc9a690
Solor <156806+Solor@users.noreply.github.com>
2020-08-23 08:53:04 +1000

    Update configs.lua

--- FreeUI/core/configs.lua
+++ FreeUI/core/configs.lua
@@ -107,7 +107,7 @@ C['Announcement'] = {
 		['special_toy'] = true,
 }
 
-C['Infobar'] = {
+--[[ C['Infobar'] = {
 	['enable'] = true,
 		['fontSize'] = 11,
 		['top'] = true,
@@ -119,7 +119,7 @@ C['Infobar'] = {
 		['guild'] = true,
 		['friends'] = true,
 		['report'] = true,
-}
+} ]]
 
 C['Chat'] = {
 	['enable'] = true,
@@ -486,76 +486,68 @@ C['ReminderBuffs'] = {
 	},
 }
 
+
 C['CornerBuffs'] = {
-	ALL = {},
-
-	PRIEST = {
-		{194384, 'TOPRIGHT',    {1, 1, 0.66}},              -- Atonement
-		{214206, 'TOPRIGHT',    {1, 1, 0.66}},              -- Atonement (PvP)
-		{ 41635, 'BOTTOMRIGHT', {0.2, 0.7, 0.2}},           -- Prayer of Mending
-		{193065, 'BOTTOMRIGHT', {0.54, 0.21, 0.78}},        -- Masochism
-		{   139, 'BOTTOMLEFT',  {0.4, 0.7, 0.2}},           -- Renew
-		{    17, 'TOPLEFT',     {0.7, 0.7, 0.7}, true},     -- Power Word: Shield
-		{ 47788, 'LEFT',        {0.86, 0.45, 0}, true},     -- Guardian Spirit
-		{ 33206, 'LEFT',        {0.47, 0.35, 0.74}, true},  -- Pain Suppression
+	['PRIEST'] = {
+		[194384] = {'TOPRIGHT', {1, 1, .66}},			-- 救赎
+		[214206] = {'TOPRIGHT', {1, 1, .66}},			-- 救赎(PvP)
+		[41635]  = {'BOTTOMRIGHT', {.2, .7, .2}},		-- 愈合导言
+		[193065] = {'BOTTOMRIGHT', {.54, .21, .78}},	-- 忍辱负重
+		[139]    = {'TOPLEFT', {.4, .7, .2}},			-- 恢复
+		[17]     = {'TOPLEFT', {.7, .7, .7}},			-- 真言术盾
+		[47788]  = {'LEFT', {.86, .45, 0}, true},		-- 守护之魂
+		[33206]  = {'LEFT', {.47, .35, .74}, true},		-- 痛苦压制
+		[6788]  = {'TOP', {.86, .11, .11}, true},		-- 虚弱灵魂
 	},
-
-	DRUID = {
-		{   774, 'TOPRIGHT',    {0.8, 0.4, 0.8}},   		-- Rejuvenation
-		{155777, 'RIGHT',       {0.8, 0.4, 0.8}},   		-- Germination
-		{  8936, 'BOTTOMLEFT',  {0.2, 0.8, 0.2}},		    -- Regrowth
-		{ 33763, 'TOPLEFT',     {0.4, 0.8, 0.2}},  		    -- Lifebloom
-		{ 48438, 'BOTTOMRIGHT', {0.8, 0.4, 0}},		        -- Wild Growth
-		{207386, 'TOP',         {0.4, 0.2, 0.8}},     		-- Spring Blossoms
-		{102351, 'LEFT',        {0.2, 0.8, 0.8}},    		-- Cenarion Ward (Initial Buff)
-		{102352, 'LEFT',        {0.2, 0.8, 0.8}},    		-- Cenarion Ward (HoT)
-		{200389, 'BOTTOM',      {1, 1, 0.4}},      		    -- Cultivation
+	['DRUID'] = {
+		[774]    = {'TOPRIGHT', {.8, .4, .8}},			-- 回春
+		[155777] = {'RIGHT', {.8, .4, .8}},				-- 萌芽
+		[8936]   = {'BOTTOMLEFT', {.2, .8, .2}},		-- 愈合
+		[33763]  = {'TOPLEFT', {.4, .8, .2}},			-- 生命绽放
+		[48438]  = {'BOTTOMRIGHT', {.8, .4, 0}},		-- 野性成长
+		[207386] = {'TOP', {.4, .2, .8}},				-- 春暖花开
+		[102351] = {'LEFT', {.2, .8, .8}},				-- 结界
+		[102352] = {'LEFT', {.2, .8, .8}},				-- 结界(HoT)
+		[200389] = {'BOTTOM', {1, 1, .4}},				-- 栽培
 	},
-
-	PALADIN = {
-		{ 53563, 'TOPRIGHT',    {0.7, 0.3, 0.7}},           -- Beacon of Light
-		{156910, 'TOPRIGHT',    {0.7, 0.3, 0.7}},           -- Beacon of Faith
-		{200025, 'TOPRIGHT',    {0.7, 0.3, 0.7}},           -- Beacon of Virtue
-		{  1022, 'BOTTOMRIGHT', {0.2, 0.2, 1}, true},       -- Hand of Protection
-		{  1044, 'BOTTOMRIGHT', {0.89, 0.45, 0}, true},     -- Hand of Freedom
-		{  6940, 'BOTTOMRIGHT', {0.89, 0.1, 0.1}, true},    -- Hand of Sacrifice
-		{223306, 'BOTTOMLEFT',  {0.7, 0.7, 0.3}},           -- Bestow Faith
+	['PALADIN'] = {
+		[287280]  = {'TOPLEFT', {1, .8, 0}},			-- 圣光闪烁
+		[53563]  = {'TOPRIGHT', {.7, .3, .7}},			-- 道标
+		[156910] = {'TOPRIGHT', {.7, .3, .7}},			-- 信仰道标
+		[200025] = {'TOPRIGHT', {.7, .3, .7}},			-- 美德道标
+		[1022]   = {'BOTTOMRIGHT', {.2, .2, 1}, true},	-- 保护
+		[1044]   = {'BOTTOMRIGHT', {.89, .45, 0}, true},-- 自由
+		[6940]   = {'BOTTOMRIGHT', {.89, .1, .1}, true},-- 牺牲
+		[223306] = {'BOTTOMLEFT', {.7, .7, .3}},		-- 赋予信仰
+		[25771]  = {'TOP', {.86, .11, .11}, true},		-- 自律
 	},
-
-	SHAMAN = {
-		{ 61295, 'TOPRIGHT',    {0.7, 0.3, 0.7}},   	    -- Riptide
-		{   974, 'BOTTOMRIGHT', {0.2, 0.2, 1}}, 	        -- Earth Shield
-		{207400, 'BOTTOMLEFT',  {0.6, 0.8, 1}}, 	        -- 先祖活力
+	['SHAMAN'] = {
+		[61295]  = {'TOPRIGHT', {.2, .8, .8}},			-- 激流
+		[974]    = {'BOTTOMRIGHT', {1, .8, 0}},			-- 大地之盾
+		[207400] = {'BOTTOMLEFT', {.6, .8, 1}},			-- 先祖活力
 	},
-
-	MONK = {
-		{119611, 'TOPLEFT',     {0.3, 0.8, 0.6}},           -- Renewing Mist
-		{116849, 'TOPRIGHT',    {0.2, 0.8, 0.2}, true},     -- Life Cocoon
-		{124682, 'BOTTOMLEFT',  {0.8, 0.8, 0.25}},          -- Enveloping Mist
-		{191840, 'BOTTOMRIGHT', {0.27, 0.62, 0.7}},         -- Essence Font
+	['MONK'] = {
+		[119611] = {'TOPLEFT', {.3, .8, .6}},			-- 复苏之雾
+		[116849] = {'TOPRIGHT', {.2, .8, .2}, true},	-- 作茧缚命
+		[124682] = {'BOTTOMLEFT', {.8, .8, .25}},		-- 氤氲之雾
+		[191840] = {'BOTTOMRIGHT', {.27, .62, .7}},		-- 精华之泉
 	},
-
-	ROGUE = {
-		{ 57934, 'TOPRIGHT',    {0.89, 0.09, 0.05}},        -- Tricks of the Trade
+	['ROGUE'] = {
+		[57934]  = {'BOTTOMRIGHT', {.9, .1, .1}},		-- 嫁祸
 	},
-
-	WARRIOR = {
-		{114030, 'TOPLEFT',    	{0.2, 0.2, 1}},     	    -- Vigilance
-		{122506, 'TOPRIGHT',   	{0.89, 0.09, 0.05}}, 	    -- Intervene
+	['WARRIOR'] = {
+		[114030] = {'TOPLEFT', {.2, .2, 1}},			-- 警戒
 	},
-
-	WARLOCK = {
-		{ 20707, 'BOTTOMRIGHT',	{0.8, 0.4, 0.8}},     	    -- Soul Stone
+	['HUNTER'] = {
+		[34477]  = {'BOTTOMRIGHT', {.9, .1, .1}},		-- 误导
+		[90361]  = {'TOPLEFT', {.4, .8, .2}},			-- 灵魂治愈
 	},
-
-	HUNTER = {
-		{ 34477, 'BOTTOMRIGHT', {.9, .1, .1}},		        -- 误导
-		{ 90361, 'TOPLEFT',     {.4, .8, .2}},			    -- 灵魂治愈
+	['WARLOCK'] = {
+		[20707]  = {'BOTTOMRIGHT', {.8, .4, .8}, true},	-- 灵魂石
 	},
-
-	DEMONHUNTER = {},
-	MAGE = {},
-	DEATHKNIGHT = {},
+	['DEMONHUNTER'] = {},
+	['MAGE'] = {},
+	['DEATHKNIGHT'] = {},
 }
 
 C['ClassBuffs'] = {
@@ -726,6 +718,61 @@ C['RaidDebuffs'] = {}
 
 
 
+C.PartySpells = {
+	[57994]  = 12,	-- 风剪
+	[1766]   = 15,	-- 脚踢
+	[6552]   = 15,	-- 拳击
+	[47528]  = 15,	-- 心灵冰冻
+	[96231]  = 15,	-- 责难
+	[106839] = 15,	-- 迎头痛击
+	[116705] = 15,	-- 切喉手
+	[183752] = 15,	-- 瓦解
+	[187707] = 15,	-- 压制
+	[2139]   = 24,	-- 法术反制
+	[147362] = 24,	-- 反制射击
+	[15487]  = 45,	-- 沉默
+	[109248] = 45,	-- 束缚射击
+	[78675]  = 60,	-- 日光术
+
+	[8143]	 = 60,	-- 战栗图腾
+	[102342] = 60,	-- 铁木树皮
+	[102793] = 60,	-- 乌索尔旋风
+	[119381] = 60,	-- 扫堂腿
+	[179057] = 60,	-- 混乱新星
+	[205636] = 60,	-- 树人
+	[31224]  = 120,	-- 暗影斗篷
+	[190319] = 120,	-- 燃烧
+	[25046]  = 120,	-- 奥术洪流
+	[28730]  = 120,
+	[50613]  = 120,
+	[69179]  = 120,
+	[80483]  = 120,
+	[129597] = 120,
+	[155145] = 120,
+	[202719] = 120,
+	[232633] = 120,
+	[186265] = 180,	-- 灵龟守护
+}
+
+C.TalentCDFix = {
+	[740]	 = 120,	-- 宁静
+	[2094]   = 90,	-- 致盲
+	[15286]  = 75,	-- 吸血鬼的拥抱
+	[15487]  = 30,	-- 沉默
+	[22812]  = 40,	-- 树皮术
+	[30283]  = 30,	-- 暗怒
+	[48792]  = 165,	-- 冰封之韧
+	[79206]  = 60,	-- 灵魂行者的恩赐
+	[102342] = 45,	-- 铁木树皮
+	[108199] = 90,	-- 血魔之握
+	[109304] = 105,	-- 意气风发
+	[116849] = 100,	-- 作茧缚命
+	[119381] = 40,	-- 扫堂腿
+	[179057] = 40,	-- 混乱新星
+}
+
+
+
 
 C.CharacterSettings = {
 	['BfA'] = false,
@@ -793,11 +840,10 @@ C.CharacterSettings = {
 		['cursor_trail'] = true,
 		['vignetting'] = true,
 			['vignetting_alpha'] = .8,
-
-		['backdrop_color'] = {.05, .05, .05},
+		['adjust_font'] = true,
+		['backdrop_color'] = {.03, .03, .03},
 		['backdrop_alpha'] = .7,
-		['backdrop_border_color'] = {0, 0, 0},
-		['backdrop_border_alpha'] = 1,
+
 		['flat_style'] = false,
 			['flat_color'] = {.06, .06, .06},
 			['flat_alpha'] = .65,
@@ -959,7 +1005,9 @@ C.CharacterSettings = {
 				['group_click_cast_filter'] = false,
 				['group_debuff_highlight'] = true,
 				['group_corner_buffs'] = true,
-
+				['group_debuffs'] = true,
+				['party_spell_watcher'] = true,
+				['party_spell_sync'] = true,
 
 				['group_threat_indicator'] = true,
 				['group_leader_indicator'] = true,
@@ -969,18 +1017,15 @@ C.CharacterSettings = {
 				['group_ready_check_indicator'] = true,
 				['group_resurrect_indicator'] = true,
 
-
 				['party_width'] = 90,
 				['party_height'] = 38,
 				['party_gap'] = 6,
-				['party_buffs'] = true,
-				['party_debuffs'] = true,
 
 				['raid_width'] = 44,
 				['raid_height'] = 32,
 				['raid_gap'] = 5,
-				['raid_debuffs'] = true,
-				['raid_debuffs_click_through'] = false,
+
+
 
 			['enable_boss'] = true,
 				['boss_color_smooth'] = true,
@@ -1039,6 +1084,19 @@ C.CharacterSettings = {
 			['world_marker'] = true,
 			['micro_menu'] = true,
 			['progress_bar'] = true,
+	},
+
+	['infobar'] = {
+		['enable_infobar'] = true,
+			['anchor_top'] = true,
+			['bar_height'] = 14,
+			['mouseover'] = true,
+			['stats'] = true,
+			['spec'] = true,
+			['durability'] = true,
+			['guild'] = true,
+			['friends'] = true,
+			['report'] = true,
 	}
 }
 
