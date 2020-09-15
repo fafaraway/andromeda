bdd836d062d1002c6c50fbc577f29119578c8441
Solor <156806+Solor@users.noreply.github.com>
2020-09-16 01:14:32 +1000

    More fade condition for actionbar
    arena and instance

--- FreeUI/modules/actionbar/fader.lua
+++ FreeUI/modules/actionbar/fader.lua
@@ -7,6 +7,9 @@ local SpellFlyout = SpellFlyout
 
 
 local function CheckCondition(frame)
+	if (frame.faderConfig.arena and (GetZonePVPInfo() == 'arena')) then return true end
+	if (frame.faderConfig.instance and (IsInInstance() == true)) then return true end
+
 	if (frame.faderConfig.combat and UnitAffectingCombat('player')) then return true end
 	if (frame.faderConfig.target and UnitExists('target')) then return true end
 	if (frame.faderConfig.hover and MouseIsOver(frame)) then return true end
@@ -22,19 +25,19 @@ local function FrameHandler(frame)
 	if not frame.faderConfig.enable then return end
 
 	if CheckCondition(frame) then
-		F:UIFrameFadeIn(frame, FreeDB.actionbar.fade_duration, frame:GetAlpha(), frame.faderConfig.fadeInAlpha or 1)
+		if FreeDB.actionbar.fade_smooth then
+			F:UIFrameFadeIn(frame, FreeDB.actionbar.fade_in_duration, frame:GetAlpha(), frame.faderConfig.fadeInAlpha or 1)
+		else
+			frame:SetAlpha(frame.faderConfig.fadeInAlpha or 1)
+		end
 
-		-- for _, button in pairs(buttonsList) do
-		-- 	button:EnableMouse(true)
-		-- end
 
 	else
-		F:UIFrameFadeOut(frame, FreeDB.actionbar.fade_duration, frame:GetAlpha(), frame.faderConfig.fadeOutAlpha or 0)
-
-		-- for _, button in pairs(buttonsList) do
-		-- 	button:EnableMouse(false)
-		-- end
-
+		if FreeDB.actionbar.fade_smooth then
+			F:UIFrameFadeOut(frame, FreeDB.actionbar.fade_out_duration, frame:GetAlpha(), frame.faderConfig.fadeOutAlpha or 0)
+		else
+			frame:SetAlpha(frame.faderConfig.fadeOutAlpha or 1)
+		end
 	end
 end
 
@@ -108,4 +111,5 @@ end
 F:RegisterEvent('PLAYER_REGEN_ENABLED', UpdateFader)
 F:RegisterEvent('PLAYER_REGEN_DISABLED', UpdateFader)
 F:RegisterEvent('PLAYER_TARGET_CHANGED', UpdateFader)
-F:RegisterEvent('PLAYER_FOCUS_CHANGED', UpdateFader)
+F:RegisterEvent('PLAYER_ENTERING_WORLD', UpdateFader)
+F:RegisterEvent('ZONE_CHANGED_NEW_AREA', UpdateFader)
