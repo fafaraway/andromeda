5e398921666d68e0860b31d262eb302d5b2f7d75
Solor <156806+Solor@users.noreply.github.com>
2020-11-09 10:32:07 +1100

    Cleanup

--- FreeUI/modules/chat/chat.lua
+++ FreeUI/modules/chat/chat.lua
@@ -379,6 +379,39 @@ function CHAT:WhisperInvite()
 	F:RegisterEvent('CHAT_MSG_BN_WHISPER', CHAT.OnChatWhisper)
 end
 
+-- Whisper sound
+local lastSoundTimer = 0
+function CHAT:WhisperAlert()
+	if not C.DB.chat.whisper_sound then return end
+
+	local f = CreateFrame('Frame')
+	f:RegisterEvent('CHAT_MSG_WHISPER')
+	f:RegisterEvent('CHAT_MSG_BN_WHISPER')
+	f:HookScript('OnEvent', function(self, event, msg, ...)
+		local currentTime = GetServerTime()
+		if currentTime and currentTime - lastSoundTimer > C.DB.chat.sound_timer then
+			lastSoundTimer = currentTime
+			if event == 'CHAT_MSG_WHISPER' then
+				PlaySoundFile(C.Assets.Sounds.whisper, 'Master')
+			elseif event == 'CHAT_MSG_BN_WHISPER' then
+				PlaySoundFile(C.Assets.Sounds.whisperBN, 'Master')
+			end
+		end
+	end)
+end
+
+-- Whisper sticky
+function CHAT:WhisperSticky()
+	if C.DB.chat.whisper_sticky then
+		ChatTypeInfo['WHISPER'].sticky = 1
+		ChatTypeInfo['BN_WHISPER'].sticky = 1
+	else
+		ChatTypeInfo['WHISPER'].sticky = 0
+		ChatTypeInfo['BN_WHISPER'].sticky = 0
+	end
+end
+
+-- (、) -> (/)
 function CHAT:PauseToSlash()
 	hooksecurefunc('ChatEdit_OnTextChanged', function(self, userInput)
 		local text = self:GetText()
