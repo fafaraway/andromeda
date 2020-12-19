ed83f5789f7588f57eb8890eab67befdf5e43cec
Solor <156806+Solor@users.noreply.github.com>
2020-12-19 19:21:04 +0800

    Improve talent frame

--- modules/blizzard/talent.lua
+++ modules/blizzard/talent.lua
@@ -15,17 +15,12 @@ local Dropdown = LibStub('LibDropDown'):NewMenu(Talentless)
 Dropdown:SetStyle('MENU')
 
 function Talentless:PLAYER_LEVEL_UP(level)
-	if (level == 51) then
-		table.remove(self.Items[1].items, 1)
-		table.remove(self.Items[1].items, 1)
-		table.insert(self.Items[1].items, 173048)
-		table.remove(self.Items[2].items, 1)
-		table.remove(self.Items[2].items, 1)
-		table.remove(self.Items[2].items, 1)
-		table.insert(self.Items[2].items, 173049)
-	elseif (level == 60) then
-		table.remove(self.Items[1].items, 1)
-		table.remove(self.Items[2].items, 1)
+	local maxLevel = GetRestrictedAccountData()
+	if (maxLevel == 0) then
+		maxLevel = GetMaxLevelForPlayerExpansion()
+	end
+
+	if (UnitLevel('player') == maxLevel) then
 		self:UnregisterEvent('PLAYER_LEVEL_UP')
 	end
 
@@ -214,37 +209,22 @@ function Talentless:CreateItemButtons()
 
 	local items = {
 		{
-			141641, -- Codex of the Clear Mind (Pre-Legion, 10-50)
-			141333, -- Codex of the Tranquil Mind (Legion, 10-50)
-			153646 -- Codex of the Quiet Mind (BfA, 10-59)
-			--173048, -- Codex of the Still Mind (Shadowlands, 51-60)
+			{itemID = 143780, min = 10, max = 50}, -- Tome of the Tranquil Mind (BoP version)
+			{itemID = 143785, min = 10, max = 50}, -- Tome of the Tranquil Mind (BoP version)
+			{itemID = 141446, min = 10, max = 50}, -- Tome of the Tranquil Mind
+			{itemID = 141640, min = 10, max = 50}, -- Tome of the Clear Mind
+			{itemID = 153647, min = 10, max = 59}, -- Tome of the Quiet Mind
+			{itemID = 173049, min = 51, max = 999} -- Tome of the Still Mind
 		},
 		{
-			141640, -- Tome of the Clear Mind (Pre-Legion, 10-50)
-			143785, -- Tome of the Tranquil Mind (BoP version, 10-50)
-			141446, -- Tome of the Tranquil Mind (Legion, 10-50)
-			153647 -- Tome of the Quiet Mind (BfA, 10-59)
-			--173049, -- Tome of the Still Mind (Shadowlands, 51-60)
+			{itemID = 141333, min = 10, max = 50}, -- Codex of the Tranquil Mind
+			{itemID = 141641, min = 10, max = 50}, -- Codex of the Clear Mind
+			{itemID = 153646, min = 10, max = 59}, -- Codex of the Quiet Mind
+			{itemID = 173048, min = 51, max = 999} -- Codex of the Still Mind
 		}
 	}
 
-	local playerLevel = UnitLevel('player')
-	if (playerLevel > 50) then
-		table.remove(items[1], 1)
-		table.remove(items[1], 1)
-		table.insert(items[1], 173048)
-		table.remove(items[2], 1)
-		table.remove(items[2], 1)
-		table.remove(items[2], 1)
-		table.insert(items[2], 173049)
-
-		if (playerLevel > 59) then
-			table.remove(items[1], 1)
-			table.remove(items[2], 1)
-		end
-	end
-
-	for index, items in next, items do
+	for index, info in next, items do
 		local Button = CreateFrame('Button', '$parentItemButton' .. index, self, 'SecureActionButtonTemplate, ActionBarButtonSpellActivationAlert')
 		Button:SetPoint('TOPRIGHT', PlayerTalentFrame, -140 - (40 * (index - 1)), -25)
 		Button:SetSize(34, 34)
@@ -252,12 +232,13 @@ function Talentless:CreateItemButtons()
 		Button:SetScript('OnEnter', OnEnter)
 		Button:SetScript('OnEvent', OnEvent)
 		Button:SetScript('OnLeave', GameTooltip_Hide)
-		Button.items = items
+		Button.items = info
 
 		local Icon = Button:CreateTexture('$parentIcon', 'OVERLAY')
 		Icon:SetAllPoints()
 		Icon:SetTexture(index == 1 and 1495827 or 134915)
 		Icon:SetTexCoord(unpack(C.TexCoord))
+		Button.Icon = Icon
 
 		-- local Normal = Button:CreateTexture('$parentNormalTexture')
 		-- Normal:SetPoint('CENTER')
@@ -312,14 +293,21 @@ function Talentless:UpdateDropdown(spec)
 end
 
 function Talentless:GetAvailableItemInfo(index)
-	for _, itemID in next, self.Items[index].items do
-		local itemCount = GetItemCount(itemID)
-		if (itemCount > 0) then
-			return itemID, itemCount
+	local playerLevel = UnitLevel('player')
+	local bestItemID
+
+	for _, info in next, self.Items[index].items do
+		if (playerLevel >= info.min and playerLevel <= info.max) then
+			local itemCount = GetItemCount(info.itemID)
+			if (itemCount > 0) then
+				return info.itemID, itemCount
+			else
+				bestItemID = info.itemID
+			end
 		end
 	end
 
-	return self.Items[index].items[1], 0
+	return bestItemID, 0
 end
 
 function Talentless:UpdateItems()
@@ -343,6 +331,7 @@ function Talentless:UpdateItems()
 			end
 		end
 
+		Button.Icon:SetTexture(C_Item.GetItemIconByID(itemID))
 		Button.Count:SetText(itemCount)
 	end
 
@@ -383,7 +372,12 @@ function Talentless:ADDON_LOADED(addon)
 		hooksecurefunc(C_EquipmentSet, 'AssignSpecToEquipmentSet', UpdateAssignedEquipmentSets)
 		hooksecurefunc(C_EquipmentSet, 'UnassignEquipmentSetSpec', UpdateAssignedEquipmentSets)
 
-		if (UnitLevel('player') < 60 and not (IsTrialAccount() or IsVeteranTrialAccount())) then
+		local maxLevel = GetRestrictedAccountData()
+		if (maxLevel == 0) then
+			maxLevel = GetMaxLevelForPlayerExpansion()
+		end
+
+		if (UnitLevel('player') < maxLevel) then
 			self:RegisterEvent('PLAYER_LEVEL_UP')
 		end
 
