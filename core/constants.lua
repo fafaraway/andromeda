e1f7132258828e77de17d1cdd60f266f98a4d369
Solor <156806+Solor@users.noreply.github.com>
2021-02-09 09:08:14 +1100

    [Core]Clean up constans.

--- core/constants.lua
+++ core/constants.lua
@@ -23,12 +23,14 @@ local GetSpecialization = GetSpecialization
 local GetSpecializationInfo = GetSpecializationInfo
 local LOCALIZED_CLASS_NAMES_MALE = LOCALIZED_CLASS_NAMES_MALE
 local RAID_CLASS_COLORS = RAID_CLASS_COLORS
-local COMBATLOG_OBJECT_AFFILIATION_MINE = COMBATLOG_OBJECT_AFFILIATION_MINE
+local COMBATLOG_OBJECT_AFFILIATION_MINE = COMBATLOG_OBJECT_AFFILIATION_MINE or 0x00000001
 local COMBATLOG_OBJECT_AFFILIATION_PARTY = COMBATLOG_OBJECT_AFFILIATION_PARTY
 local COMBATLOG_OBJECT_REACTION_FRIENDLY = COMBATLOG_OBJECT_REACTION_FRIENDLY
 local COMBATLOG_OBJECT_CONTROL_PLAYER = COMBATLOG_OBJECT_CONTROL_PLAYER
 local COMBATLOG_OBJECT_TYPE_PET = COMBATLOG_OBJECT_TYPE_PET
 local COMBATLOG_OBJECT_AFFILIATION_RAID = COMBATLOG_OBJECT_AFFILIATION_RAID
+local BAG_ITEM_QUALITY_COLORS = BAG_ITEM_QUALITY_COLORS
+
 
 C.MyClass = select(2, UnitClass('player'))
 C.MyName = UnitName('player')
@@ -64,7 +66,7 @@ C.BackdropColor = {.1, .1, .1}
 C.BorderColor = {.04, .04, .04}
 C.GradientColor = {.04, .04, .04, .4, .08, .08, .08, .4}
 
-C['Assets'] = {
+C.Assets = {
     ['norm_tex'] = C.AssetsPath .. 'textures\\norm_tex',
     ['grad_tex'] = C.AssetsPath .. 'textures\\grad_tex',
     ['flat_tex'] = C.AssetsPath .. 'textures\\flat_tex',
@@ -98,26 +100,30 @@ C['Assets'] = {
     ['mouse_right'] = ' |TInterface\\TUTORIALFRAME\\UI-TUTORIAL-FRAME:13:11:0:-1:512:512:12:66:333:410|t ',
     ['mouse_middle'] = ' |TInterface\\TUTORIALFRAME\\UI-TUTORIAL-FRAME:13:11:0:-1:512:512:12:66:127:204|t ',
 
-	['Sounds'] = {
-        ['whisper'] = C.AssetsPath .. 'sounds\\whisper_normal.ogg',
-        ['whisperBN'] = C.AssetsPath .. 'sounds\\whisper_bn.ogg',
-        ['notification'] = C.AssetsPath .. 'sounds\\notification.ogg',
-        ['feast'] = C.AssetsPath .. 'sounds\\feast.ogg',
-        ['health'] = C.AssetsPath .. 'sounds\\health.ogg',
-        ['mana'] = C.AssetsPath .. 'sounds\\mana.ogg',
-        ['interrupt'] = C.AssetsPath .. 'sounds\\interrupt.ogg',
-        ['dispel'] = C.AssetsPath .. 'sounds\\dispel.ogg'
+    Textures = {
+
     },
 
-	['Fonts'] = {
-        ['Regular'] = C.AssetsPath .. 'fonts\\regular.ttf',
-        ['Condensed'] = C.AssetsPath .. 'fonts\\condensed.ttf',
-        ['Bold'] = C.AssetsPath .. 'fonts\\bold.ttf',
-        ['Header'] = C.AssetsPath .. 'fonts\\header.ttf',
-        ['Combat'] = C.AssetsPath .. 'fonts\\combat.ttf',
-        ['Pixel'] = C.AssetsPath .. 'fonts\\pixel.ttf',
-        ['Square'] = C.AssetsPath .. 'fonts\\square.ttf',
-        ['Roadway'] = C.AssetsPath .. 'fonts\\roadway.ttf'
+	Sounds = {
+        whisper      = C.AssetsPath .. 'sounds\\whisper_normal.ogg',
+        whisperBN    = C.AssetsPath .. 'sounds\\whisper_bn.ogg',
+        notification = C.AssetsPath .. 'sounds\\notification.ogg',
+        feast        = C.AssetsPath .. 'sounds\\feast.ogg',
+        health       = C.AssetsPath .. 'sounds\\health.ogg',
+        mana         = C.AssetsPath .. 'sounds\\mana.ogg',
+        interrupt    = C.AssetsPath .. 'sounds\\interrupt.ogg',
+        dispel       = C.AssetsPath .. 'sounds\\dispel.ogg'
+    },
+
+	Fonts = {
+        Regular   = C.AssetsPath .. 'fonts\\regular.ttf',
+        Condensed = C.AssetsPath .. 'fonts\\condensed.ttf',
+        Bold      = C.AssetsPath .. 'fonts\\bold.ttf',
+        Header    = C.AssetsPath .. 'fonts\\header.ttf',
+        Combat    = C.AssetsPath .. 'fonts\\combat.ttf',
+        Pixel     = C.AssetsPath .. 'fonts\\pixel.ttf',
+        Square    = C.AssetsPath .. 'fonts\\square.ttf',
+        Roadway   = C.AssetsPath .. 'fonts\\roadway.ttf'
     }
 }
 
@@ -177,34 +183,34 @@ C.QualityColors[-1] = {r = 0, g = 0, b = 0}
 C.QualityColors[LE_ITEM_QUALITY_POOR] = {r = .61, g = .61, b = .61}
 C.QualityColors[LE_ITEM_QUALITY_COMMON] = {r = 0, g = 0, b = 0}
 
-_G.GOLD_AMOUNT_SYMBOL = format('|cffffd700%s|r', GOLD_AMOUNT_SYMBOL)
-_G.SILVER_AMOUNT_SYMBOL = format('|cffd0d0d0%s|r', SILVER_AMOUNT_SYMBOL)
-_G.COPPER_AMOUNT_SYMBOL = format('|cffc77050%s|r', COPPER_AMOUNT_SYMBOL)
-_G.COPPER_AMOUNT = '%d\124TInterface\\MoneyFrame\\UI-CopperIcon:0:0:2:0\124t'
-_G.SILVER_AMOUNT = '%d\124TInterface\\MoneyFrame\\UI-SilverIcon:0:0:2:0\124t'
-_G.GOLD_AMOUNT = '%d\124TInterface\\MoneyFrame\\UI-GoldIcon:0:0:2:0\124t'
+GOLD_AMOUNT_SYMBOL = format('|cffffd700%s|r', GOLD_AMOUNT_SYMBOL)
+SILVER_AMOUNT_SYMBOL = format('|cffd0d0d0%s|r', SILVER_AMOUNT_SYMBOL)
+COPPER_AMOUNT_SYMBOL = format('|cffc77050%s|r', COPPER_AMOUNT_SYMBOL)
+COPPER_AMOUNT = '%d\124TInterface\\MoneyFrame\\UI-CopperIcon:0:0:2:0\124t'
+SILVER_AMOUNT = '%d\124TInterface\\MoneyFrame\\UI-SilverIcon:0:0:2:0\124t'
+GOLD_AMOUNT = '%d\124TInterface\\MoneyFrame\\UI-GoldIcon:0:0:2:0\124t'
 
 -- RoleUpdater
-local function checkRole()
+local function checkMyRole()
     local tree = GetSpecialization()
     if not tree then
         return
     end
-    local _, _, _, _, role, stat = GetSpecializationInfo(tree)
+    local _, _, _, _, role, primaryStat = GetSpecializationInfo(tree)
     if role == 'TANK' then
         C.Role = 'Tank'
     elseif role == 'HEALER' then
         C.Role = 'Healer'
     elseif role == 'DAMAGER' then
-        if stat == 4 then -- 1力量，2敏捷，4智力
+        if primaryStat == 4 then -- 1 - Strength, 2 - Agility, 4 - Intellect
             C.Role = 'Caster'
         else
             C.Role = 'Melee'
         end
     end
 end
-F:RegisterEvent('ADDON_LOADED', checkRole)
-F:RegisterEvent('PLAYER_TALENT_UPDATE', checkRole)
+F:RegisterEvent('ADDON_LOADED', checkMyRole)
+F:RegisterEvent('PLAYER_TALENT_UPDATE', checkMyRole)
 
 -- Flags
 function C:IsMyPet(flags)
