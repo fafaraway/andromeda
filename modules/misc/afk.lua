751b008a15250bed59046d5c112a6513bfa2070c
Solor <farfaraway@live.jp>
2021-04-06 04:10:45 +1000

    [Misc]Fix AFK screen saver.

--- modules/misc/afk.lua
+++ modules/misc/afk.lua
@@ -1,105 +1,97 @@
-local F, C, L = unpack(select(2, ...))
+local _G = _G
+local unpack = unpack
+local select = select
+local CreateFrame = CreateFrame
+local PlaySound = PlaySound
+local UnitIsAFK = UnitIsAFK
+local UnitAffectingCombat = UnitAffectingCombat
+local GetServerTime = GetServerTime
+local SecondsToClock = SecondsToClock
+
+local F, C = unpack(select(2, ...))
 local MISC = F.MISC
 
 local afkStart
-function MISC:UpdateTimer(...)
-	if _G.UnitIsAFK('player') then
-		if not afkStart then
-			afkStart = _G.GetServerTime()
-			MISC.frame.alphaIn:Play()
-		end
-
-		if _G.UnitAffectingCombat('player') then
-			_G.PlaySound(15262, 'MASTER')
-		end
-	else
-		afkStart = nil
-		MISC.frame.alphaOut:Play()
-	end
+local function UpdateTimer(self)
+    if UnitIsAFK('player') then
+        if not afkStart then
+            afkStart = GetServerTime()
+            MISC.AFK.alphaIn:Play()
+        end
+    else
+        afkStart = nil
+        MISC.AFK.alphaOut:Play()
+    end
 end
 
-function MISC:CreateScreenSaver()
-	local frame = _G.CreateFrame('Frame', nil, _G.UIParent, 'BackdropTemplate')
-	frame:SetPoint('TOPLEFT', 0, -300)
-	frame:SetPoint('TOPRIGHT', 0, -300)
-	frame:SetHeight(60)
+local function CreateScreenSaver(self)
+    local frame = CreateFrame('Frame', nil, _G.UIParent, 'BackdropTemplate')
+    frame:SetPoint('TOPLEFT', 0, -300)
+    frame:SetPoint('TOPRIGHT', 0, -300)
+    frame:SetHeight(80)
+    F.SetBD(frame, .65)
 
-	F.CreateBD(frame, .25)
-	MISC.frame = frame
+    frame:SetScript('OnUpdate', function()
+        if afkStart then
+            local timeStr = SecondsToClock(GetServerTime() - afkStart)
+            frame.time:SetText(timeStr)
+        end
+    end)
 
-	frame:SetScript(
-		'OnUpdate',
-		function()
-			if afkStart then
-				local timeStr = _G.SecondsToClock(_G.GetServerTime() - afkStart)
-				frame.time:SetText(timeStr)
-			end
-		end
-	)
+    local alphaIn = frame:CreateAnimationGroup()
+    alphaIn:SetScript('OnPlay', function()
+        frame:Show()
+    end)
 
-	local alphaIn = frame:CreateAnimationGroup()
-	alphaIn:SetScript(
-		'OnPlay',
-		function()
-			frame:Show()
-		end
-	)
-	alphaIn:SetScript(
-		'OnFinished',
-		function()
-			frame:SetAlpha(1)
-		end
-	)
-	local animIn = alphaIn:CreateAnimation('Alpha')
-	animIn:SetDuration(.5)
-	animIn:SetFromAlpha(0)
-	animIn:SetToAlpha(1)
-	frame.alphaIn = alphaIn
+    alphaIn:SetScript('OnFinished', function()
+        frame:SetAlpha(1)
+    end)
 
-	local alphaOut = frame:CreateAnimationGroup()
-	alphaOut:SetScript(
-		'OnPlay',
-		function()
-			frame.time:SetText('')
-		end
-	)
-	alphaOut:SetScript(
-		'OnFinished',
-		function()
-			frame:SetAlpha(0)
-			frame:Hide()
-		end
-	)
-	local animOut = alphaOut:CreateAnimation('Alpha')
-	animOut:SetDuration(.5)
-	animOut:SetFromAlpha(1)
-	animOut:SetToAlpha(0)
-	frame.alphaOut = alphaOut
+    local animIn = alphaIn:CreateAnimation('Alpha')
+    animIn:SetDuration(.5)
+    animIn:SetFromAlpha(0)
+    animIn:SetToAlpha(1)
+    frame.alphaIn = alphaIn
 
-	local bg = frame:CreateTexture(nil, 'BACKGROUND')
-	bg:SetColorTexture(0, 0, 0, .75)
-	bg:SetAllPoints(_G.UIParent)
+    local alphaOut = frame:CreateAnimationGroup()
+    alphaOut:SetScript('OnPlay', function()
+        frame.time:SetText('')
+    end)
 
-	frame.text = F.CreateFS(frame, C.Assets.Fonts.Header, 42, 'OUTLINE', 'You are now away', 'GREY', true, 'CENTER', 0, 0)
-	frame.time = F.CreateFS(frame, C.Assets.Fonts.Header, 36, 'OUTLINE', '', 'CLASS', true, 'CENTER', 0, -60)
+    alphaOut:SetScript('OnFinished', function()
+        frame:SetAlpha(0)
+        frame:Hide()
+    end)
+
+    local animOut = alphaOut:CreateAnimation('Alpha')
+    animOut:SetDuration(.5)
+    animOut:SetFromAlpha(1)
+    animOut:SetToAlpha(0)
+    frame.alphaOut = alphaOut
+
+    local bg = frame:CreateTexture(nil, 'BACKGROUND')
+    bg:SetColorTexture(0, 0, 0, .85)
+    bg:SetAllPoints(_G.UIParent)
+
+    local font = C.Assets.Fonts.Header
+    frame.text = F.CreateFS(frame, font, 42, 'OUTLINE', 'You are now away', 'GREY', true, 'CENTER', 0, 0)
+    frame.time = F.CreateFS(frame, font, 36, 'OUTLINE', '', 'CLASS', true, 'CENTER', 0, -60)
+
+    MISC.AFK = frame
 end
 
-function MISC:RefreshMod()
-	MISC:UpdateTimer('Refresh')
+local function Refresh()
+    UpdateTimer('Refresh')
 end
 
 function MISC:ScreenSaver()
-	MISC:CreateScreenSaver()
-	MISC:RefreshMod()
+    if not C.DB.Misc.ScreenSaver then
+        return
+    end
 
-	if C.DB.Misc.ScreenSaver then
-		F:RegisterEvent('PLAYER_FLAGS_CHANGED', MISC.UpdateTimer)
-		F:RegisterEvent('PLAYER_REGEN_ENABLED', MISC.UpdateTimer)
-		F:RegisterEvent('PLAYER_REGEN_DISABLED', MISC.UpdateTimer)
-	else
-		F:UnregisterEvent('PLAYER_FLAGS_CHANGED', MISC.UpdateTimer)
-		F:UnregisterEvent('PLAYER_REGEN_ENABLED', MISC.UpdateTimer)
-		F:UnregisterEvent('PLAYER_REGEN_DISABLED', MISC.UpdateTimer)
-	end
+    CreateScreenSaver()
+    Refresh()
+
+    F:RegisterEvent('PLAYER_FLAGS_CHANGED', UpdateTimer)
 end
 MISC:RegisterMisc('ScreenSaver', MISC.ScreenSaver)
