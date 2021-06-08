3bd327004d1859f4492ff148ca2fef358aedc139
Solor <farfaraway@live.jp>
2021-06-09 05:23:24 +0800

    why

--- locales/zhCN.lua
+++ locales/zhCN.lua
@@ -1,4 +1,5 @@
 local L = _G.LibStub('AceLocale-3.0'):NewLocale('FreeUI', 'zhCN')
+if not L then return end
 
 --@localization(locale="zhCN", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
 
--- locales/zhTW.lua
+++ locales/zhTW.lua
@@ -1,4 +1,4 @@
-
 local L = _G.LibStub('AceLocale-3.0'):NewLocale('FreeUI', 'zhTW')
+if not L then return end
 
 --@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
