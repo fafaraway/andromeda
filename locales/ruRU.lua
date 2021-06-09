3524086ebacce3b66340fcb7d0a65a85bee252b3
Solor <farfaraway@live.jp>
2021-06-09 21:47:22 +0800

    Cleanup

--- locales/ruRU.lua
+++ locales/ruRU.lua
@@ -1,4 +1,4 @@
 local L = _G.LibStub('AceLocale-3.0'):NewLocale('FreeUI', 'ruRU')
 if not L then return end
 
---@localization(locale="ruRU", format="lua_additive_table", handle-unlocalized="comment")@
+--@localization(locale="ruRU", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="comment")@
