930aaa37eae9d36d68cbab1cd5bd3b783aa8e7bc
Solor <farfaraway@live.jp>
2021-06-15 20:35:46 +0800

    Theme: cleanup

--- modules/theme/blizzard/FrameXML/ModelPreviewFrame.lua
+++ modules/theme/blizzard/FrameXML/ModelPreviewFrame.lua
@@ -1,22 +1,30 @@
+local _G = _G
+local unpack = unpack
+local select = select
+local tinsert = tinsert
+
 local F, C = unpack(select(2, ...))
 
-tinsert(C.BlizzThemes, function()
-	if not _G.FREE_ADB.ReskinBlizz then return end
+tinsert(
+    C.BlizzThemes,
+    function()
+        local ModelPreviewFrame = _G.ModelPreviewFrame
+        local Display = ModelPreviewFrame.Display
 
-	local ModelPreviewFrame = ModelPreviewFrame
-	local Display = ModelPreviewFrame.Display
+        Display.YesMountsTex:Hide()
+        Display.ShadowOverlay:Hide()
 
-	Display.YesMountsTex:Hide()
-	Display.ShadowOverlay:Hide()
+        F.StripTextures(ModelPreviewFrame)
+        F.SetBD(ModelPreviewFrame)
+        F.ReskinArrow(Display.ModelScene.RotateLeftButton, 'left')
+        F.ReskinArrow(Display.ModelScene.RotateRightButton, 'right')
+        F.ReskinArrow(Display.ModelScene.CarouselLeftButton, 'left')
+        F.ReskinArrow(Display.ModelScene.CarouselRightButton, 'right')
+        F.ReskinClose(_G.ModelPreviewFrameCloseButton)
+        F.Reskin(ModelPreviewFrame.CloseButton)
 
-	F.StripTextures(ModelPreviewFrame)
-	F.SetBD(ModelPreviewFrame)
-	F.ReskinArrow(Display.ModelScene.RotateLeftButton, "left")
-	F.ReskinArrow(Display.ModelScene.RotateRightButton, "right")
-	F.ReskinClose(ModelPreviewFrameCloseButton)
-	F.Reskin(ModelPreviewFrame.CloseButton)
-
-	local bg = F.CreateBDFrame(Display.ModelScene, .25)
-	bg:SetPoint("TOPLEFT", -1, 0)
-	bg:SetPoint("BOTTOMRIGHT", 2, -2)
-end)
+        local bg = F.CreateBDFrame(Display.ModelScene, .25)
+        bg:SetPoint('TOPLEFT', -1, 0)
+        bg:SetPoint('BOTTOMRIGHT', 2, -2)
+    end
+)
