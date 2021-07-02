21d3773c48f7d32c6a86ef9854428211c473fa2c
Solor <farfaraway@live.jp>
2021-07-03 06:13:50 +0800

    theme: fix DBM bar

--- modules/theme/addons/dbm.lua
+++ modules/theme/addons/dbm.lua
@@ -66,11 +66,11 @@ local function ApplyStyle(self)
     local timer = _G[frame_name .. 'BarTimer']
 
     if self.enlarged then
-        frame:SetWidth(self.owner.options.HugeWidth)
-        tbar:SetWidth(self.owner.options.HugeWidth)
+        frame:SetWidth(self.owner.Options.HugeWidth)
+        tbar:SetWidth(self.owner.Options.HugeWidth)
     else
-        frame:SetWidth(self.owner.options.Width)
-        tbar:SetWidth(self.owner.options.Width)
+        frame:SetWidth(self.owner.Options.Width)
+        tbar:SetWidth(self.owner.Options.Width)
     end
 
     frame:SetScale(1)
