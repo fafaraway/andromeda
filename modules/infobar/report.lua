46721968e5cc4037d064962afd226e04bead1b34
Solor <farfaraway@live.jp>
2021-07-17 13:40:50 +0800

    infobar: add Tormentors of Torghast

--- modules/infobar/report.lua
+++ modules/infobar/report.lua
@@ -61,12 +61,13 @@ local function CheckTexture(texture)
 end
 
 local questlist = {
-    {name = L['Blingtron Daily Pack'], id = 34774},
     {name = L['Winter Veil Daily'], id = 6983},
+    {name = L['Blingtron Daily Pack'], id = 34774},
+    {name = L['Tormentors of Torghast'], id = 63854},
     {name = L['Timewarped Badge Reward'], id = 40168, texture = 1129674}, -- TBC
     {name = L['Timewarped Badge Reward'], id = 40173, texture = 1129686}, -- WotLK
     {name = L['Timewarped Badge Reward'], id = 40786, texture = 1304688}, -- Cata
-    {name = L['Timewarped Badge Reward'], id = 45799, texture = 1530590}, -- MoP
+    {name = L['Timewarped Badge Reward'], id = 45799, texture = 1530590} -- MoP
 }
 
 -- Torghast
