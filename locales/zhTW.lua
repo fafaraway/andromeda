368060053d440305e6e577bfa5b99ed109a4bbc6
Solor <farfaraway@live.jp>
2021-08-02 05:28:33 +0800

    locales: cleanup

--- locales/zhTW.lua
+++
@@ -1,7 +0,0 @@
-local F = unpack(select(2, ...))
-local L = F.Libs.ACL:NewLocale('FreeUI', 'zhTW')
-if not L then return end
-
---@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
-
-
