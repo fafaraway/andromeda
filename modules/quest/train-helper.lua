183bf78df8ddf6976a607d249cedb2cffc5d7f83
Solor <farfaraway@live.jp>
2021-08-26 17:13:16 +0800

    quest: update quest helper

--- modules/quest/train-helper.lua
+++ modules/quest/train-helper.lua
@@ -1,6 +1,4 @@
 --[[
-    Spam SpaceBar to finish "We'll Make an Aspirant Out of You"
-    https://www.wowhead.com/quest=59585/well-make-an-aspirant-out-of-you
     Credits P3lim
     https://github.com/p3lim-wow/BetterWorldQuests/blob/master/Helpers/Training.lua
 ]]
@@ -13,11 +11,12 @@ local strmatch = strmatch
 local CreateFrame = CreateFrame
 local GetSpellInfo = GetSpellInfo
 local C_QuestLog_IsOnQuest = C_QuestLog.IsOnQuest
-local AuraUtil = AuraUtil
+local GetPlayerAuraBySpellID = GetPlayerAuraBySpellID
 local ClearOverrideBindings = ClearOverrideBindings
 local SetOverrideBindingClick = SetOverrideBindingClick
 local InCombatLockdown = InCombatLockdown
 local RaidNotice_AddMessage = RaidNotice_AddMessage
+local C_Timer_After = C_Timer.After
 
 local F, C, L = unpack(select(2, ...))
 
@@ -29,21 +28,27 @@ local spells = {
     [321842] = {
         [321843] = 1, -- Strike
         [321844] = 2, -- Sweep
-        [321847] = 3, -- Parry
+        [321847] = 3 -- Parry
     },
     [341925] = {
         [341931] = 1, -- Slash
         [341928] = 2, -- Bash
-        [341929] = 3, -- Block
+        [341929] = 3 -- Block
     },
     [341985] = {
         [342000] = 1, -- Jab
         [342001] = 2, -- Kick
-        [342002] = 3, -- Dodge
+        [342002] = 3 -- Dodge
     },
+    [355677] = {
+        [355834] = 1, -- 突刺
+        [355835] = 2, -- 招架
+        [355836] = 3 -- 还击
+    }
 }
 
 local trainerName = 'Trainer Ikaros'
+local nadjiaName = 'Nadjia the Mistblade'
 if C.GameLocale == 'deDE' then
     trainerName = 'Ausbilder Ikaros'
 elseif C.GameLocale == 'esES' or C.GameLocale == 'esMX' then
@@ -58,70 +63,92 @@ elseif C.GameLocale == 'ptBR' then
     trainerName = 'Treinador Ikaros'
 elseif C.GameLocale == 'ruRU' then
     trainerName = 'Укротитель Икар'
-elseif C.GameLocale == 'zhCN' or C.GameLocale == 'zhTW' then
+elseif C.GameLocale == 'zhCN' then
     trainerName = '训练师伊卡洛斯'
+    nadjiaName = '娜德佳，迷雾之刃'
 end
 
-local function auraFilter(a, _, _, _, _, _, _, _, _, _, _, _, b)
-    return a == b
-end
+local questIDs = {
+    [59585] = true, -- https://www.wowhead.com/quest=59585/well-make-an-aspirant-out-of-you
+    [64271] = true, -- https://www.wowhead.com/quest=64271/a-more-civilized-way
+}
+
+local questNPCs = {
+    [trainerName] = true,
+    [nadjiaName] = true
+}
 
 local Handler = CreateFrame('Frame')
 Handler:RegisterEvent('QUEST_LOG_UPDATE')
 Handler:RegisterEvent('QUEST_ACCEPTED')
-Handler:SetScript('OnEvent', function(self, event, ...)
-    if event == 'QUEST_LOG_UPDATE' then
-        if C_QuestLog_IsOnQuest(59585) then
-            self:Watch()
-        else
-            self:Unwatch()
-        end
-    elseif event == 'QUEST_ACCEPTED' then
-        local questID = ...
-        if questID == 59585 then
-            self:Watch()
-        end
-    elseif (event == 'QUEST_REMOVED') then
-        local questID = ...
-        if questID == 59585 then
-            self:Unwatch()
-        end
-    elseif event == 'UNIT_AURA' then
-        for buff, spellSet in next, spells do
-            if AuraUtil.FindAura(auraFilter, 'player', 'HELPFUL', buff) then
-                self:Control(spellSet)
-                return
-            else
-                self:Uncontrol()
-            end
-        end
-    elseif event == 'CHAT_MSG_MONSTER_SAY' then
-        local msg, sender = ...
-        if sender == trainerName then -- Trainer Ikaros
-            for spell, actionID in pairs(actionMessages) do
-                if strmatch(msg, spell) then
-                    F:Delay(0.5, function()
-                        -- wait a split second to get "Perfect"
-                        ClearOverrideBindings(self)
-                        SetOverrideBindingClick(self, true, 'SPACE', BUTTON:format(actionID))
-                    end)
+Handler:SetScript(
+    'OnEvent',
+    function(self, event, ...)
+        if event == 'QUEST_LOG_UPDATE' then
+            local found = false
+
+            for questID in pairs(questIDs) do
+                if C_QuestLog_IsOnQuest(questID) then
+                    found = true
                     break
                 end
             end
-        end
-    elseif event == 'UNIT_SPELLCAST_SUCCEEDED' then
-        local _, _, spellID = ...
-        if actionResetSpells[spellID] then
-            ClearOverrideBindings(self)
 
-            -- bind to something useless to avoid spamming jump
-            SetOverrideBindingClick(self, true, 'SPACE', BUTTON:format(12))
+            if found then
+                self:Watch()
+            else
+                self:Unwatch()
+            end
+        elseif event == 'QUEST_ACCEPTED' then
+            local questID = ...
+            if questIDs[questID] then
+                self:Watch()
+            end
+        elseif event == 'QUEST_REMOVED' then
+            local questID = ...
+            if questIDs[questID] then
+                self:Unwatch()
+            end
+        elseif event == 'UNIT_AURA' then
+            for buff, spellSet in next, spells do
+                if GetPlayerAuraBySpellID(buff) then
+                    self:Control(spellSet)
+                    return
+                else
+                    self:Uncontrol()
+                end
+            end
+        elseif event == 'CHAT_MSG_MONSTER_SAY' then
+            local msg, sender = ...
+            if questNPCs[sender] then
+                for spell, actionID in pairs(actionMessages) do
+                    if strmatch(msg, spell) then
+                        C_Timer_After(
+                            .2,
+                            function()
+                                -- wait a split second to get "Perfect"
+                                ClearOverrideBindings(self)
+                                SetOverrideBindingClick(self, true, 'SPACE', BUTTON:format(actionID))
+                            end
+                        )
+                        break
+                    end
+                end
+            end
+        elseif event == 'UNIT_SPELLCAST_SUCCEEDED' then
+            local _, _, spellID = ...
+            if actionResetSpells[spellID] then
+                ClearOverrideBindings(self)
+
+                -- bind to something useless to avoid spamming jump
+                SetOverrideBindingClick(self, true, 'SPACE', BUTTON:format(12))
+            end
+        elseif (event == 'PLAYER_REGEN_ENABLED') then
+            ClearOverrideBindings(self)
+            self:UnregisterEvent(event)
         end
-    elseif (event == 'PLAYER_REGEN_ENABLED') then
-        ClearOverrideBindings(self)
-        self:UnregisterEvent(event)
     end
-end)
+)
 
 function Handler:Watch()
     self:RegisterUnitEvent('UNIT_AURA', 'player')
@@ -141,8 +168,11 @@ function Handler:Control(spellSet)
         actionMessages[(GetSpellInfo(spellID))] = actionIndex
         actionResetSpells[spellID] = true
 
+        -- zhCN fix
         if spellID == 321844 then
             actionMessages['低扫'] = actionIndex
+        elseif spellID == 355834 then
+            actionMessages['突袭'] = actionIndex
         end
     end
 
