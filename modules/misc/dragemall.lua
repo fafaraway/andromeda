2b3a938812e65b20d627373bed072b65e69198a2
Solor <farfaraway@live.jp>
2021-10-27 10:17:49 +0800

    clear debug output

--- modules/misc/dragemall.lua
+++ modules/misc/dragemall.lua
@@ -55,7 +55,7 @@ local function IsFrameExists()
     for k in pairs(frames) do
         local name = _G[k]
         if not name and C.IsDeveloper then
-            F:Debug('Frame not found:', k)
+            F:Debug('(DragEmAll) Frame not found: ' .. k)
         end
     end
 end
