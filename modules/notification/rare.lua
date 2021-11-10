47404eb7222293bcaeb5fc07e34b9b46afff4084
Solor <farfaraway@live.jp>
2021-11-11 02:01:03 +0800

    notification: output precise coordinates of rare events

--- modules/notification/rare.lua
+++ modules/notification/rare.lua
@@ -1,34 +1,24 @@
-local _G = _G
-local unpack = unpack
-local select = select
-local format = format
-local strfind = strfind
-local wipe = wipe
-local GetInstanceInfo = GetInstanceInfo
-local C_Texture_GetAtlasInfo = C_Texture.GetAtlasInfo
-local C_VignetteInfo_GetVignetteInfo = C_VignetteInfo.GetVignetteInfo
-local GARRISON_MISSION_RARE = GARRISON_MISSION_RARE
-
 local F, C = unpack(select(2, ...))
 local NOTIFICATION = F:GetModule('Notification')
 
 local cache = {}
+local rareString = '|Hworldmap:%d+:%d+:%d+|h[%s (%.1f, %.1f)%s]|h|r'
 
 local function IsUsefulAtlas(info)
     local atlas = info.atlasName
     if atlas then
-        return strfind(atlas, '[Vv]ignette') or (atlas == 'nazjatar-nagaevent')
+        return string.find(atlas, '[Vv]ignette') or (atlas == 'nazjatar-nagaevent')
     end
 end
 
 function NOTIFICATION:RareAlert_Update(id)
     if id and not cache[id] then
-        local info = C_VignetteInfo_GetVignetteInfo(id)
+        local info = C_VignetteInfo.GetVignetteInfo(id)
         if not info or not IsUsefulAtlas(info) then
             return
         end
 
-        local atlasInfo = C_Texture_GetAtlasInfo(info.atlasName)
+        local atlasInfo = C_Texture.GetAtlasInfo(info.atlasName)
         if not atlasInfo then
             return
         end
@@ -47,16 +37,24 @@ function NOTIFICATION:RareAlert_Update(id)
 
         local atlasWidth = width / (txRight - txLeft)
         local atlasHeight = height / (txBottom - txTop)
-        local tex = format('|T%s:%d:%d:0:0:%d:%d:%d:%d:%d:%d|t', file, 0, 0, atlasWidth, atlasHeight, atlasWidth * txLeft, atlasWidth * txRight, atlasHeight * txTop, atlasHeight * txBottom)
+        local tex = string.format('|T%s:%d:%d:0:0:%d:%d:%d:%d:%d:%d|t', file, 0, 0, atlasWidth, atlasHeight, atlasWidth * txLeft, atlasWidth * txRight, atlasHeight * txTop, atlasHeight * txBottom)
 
-        F:Print(C.InfoColor .. GARRISON_MISSION_RARE .. C.BlueColor .. ' (' .. tex .. (info.name or '') .. ')')
-        F:CreateNotification(GARRISON_MISSION_RARE, tex .. (info.name or ''), nil, 'Interface\\ICONS\\INV_Misc_Map_01')
+        F:CreateNotification(_G.GARRISON_MISSION_RARE, tex .. (info.name or ''), nil, 'Interface\\ICONS\\INV_Misc_Map_01')
+
+        local nameString
+        local mapID = C_Map.GetBestMapForUnit('player')
+        local position = mapID and C_VignetteInfo.GetVignettePosition(info.vignetteGUID, mapID)
+        if position then
+            local x, y = position:GetXY()
+            nameString = string.format(rareString, mapID, x * 10000, y * 10000, info.name, x * 100, y * 100, '')
+        end
+        F:Print(tex .. C.InfoColor .. (nameString or info.name or ''))
 
         cache[id] = true
     end
 
     if #cache > 666 then
-        wipe(cache)
+        table.wipe(cache)
     end
 end
 
@@ -76,7 +74,7 @@ function NOTIFICATION:RareNotify()
         self:RareAlert_CheckInstance()
         F:RegisterEvent('PLAYER_ENTERING_WORLD', self.RareAlert_CheckInstance)
     else
-        wipe(cache)
+        table.wipe(cache)
         F:UnregisterEvent('VIGNETTE_MINIMAP_UPDATED', self.RareAlert_Update)
         F:UnregisterEvent('PLAYER_ENTERING_WORLD', self.RareAlert_CheckInstance)
     end
