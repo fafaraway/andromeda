dc2a0b3e238b77a95987317eaf573a5d0d208230
Solor <farfaraway@live.jp>
2021-11-15 16:06:45 +0800

    remove enhanced menu

--- modules/misc/misc.lua
+++ modules/misc/misc.lua
@@ -1,101 +1,91 @@
-local F, C, L = unpack(select(2, ...))
-local MISC = F:RegisterModule('General')
+local F, C = unpack(select(2, ...))
+local M = F:RegisterModule('General')
 
-local MISC_LIST = {}
 
-function MISC:RegisterMisc(name, func)
-    if not MISC_LIST[name] then
-        MISC_LIST[name] = func
-    end
-end
 
-function MISC:OnLogin()
-    for name, func in next, MISC_LIST do
-        if name and type(func) == 'function' then
-            func()
-        end
-    end
 
-    MISC:ForceWarning()
-    MISC:JerryWay()
-end
+
+
 
 -- Force warning
-local function ForceWarning_OnEvent(_, event)
-    if event == 'UPDATE_BATTLEFIELD_STATUS' then
-        for i = 1, GetMaxBattlefieldID() do
-            local status = GetBattlefieldStatus(i)
-            if status == 'confirm' then
-                PlaySound(_G.SOUNDKIT.PVP_THROUGH_QUEUE, 'Master')
-                break
+do
+    local function ForceWarning_OnEvent(_, event)
+        if event == 'UPDATE_BATTLEFIELD_STATUS' then
+            for i = 1, GetMaxBattlefieldID() do
+                local status = GetBattlefieldStatus(i)
+                if status == 'confirm' then
+                    PlaySound(_G.SOUNDKIT.PVP_THROUGH_QUEUE, 'Master')
+                    break
+                end
+                i = i + 1
             end
-            i = i + 1
+        elseif event == 'PET_BATTLE_QUEUE_PROPOSE_MATCH' then
+            PlaySound(_G.SOUNDKIT.PVP_THROUGH_QUEUE, 'Master')
+        elseif event == 'LFG_PROPOSAL_SHOW' then
+            PlaySound(_G.SOUNDKIT.READY_CHECK, 'Master')
+        elseif event == 'RESURRECT_REQUEST' then
+            PlaySound(37, 'Master')
         end
-    elseif event == 'PET_BATTLE_QUEUE_PROPOSE_MATCH' then
-        PlaySound(_G.SOUNDKIT.PVP_THROUGH_QUEUE, 'Master')
-    elseif event == 'LFG_PROPOSAL_SHOW' then
-        PlaySound(_G.SOUNDKIT.READY_CHECK, 'Master')
-    elseif event == 'RESURRECT_REQUEST' then
-        PlaySound(37, 'Master')
     end
-end
 
-local function ReadyCheckHook(_, initiator)
-    if initiator ~= 'player' then
-        PlaySound(_G.SOUNDKIT.READY_CHECK, 'Master')
+    local function ReadyCheckHook(_, initiator)
+        if initiator ~= 'player' then
+            PlaySound(_G.SOUNDKIT.READY_CHECK, 'Master')
+        end
     end
-end
 
-function MISC:ForceWarning()
-    F:RegisterEvent('UPDATE_BATTLEFIELD_STATUS', ForceWarning_OnEvent)
-    F:RegisterEvent('PET_BATTLE_QUEUE_PROPOSE_MATCH', ForceWarning_OnEvent)
-    F:RegisterEvent('LFG_PROPOSAL_SHOW', ForceWarning_OnEvent)
-    F:RegisterEvent('RESURRECT_REQUEST', ForceWarning_OnEvent)
+    function M:ForceWarning()
+        F:RegisterEvent('UPDATE_BATTLEFIELD_STATUS', ForceWarning_OnEvent)
+        F:RegisterEvent('PET_BATTLE_QUEUE_PROPOSE_MATCH', ForceWarning_OnEvent)
+        F:RegisterEvent('LFG_PROPOSAL_SHOW', ForceWarning_OnEvent)
+        F:RegisterEvent('RESURRECT_REQUEST', ForceWarning_OnEvent)
 
-    hooksecurefunc('ShowReadyCheck', ReadyCheckHook)
+        hooksecurefunc('ShowReadyCheck', ReadyCheckHook)
+    end
 end
 
 -- Support cmd /way if TomTom disabled
+do
+    local pointString = C.InfoColor .. '|Hworldmap:%d+:%d+:%d+|h[|A:Waypoint-MapPin-ChatIcon:13:13:0:0|a%s (%s, %s)%s]|h|r'
 
-local pointString = C.InfoColor .. '|Hworldmap:%d+:%d+:%d+|h[|A:Waypoint-MapPin-ChatIcon:13:13:0:0|a%s (%s, %s)%s]|h|r'
-
-local function GetCorrectCoord(x)
-    x = tonumber(x)
-    if x then
-        if x > 100 then
-            return 100
-        elseif x < 0 then
-            return 0
+    local function GetCorrectCoord(x)
+        x = tonumber(x)
+        if x then
+            if x > 100 then
+                return 100
+            elseif x < 0 then
+                return 0
+            end
+            return x
         end
-        return x
-    end
-end
-
-function MISC:JerryWay()
-    if IsAddOnLoaded('TomTom') then
-        return
     end
 
-    _G.SlashCmdList['FREEUI_JERRY_WAY'] = function(msg)
-        msg = string.gsub(msg, '(%d)[%.,] (%d)', '%1 %2')
-        local x, y, z = string.match(msg, '(%S+)%s(%S+)(.*)')
-        if x and y then
-            local mapID = C_Map.GetBestMapForUnit('player')
-            if mapID then
-                local mapInfo = C_Map.GetMapInfo(mapID)
-                local mapName = mapInfo and mapInfo.name
-                if mapName then
-                    x = GetCorrectCoord(x)
-                    y = GetCorrectCoord(y)
+    function M:JerryWay()
+        if IsAddOnLoaded('TomTom') then
+            return
+        end
 
-                    if x and y then
-                        print(string.format(pointString, mapID, x * 100, y * 100, mapName, x, y, z or ''))
+        _G.SlashCmdList['FREEUI_JERRY_WAY'] = function(msg)
+            msg = string.gsub(msg, '(%d)[%.,] (%d)', '%1 %2')
+            local x, y, z = string.match(msg, '(%S+)%s(%S+)(.*)')
+            if x and y then
+                local mapID = C_Map.GetBestMapForUnit('player')
+                if mapID then
+                    local mapInfo = C_Map.GetMapInfo(mapID)
+                    local mapName = mapInfo and mapInfo.name
+                    if mapName then
+                        x = GetCorrectCoord(x)
+                        y = GetCorrectCoord(y)
+
+                        if x and y then
+                            print(string.format(pointString, mapID, x * 100, y * 100, mapName, x, y, z or ''))
+                        end
                     end
                 end
             end
         end
+        _G.SLASH_FREEUI_JERRY_WAY1 = '/way'
     end
-    _G.SLASH_FREEUI_JERRY_WAY1 = '/way'
 end
 
 -- Auto select current event boss from LFD tool
@@ -148,3 +138,7 @@ do
     )
 end
 
+function M:OnLogin()
+    M:ForceWarning()
+    M:JerryWay()
+end
