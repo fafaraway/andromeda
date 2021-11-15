a128ce2c2a7fb80b8e946a00b3043a1af8afbb30
Solor <farfaraway@live.jp>
2021-11-15 16:05:48 +0800

    flash cursor when mouse stops moving

--- modules/misc/cursor.lua
+++ modules/misc/cursor.lua
@@ -1,22 +1,14 @@
-local _G = _G
-local select = select
-local unpack = unpack
-local Lerp = Lerp
-local sqrt = sqrt
-local GetScaledCursorPosition = GetScaledCursorPosition
-local C_Timer_NewTicker = C_Timer.NewTicker
-
 local F, C = unpack(select(2, ...))
-local MISC = F:GetModule('General')
+local CT = F:RegisterModule('CursorTrail')
 
 local pollingRate, numLines = 0.05, 15
 local lines = {}
 for i = 1, numLines do
     local line = _G.UIParent:CreateLine()
-    line:SetThickness(Lerp(5, 1, (i - 1) / numLines))
+    line:SetThickness(_G.Lerp(5, 1, (i - 1) / numLines))
     line:SetColorTexture(1, 1, 1)
 
-    local startA, endA = Lerp(1, 0, (i - 1) / numLines), Lerp(1, 0, i / numLines)
+    local startA, endA = _G.Lerp(1, 0, (i - 1) / numLines), _G.Lerp(1, 0, i / numLines)
     line:SetGradientAlpha('HORIZONTAL', 1, 1, 1, startA, 1, 1, 1, endA)
 
     lines[i] = {line = line, x = 0, y = 0}
@@ -29,11 +21,11 @@ local function GetLength(startX, startY, endX, endY)
         dx, dy = -dx, -dy
     end
 
-    return sqrt((dx * dx) + (dy * dy))
+    return math.sqrt((dx * dx) + (dy * dy))
 end
 
 local function UpdateTrail()
-    local startX, startY = GetScaledCursorPosition()
+    local startX, startY = _G.GetScaledCursorPosition()
 
     for i = 1, numLines do
         local info = lines[i]
@@ -52,12 +44,52 @@ local function UpdateTrail()
     end
 end
 
-function MISC:CursorTrail()
+local function AddTrail()
+    C_Timer.NewTicker(pollingRate, UpdateTrail)
+end
+
+local x = 0
+local y = 0
+local speed = 0
+
+local function UpdateGlow(self, elapsed)
+    local dX = x
+    local dY = y
+    x, y = GetCursorPosition()
+    dX = x - dX
+    dY = y - dY
+    local weight = 2048 ^ -elapsed
+    speed = math.min(weight * speed + (1 - weight) * math.sqrt(dX * dX + dY * dY) / elapsed, 1024)
+    local size = speed / 6 - 16
+    if (size > 0) then
+        local scale = _G.UIParent:GetEffectiveScale()
+        self.texture:SetHeight(size)
+        self.texture:SetWidth(size)
+        self.texture:SetPoint('CENTER', _G.UIParent, 'BOTTOMLEFT', (x + 0.5 * dX) / scale, (y + 0.5 * dY) / scale)
+        self.texture:Show()
+    else
+        self.texture:Hide()
+    end
+end
+
+local function AddGlow()
+    local frame = CreateFrame('Frame', nil, _G.UIParent)
+    frame:SetFrameStrata('TOOLTIP')
+
+    local texture = frame:CreateTexture()
+    texture:SetTexture([[Interface\Cooldown\star4]])
+    texture:SetBlendMode('ADD')
+    texture:SetAlpha(0.5)
+    frame.texture = texture
+
+    frame:SetScript('OnUpdate', UpdateGlow)
+end
+
+function CT:OnLogin()
     if not C.DB.General.CursorTrail then
         return
     end
 
-    C_Timer_NewTicker(pollingRate, UpdateTrail)
+    AddTrail()
+    AddGlow()
 end
-
-MISC:RegisterMisc('CursorTrail', MISC.CursorTrail)
