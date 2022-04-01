f1514730b274e71ec4c88f2c1267619bdddf2291
Solor <farfaraway@live.jp>
2022-04-01 14:39:39 +0800

    cleanup

--- modules/announcement/announcement.lua
+++ modules/announcement/announcement.lua
@@ -1,60 +1,7 @@
 local F, C = unpack(select(2, ...))
 local ANNOUNCEMENT = F:GetModule('Announcement')
 
-C.AnnounceableSpellsList = {
-    -- Paladin
-    [1044] = true, -- Blessing of Freedom
-    [204018] = true, -- Blessing of Spellwarding
-    [6940] = true, -- Blessing of Sacrifice
-    [1022] = true, -- Blessing of Protection
-    [498] = true, -- Divine Protection
-    [31850] = true, -- Ardent Defender
-    [86659] = true, -- Guardian of Ancient Kings
-    [212641] = true, -- Guardian of Ancient Kings (Glyph)
-    [642] = true, -- Divine Shield
-    [31884] = true, -- Avenging Wrath
-    [633] = true, -- Lay On Hands
-    [31821] = true, -- Aura Mastery
-    -- Warrior
-    [97462] = true, -- Rallying Cry
-    [23920] = true, -- Spell Reflection
-    -- Demon Hunter
-    [196718] = true, -- Darkness
-    -- Death Knight
-    [51052] = true, -- Anti-Magic Zone
-    [15286] = true, -- Vampiric Embrace
-    -- Priest
-    [246287] = true, -- Evangelism
-    [265202] = true, -- Holy Word: Salvation
-    [200183] = true, -- Apotheosis
-    [62618] = true, -- Power Word: Barrier
-    [64843] = true, -- Divine Hymn
-    [64901] = true, -- Symbol of Hope
-    [47536] = true, -- Rapture
-    [109964] = true, -- Spirit Shell
-    [33206] = true, -- Pain Suppression
-    [47788] = true, -- Guardian Spirit
-    -- Shaman
-    [207399] = true, -- Ancestral Protection Totem
-    [108280] = true, -- Healing Tide Totem
-    [98008] = true, -- Spirit Link Totem
-    [114052] = true, -- Ascendance
-    [16191] = true, -- Mana Tide Totem
-    [108281] = true, -- Ancestral Guidance
-    [198838] = true, -- Earthen Wall Totem
-    -- Druid
-    [740] = true, -- Tranquility
-    [33891] = true, -- Incarnation: Tree of Life
-    [197721] = true, -- Flourish
-    [205636] = true, -- Force of Nature
-    -- Monk
-    [115310] = true, -- Revival
-    [325197] = true, -- Invoke Chi-Ji, the Red Crane
-    [116849] = true, -- Life Cocoon
-    [322118] = true, -- Invoke Yu'lon, the Jade Serpent
-    -- Covenants
-    [316958] = true -- Ashen Hallow
-}
+local arrowStr = GetLocale() == 'zhCN' and '→' or '->'
 
 local function GetChannel(warning)
     if C.DB.Announcement.Channel == 1 then
@@ -117,7 +64,7 @@ function ANNOUNCEMENT:OnEvent()
     if eventType == 'SPELL_MISSED' and C.DB.Announcement.Reflect then
         local spellID, _, _, missType = select(12, CombatLogGetCurrentEventInfo())
         if missType == 'REFLECT' and destName == C.MyName then
-            SendChatMessage(string.format(_G.COMBAT_TEXT_REFLECT .. ' -> %s', GetSpellLink(spellID)), GetChannel())
+            SendChatMessage(string.format(_G.COMBAT_TEXT_REFLECT .. ' %s %s', arrowStr, GetSpellLink(spellID)), GetChannel())
         end
     end
 
@@ -130,15 +77,15 @@ function ANNOUNCEMENT:OnEvent()
             if destName == nil then
                 SendChatMessage(string.format(_G.ACTION_SPELL_CAST_SUCCESS .. ' %s', GetSpellLink(spellID)), GetChannel())
             else
-                SendChatMessage(string.format(_G.ACTION_SPELL_CAST_SUCCESS .. ' %s -> %s', GetSpellLink(spellID), destName), GetChannel())
+                SendChatMessage(string.format(_G.ACTION_SPELL_CAST_SUCCESS .. ' %s %s %s', GetSpellLink(spellID), destName), arrowStr, GetChannel())
             end
         end
     elseif eventType == 'SPELL_INTERRUPT' and C.DB.Announcement.Interrupt then
-        SendChatMessage(string.format(_G.ACTION_SPELL_INTERRUPT .. ' -> %s', GetSpellLink(extraSpellID)), GetChannel())
+        SendChatMessage(string.format(_G.ACTION_SPELL_INTERRUPT .. ' %s %s', arrowStr, GetSpellLink(extraSpellID)), GetChannel())
     elseif eventType == 'SPELL_DISPEL' and C.DB.Announcement.Dispel then
-        SendChatMessage(string.format(_G.ACTION_SPELL_DISPEL .. ' -> %s', GetSpellLink(extraSpellID)), GetChannel())
+        SendChatMessage(string.format(_G.ACTION_SPELL_DISPEL .. ' %s %s', arrowStr, GetSpellLink(extraSpellID)), GetChannel())
     elseif eventType == 'SPELL_STOLEN' and C.DB.Announcement.Stolen then
-        SendChatMessage(string.format(_G.ACTION_SPELL_STOLEN .. ' -> %s', GetSpellLink(extraSpellID)), GetChannel())
+        SendChatMessage(string.format(_G.ACTION_SPELL_STOLEN .. ' %s %s', arrowStr, GetSpellLink(extraSpellID)), GetChannel())
     end
 end
 
@@ -155,7 +102,7 @@ function ANNOUNCEMENT:CheckAnnounceableSpells()
             end
         else
             if C.IsDeveloper then
-                F:Debug('Invalid Announceable Spells ID: ' .. spellID)
+                F:DebugPrint('Invalid Announceable Spells ID: ' .. spellID)
             end
         end
     end
---
+++ modules/announcement/filter.lua
@@ -0,0 +1,56 @@
+local _, C = unpack(select(2, ...))
+
+C.AnnounceableSpellsList = {
+    -- Paladin
+    [1044] = true, -- Blessing of Freedom
+    [204018] = true, -- Blessing of Spellwarding
+    [6940] = true, -- Blessing of Sacrifice
+    [1022] = true, -- Blessing of Protection
+    [498] = true, -- Divine Protection
+    [31850] = true, -- Ardent Defender
+    [86659] = true, -- Guardian of Ancient Kings
+    [212641] = true, -- Guardian of Ancient Kings (Glyph)
+    [642] = true, -- Divine Shield
+    [31884] = true, -- Avenging Wrath
+    [633] = true, -- Lay On Hands
+    [31821] = true, -- Aura Mastery
+    -- Warrior
+    [97462] = true, -- Rallying Cry
+    [23920] = true, -- Spell Reflection
+    -- Demon Hunter
+    [196718] = true, -- Darkness
+    -- Death Knight
+    [51052] = true, -- Anti-Magic Zone
+    [15286] = true, -- Vampiric Embrace
+    -- Priest
+    [246287] = true, -- Evangelism
+    [265202] = true, -- Holy Word: Salvation
+    [200183] = true, -- Apotheosis
+    [62618] = true, -- Power Word: Barrier
+    [64843] = true, -- Divine Hymn
+    [64901] = true, -- Symbol of Hope
+    [47536] = true, -- Rapture
+    [109964] = true, -- Spirit Shell
+    [33206] = true, -- Pain Suppression
+    [47788] = true, -- Guardian Spirit
+    -- Shaman
+    [207399] = true, -- Ancestral Protection Totem
+    [108280] = true, -- Healing Tide Totem
+    [98008] = true, -- Spirit Link Totem
+    [114052] = true, -- Ascendance
+    [16191] = true, -- Mana Tide Totem
+    [108281] = true, -- Ancestral Guidance
+    [198838] = true, -- Earthen Wall Totem
+    -- Druid
+    [740] = true, -- Tranquility
+    [33891] = true, -- Incarnation: Tree of Life
+    [197721] = true, -- Flourish
+    [205636] = true, -- Force of Nature
+    -- Monk
+    [115310] = true, -- Revival
+    [325197] = true, -- Invoke Chi-Ji, the Red Crane
+    [116849] = true, -- Life Cocoon
+    [322118] = true, -- Invoke Yu'lon, the Jade Serpent
+    -- Covenants
+    [316958] = true -- Ashen Hallow
+}
