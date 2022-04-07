f67ad838aee0cfb1d466a8dc0292bc694d41cb35
Solor <farfaraway@live.jp>
2022-04-07 09:53:56 +0800

    cleanup slash commands

--- core/slash.lua
+++ core/slash.lua
@@ -1,11 +1,5 @@
 local F, C, L = unpack(select(2, ...))
 
-_G.SlashCmdList.RELOADUI = function()
-    _G.ReloadUI()
-end
-_G.SLASH_RELOADUI1 = '/rl'
-_G.SLASH_RELOADUI2 = '/reload'
-
 local cmdList = {
     '/free install - Open installation panel',
     '/free config - Open config panel',
@@ -22,7 +16,7 @@ local cmdList = {
     '/clear - Clear chat',
     '/bb - Set BattleNet broadcast',
     '/spec - Switch specialization',
-    '/bind - Launch quick keybind mode'
+    '/bind - Launch quick keybind mode',
 }
 
 local function CreateHelpFrame()
@@ -39,11 +33,10 @@ local function Commands()
     --     print(string.format('%s|r - %s|r', C.YELLOW_COLOR .. command, C.BLUE_COLOR .. desc))
     -- end
 
-    CreateHelpFrame()
 end
 
 local function Version()
-    print(string.format(C.ADDON_NAME .. C.CLASS_COLOR.. '%s', C.ADDON_VERSION))
+    print(string.format(C.ADDON_NAME .. C.CLASS_COLOR .. '%s', C.ADDON_VERSION))
 end
 
 local function Reset()
@@ -62,54 +55,48 @@ local function GUI()
     F.ToggleGUI()
 end
 
-_G.SlashCmdList.FREEUI = function(str)
-    local cmd, _ = string.split(' ', str:lower(), 2)
-    if cmd == 'reset' then
+-- _G.SlashCmdList.FREEUI = function(str)
+--     local cmd, _ = string.split(' ', str:lower(), 2)
+--     if cmd == 'reset' then
+--         Reset()
+--     elseif cmd == 'install' then
+--         Install()
+--     elseif cmd == 'unlock' or cmd == 'layout' then
+--         Unlock()
+--     elseif cmd == 'gui' or cmd == 'config' then
+--         GUI()
+--     elseif cmd == 'help' then
+--         Commands()
+--     elseif cmd == 'ver' or cmd == 'version' then
+--         Version()
+--     else
+--         Commands()
+--     end
+-- end
+-- _G.SLASH_FREEUI1 = '/freeui'
+-- _G.SLASH_FREEUI2 = '/free'
+
+F:RegisterSlash('/free', function(msg)
+    local str, _ = string.split(' ', string.lower(msg), 2)
+    if str == 'reset' then
         Reset()
-    elseif cmd == 'install' then
+    elseif str == 'install' then
         Install()
-    elseif cmd == 'unlock' or cmd == 'layout' then
+    elseif str == 'unlock' or str == 'layout' then
         Unlock()
-    elseif cmd == 'gui' or cmd == 'config' then
+    elseif str == 'gui' or str == 'config' then
         GUI()
-    elseif cmd == 'help' then
+    elseif str == 'help' then
         Commands()
-    elseif cmd == 'ver' or cmd == 'version' then
+    elseif str == 'ver' or str == 'version' then
         Version()
     else
         Commands()
     end
-end
-_G.SLASH_FREEUI1 = '/freeui'
-_G.SLASH_FREEUI2 = '/free'
+end)
 
---	Enable/Disable addons
-_G.SlashCmdList.DISABLE_ADDON = function(addon)
-    local _, _, _, _, _, reason = GetAddOnInfo(addon)
-    if reason ~= 'MISSING' then
-        DisableAddOn(addon)
-        _G.ReloadUI()
-    else
-        print('|cffffff00' .. L['Addon'] .. "'" .. addon .. "'" .. L['not found'] .. '|r')
-    end
-end
-_G.SLASH_DISABLE_ADDON1 = '/dis'
-_G.SLASH_DISABLE_ADDON2 = '/disable'
-
-_G.SlashCmdList.ENABLE_ADDON = function(addon)
-    local _, _, _, _, _, reason = GetAddOnInfo(addon)
-    if reason ~= 'MISSING' then
-        EnableAddOn(addon)
-        LoadAddOn(addon)
-        _G.ReloadUI()
-    else
-        print('|cffffff00' .. L['Addon'] .. " '" .. addon .. "' " .. L['not found'] .. '|r')
-    end
-end
-_G.SLASH_ENABLE_ADDON1 = '/en'
-_G.SLASH_ENABLE_ADDON2 = '/enable'
-
-_G.SlashCmdList.ONLY_UI = function()
+-- Disable all addons except FreeUI and debug tool
+F:RegisterSlash('/onlyfree', function()
     for i = 1, GetNumAddOns() do
         local name = GetAddOnInfo(i)
         if name ~= 'FreeUI' and name ~= '!BaudErrorFrame' and GetAddOnEnableState(C.NAME, name) == 2 then
@@ -117,19 +104,15 @@ _G.SlashCmdList.ONLY_UI = function()
         end
     end
     _G.ReloadUI()
-end
-_G.SLASH_ONLY_UI1 = '/onlyfree'
-
--- [[ group ]] --
+end)
 
 --	Disband party or raid
-_G.SlashCmdList.GROUPDISBAND = function()
-    _G.StaticPopup_Show('FREEUI_DISBAND_GROUP')
-end
-_G.SLASH_GROUPDISBAND1 = '/disband'
+F:RegisterSlash('/disband', function()
+    StaticPopup_Show('FREEUI_DISBAND_GROUP')
+end)
 
 --	Convert party raid
-_G.SlashCmdList.PARTYTORAID = function()
+F:RegisterSlash('/convert', function()
     if GetNumGroupMembers() > 0 then
         if UnitInRaid('player') and (UnitIsGroupLeader('player')) then
             C_PartyInfo.ConvertToParty()
@@ -137,104 +120,207 @@ _G.SlashCmdList.PARTYTORAID = function()
             C_PartyInfo.ConvertToRaid()
         end
     else
-        print('|cffffff00' .. _G.ERR_NOT_IN_GROUP .. '|r')
+        F:Print('|cffff2020' .. _G.ERR_NOT_IN_GROUP .. '|r')
     end
-end
-_G.SLASH_PARTYTORAID1 = '/toraid'
-_G.SLASH_PARTYTORAID2 = '/toparty'
-_G.SLASH_PARTYTORAID3 = '/convert'
+end)
 
--- Instance reset
-_G.SlashCmdList.INSTRESET = function()
+F:RegisterSlash('/rdc', function()
+    DoReadyCheck()
+end)
+
+F:RegisterSlash('/role', function()
+    InitiateRolePoll()
+end)
+
+F:RegisterSlash('/ri', function()
     ResetInstances()
-end
-_G.SLASH_INSTRESET1 = '/reset'
+end)
 
--- Instance teleport
-_G.SlashCmdList.INSTTELEPORT = function()
-    local inInstance = IsInInstance()
-    if inInstance then
+F:RegisterSlash('/tp', function()
+    if IsInInstance() then
         LFGTeleport(true)
     else
         LFGTeleport()
     end
-end
-_G.SLASH_INSTTELEPORT1 = '/tp'
+end)
 
-_G.SlashCmdList.READYCHECK = function()
-    DoReadyCheck()
-end
-_G.SLASH_READYCHECK1 = '/rdc'
-_G.SLASH_READYCHECK2 = '/ready'
-
-_G.SlashCmdList.ROLECHECK = function()
-    InitiateRolePoll()
-end
-_G.SLASH_ROLECHECK1 = '/rpc'
-_G.SLASH_ROLECHECK2 = '/role'
-
-_G.SlashCmdList.LEAVEGROUP = function()
+F:RegisterSlash('/lg', function()
     C_PartyInfo.LeaveParty()
-end
-_G.SLASH_LEAVEGROUP1 = '/lg'
-_G.SLASH_LEAVEGROUP2 = '/leave'
+end)
 
---[[ misc ]]
 -- Take screenshot
-_G.SlashCmdList.SCREENSHOT = function()
+F:RegisterSlash('/ss', function()
     Screenshot()
-end
-_G.SLASH_SCREENSHOT1 = '/ss'
+end)
+
+-- Mount special pose
+F:RegisterSlash('/ms', function()
+    if IsMounted() then
+        DoEmote('MOUNTSPECIAL')
+    else
+        F:Print('You are |cffff2020NOT|r mounted.')
+    end
+end)
+
+-- Set BattleNet broadcast
+F:RegisterSlash('/bb', function(msg)
+    BNSetCustomMessage(msg)
+end)
+
+-- Switch specialization
+F:RegisterSlash('/spec', function(msg)
+    local specID = tonumber(msg)
+    if specID then
+        local canUse, failureReason = C_SpecializationInfo.CanPlayerUseTalentSpecUI()
+        if canUse then
+            if GetSpecialization() ~= specID then
+                SetSpecialization(specID)
+            end
+        else
+            F:Print('|cffff2020' .. failureReason)
+        end
+    else
+        F:Print('Please enter the |cffff2020SPECIALIZATION NUMBER|r.')
+    end
+end)
+
+-- Whisper current target
+hooksecurefunc('ChatEdit_OnSpacePressed', function(editBox)
+    if editBox:GetText():sub(1, 3) == '/tt' and (UnitCanCooperate('player', 'target') or UnitIsUnit('player', 'target')) then
+        editBox:SetText(_G.SLASH_SMART_WHISPER1 .. ' ' .. GetUnitName('target', true):gsub(' ', '') .. ' ')
+        ChatEdit_ParseText(editBox, 0)
+    end
+end)
+
+F:RegisterSlash('/tt', function(msg)
+    if UnitCanCooperate('player', 'target') or UnitIsUnit('player', 'target') then
+        SendChatMessage(msg, 'WHISPER', nil, GetUnitName('target', true))
+    end
+end)
 
 -- Clear chat
-_G.SlashCmdList.CLEARCHAT = function()
+F:RegisterSlash('/clear', function()
     for i = 1, _G.NUM_CHAT_WINDOWS do
         _G[string.format('ChatFrame%d', i)]:Clear()
     end
-end
-_G.SLASH_CLEARCHAT1 = '/clear'
+end)
 
--- Mount special pose
-_G.SlashCmdList.MOUNTSPECIAL = function()
-    if GetUnitSpeed('player') == 0 then
-        DoEmote('MOUNTSPECIAL')
+-- Print NPC ID
+local pattern = '%w+%-.-%-.-%-.-%-.-%-(.-)%-'
+local function GetNPCID(unit)
+    if unit and UnitExists(unit) then
+        local npcGUID = UnitGUID(unit)
+        return npcGUID and (tonumber(npcGUID:match(pattern)))
     end
 end
-_G.SLASH_MOUNTSPECIAL1 = '/ms'
 
--- Set BattleNet broadcast
-_G.SlashCmdList.BNBROADCAST = function(msg)
-    BNSetCustomMessage(msg)
-end
-_G.SLASH_BNBROADCAST1 = '/bb'
+F:RegisterSlash('/npcid', function()
+    local npcID = GetNPCID('target')
+    if npcID then
+        local str = 'NPC ID: ' .. npcID
+        F:Print(str)
+    end
+end)
 
--- Switch specialization
-_G.SlashCmdList.SPEC = function(spec)
-    local canUse, failureReason = C_SpecializationInfo.CanPlayerUseTalentSpecUI()
-    if canUse then
-        if GetSpecialization() ~= tonumber(spec) then
-            SetSpecialization(spec)
-        end
+-- Print quest info
+F:RegisterSlash('/questid', function(msg)
+    local questID = tonumber(msg)
+    if questID then
+        local isCompleted = C_QuestLog.IsQuestFlaggedCompleted(questID)
+        local status = isCompleted and '|cff20ff20COMPLETE|r' or '|cffff2020NOT complete|r'
+        local str = 'Quest |cffe9c55d' .. questID .. '|r is ' .. status
+
+        F:Print(str)
+    end
+end)
+
+-- Print map info
+F:RegisterSlash('/mapid', function()
+    local mapID
+    if WorldMapFrame:IsShown() then
+        mapID = WorldMapFrame:GetMapID()
     else
-        print('|cffffff00' .. failureReason .. '|r')
+        mapID = C_Map.GetBestMapForUnit('player')
     end
-end
-_G.SLASH_SPEC1 = '/spec'
+    local str = 'Map ID: |cffe9c55d' .. mapID .. '|r - ' .. C_Map.GetMapInfo(mapID).name
 
--- Whisper target
-hooksecurefunc(
-    'ChatEdit_OnSpacePressed',
-    function(self)
-        if (string.sub(self:GetText(), 1, 3) == '/tt' and (UnitCanCooperate('player', 'target') or UnitIsUnit('player', 'target'))) then
-            self:SetText(_G.SLASH_SMART_WHISPER1 .. ' ' .. GetUnitName('target', true):gsub(' ', '') .. ' ')
-            _G.ChatEdit_ParseText(self, 0)
+    F:Print(str)
+end)
+
+-- Print instance info
+F:RegisterSlash('/instinfo', function()
+    local name, instanceType, difficultyID, difficultyName, _, _, _, instanceMapID = GetInstanceInfo()
+    F:Print(C.LINE_STRING)
+    F:Print('Name ' .. C.INFO_COLOR .. name)
+    F:Print('instanceType ' .. C.INFO_COLOR .. instanceType)
+    F:Print('difficultyID ' .. C.INFO_COLOR .. difficultyID)
+    F:Print('difficultyName ' .. C.INFO_COLOR .. difficultyName)
+    F:Print('instanceMapID ' .. C.INFO_COLOR .. instanceMapID)
+    F:Print(C.LINE_STRING)
+end)
+
+-- Print item info
+F:RegisterSlash('/iteminfo', function(msg)
+    local itemID = tonumber(msg)
+    if itemID then
+        local name, link, rarity, level, minLevel, type, subType, _, _, _, _, classID, subClassID, bindType = GetItemInfo(itemID)
+        if name then
+            F:Print(C.LINE_STRING)
+            F:Print('Name ' .. C.INFO_COLOR .. name)
+            F:Print('Link ' .. link)
+            F:Print('Rarity ' .. C.INFO_COLOR .. rarity)
+            F:Print('Level ' .. C.INFO_COLOR .. level)
+            F:Print('MinLevel ' .. C.INFO_COLOR .. minLevel)
+            F:Print('Type ' .. C.INFO_COLOR .. type)
+            F:Print('SubType ' .. C.INFO_COLOR .. subType)
+            F:Print('ClassID ' .. C.INFO_COLOR .. classID)
+            F:Print('SubClassID ' .. C.INFO_COLOR .. subClassID)
+            F:Print('BindType ' .. C.INFO_COLOR .. bindType)
+            F:Print(C.LINE_STRING)
+        else
+            F:Print('Item ' .. itemID .. ' |cffff2020NOT found|r')
         end
     end
-)
+end)
 
-_G.SLASH_WHISPERTARGET1 = '/tt'
-_G.SlashCmdList.WHISPERTARGET = function(str)
-    if (UnitCanCooperate('player', 'target')) then
-        SendChatMessage(str, 'WHISPER', GetDefaultLanguage('player'), GetUnitName('target', true):gsub(' ', ''))
+-- Print item info
+F:RegisterSlash('/scaleinfo', function()
+    F:Print(C.LINE_STRING)
+    F:Print('C.ScreenWidth ' .. C.ScreenWidth)
+    F:Print('C.ScreenHeight ' .. C.ScreenHeight)
+    F:Print('C.MULT ' .. C.MULT)
+    F:Print('UIScale ' .. _G.FREE_ADB.UIScale)
+    F:Print('UIParentScale ' .. _G.UIParent:GetScale())
+    F:Print(C.LINE_STRING)
+end)
+
+-- DBM test
+F:RegisterSlash('/dbmtest', function()
+    if IsAddOnLoaded('DBM-Core') then
+        _G.DBM:DemoMode()
+    else
+        F:Print(C.RED_COLOR .. 'DBM is not loaded.')
     end
-end
+end)
+
+-- Dev tool
+F:RegisterSlash('/rl', function()
+    ReloadUI()
+end)
+
+F:RegisterSlash('/fs', function()
+    _G.UIParentLoadAddOn('Blizzard_DebugTools')
+    _G.FrameStackTooltip_Toggle(false, true, true)
+end)
+
+--[[ do
+    local sortedKeys = {}
+    for k in pairs(tempTable) do
+        sortedKeys[#sortedKeys + 1] = k
+    end
+    table.sort(sortedKeys)
+    for i, k in ipairs(sortedKeys) do
+        local v = tempTable[k]
+        F:Print(k, v)
+    end
+end ]]
