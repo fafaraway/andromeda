5eab059d256dbbe419be1e2ef67fe1b020572958
Solor <farfaraway@live.jp>
2022-04-16 13:14:42 +0800

    just in case

--- modules/unitframe/gcd.lua
+++ modules/unitframe/gcd.lua
@@ -1,5 +1,6 @@
 local F, C = unpack(select(2, ...))
 local UNITFRAME = F:GetModule('UnitFrame')
+local oUF = F.Libs.oUF
 
 function UNITFRAME:CreateGCDTicker(self)
     local bar = CreateFrame('StatusBar', nil, self)
@@ -18,21 +19,18 @@ function UNITFRAME:CreateGCDTicker(self)
 end
 
 function UNITFRAME:UpdateGCDTicker()
-    local frame = _G.oUF_Player
-    if not frame then
-        return
-    end
-
-    if C.DB.Unitframe.GCDIndicator then
-        if not frame:IsElementEnabled('GCD') then
-            frame:EnableElement('GCD')
-            if frame.GCD then
-                frame.GCD:ForceUpdate()
+    for _, frame in pairs(oUF.objects) do
+        if C.DB.Unitframe.GCDIndicator then
+            if not frame:IsElementEnabled('GCD') then
+                frame:EnableElement('GCD')
+                if frame.GCD then
+                    frame.GCD:ForceUpdate()
+                end
             end
-        end
-    else
-        if frame:IsElementEnabled('GCD') then
-            frame:DisableElement('GCD')
+        else
+            if frame:IsElementEnabled('GCD') then
+                frame:DisableElement('GCD')
+            end
         end
     end
 end
