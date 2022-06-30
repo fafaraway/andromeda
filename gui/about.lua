e49f874b1bd0737aa64a1040d5b5f2a08b65768a
neotpravlennoye <farfaraway@live.jp>
2022-06-30 16:11:56 +0800

    feat(gui): update about panel

--- gui/about.lua
+++ gui/about.lua
@@ -1,22 +1,23 @@
 local F, C, L = unpack(select(2, ...))
 local GUI = F:GetModule('GUI')
 
-local urls = {
+GUI.urlsList = {
     curse = 'https://www.curseforge.com/wow/addons/andromeda',
-    wago = 'https://addons.wago.io/addons/freeui',
+    wago = 'https://addons.wago.io/addons/andromeda',
     wowi = 'https://www.wowinterface.com/downloads/info23258',
     github = 'https://github.com/neotpravlennoye/andromeda',
+    tracker = 'https://github.com/neotpravlennoye/andromeda/issues',
     discord = 'https://discord.gg/86wbfZXxn7',
     qq = '203621176',
 }
 
-local icons = {
-    curse = C.ASSET_PATH .. 'textures\\site\\curse',
-    wago = C.ASSET_PATH .. 'textures\\site\\wago',
-    wowi = C.ASSET_PATH .. 'textures\\site\\wowi',
-    github = C.ASSET_PATH .. 'textures\\site\\github',
-    discord = C.ASSET_PATH .. 'textures\\site\\discord',
-    qq = C.ASSET_PATH .. 'textures\\site\\qq',
+GUI.lablesList = {
+    curse = 'CurseForge',
+    wago = 'Wago',
+    wowi = 'WoWI',
+    github = 'Github',
+    discord = 'Discord',
+    qq = 'QQ'
 }
 
 local function ResetUrlBox(self)
@@ -31,22 +32,12 @@ local function UrlBox_OnEditFocusLost(self)
     self:HighlightText(0, 0)
 end
 
-local function CreateUrlBox(parent, text, url, texture, position)
-    local box = F.CreateEditBox(parent, 340, 24)
+local function CreateUrlBox(parent, text, url, position)
+    local box = F.CreateEditBox(parent, 320, 24)
+    box:SetPoint(unpack(position))
 
-    if position then
-        box:SetPoint(unpack(position))
-    else
-        box:SetPoint('TOP', 0, -70)
-    end
-
-    box.lable = F.CreateFS(parent, C.Assets.Font.Condensed, 13, nil, text, 'YELLOW', true)
-    box.lable:SetPoint('BOTTOM', box, 'TOP', 0, 4)
-
-    box.icon = box:CreateTexture()
-    box.icon:SetSize(20, 20)
-    box.icon:SetPoint('RIGHT', box, 'LEFT', -4, 0)
-    box.icon:SetTexture(texture)
+    box.lable = F.CreateFS(box, C.Assets.Font.Condensed, 12, nil, text, 'YELLOW', true)
+    box.lable:SetPoint('RIGHT', box, 'LEFT', -4, 0)
 
     box.url = url
 
@@ -62,35 +53,45 @@ function GUI:CreateAboutFrame(parent)
     local outline = _G.ANDROMEDA_ADB.FontOutline
 
     local repo = CreateFrame('Frame', nil, parent)
-    repo:SetSize(400, 120)
+    repo:SetSize(400, 90)
     repo:SetPoint('TOP', 0, -20)
 
-    F.CreateFS(repo, C.Assets.Font.Heavy, 18, outline, L['Development Repository'], { 242 / 255, 211 / 255, 104 / 255 }, outline or 'THICK', 'TOP', 0, -10)
+    F.CreateFS(repo, C.Assets.Font.Heavy, 18, outline, L['Repository'], 'YELLOW', outline or 'THICK', 'TOP', 0, -10)
 
     GUI:CreateGradientLine(repo, 120, -60, -34, 60, -34)
 
-    CreateUrlBox(repo, 'Github', urls.github, icons.github)
+    CreateUrlBox(repo, GUI.lablesList.github, GUI.urlsList.github, { 'TOP', 0, -50 })
+
+    local tracker = CreateFrame('Frame', nil, parent)
+    tracker:SetSize(400, 90)
+    tracker:SetPoint('TOP', repo, 'BOTTOM')
+
+    F.CreateFS(tracker, C.Assets.Font.Heavy, 18, outline, L['Issue Tracker'], 'YELLOW', outline or 'THICK', 'TOP', 0, -10)
+
+    GUI:CreateGradientLine(tracker, 120, -60, -34, 60, -34)
+
+    CreateUrlBox(tracker, GUI.lablesList.github, GUI.urlsList.tracker, { 'TOP', 0, -50 })
 
     local download = CreateFrame('Frame', nil, parent)
-    download:SetSize(400, 220)
-    download:SetPoint('TOP', repo, 'BOTTOM')
+    download:SetSize(400, 150)
+    download:SetPoint('TOP', tracker, 'BOTTOM')
 
-    F.CreateFS(download, C.Assets.Font.Heavy, 18, outline, L['Stable Version Download'], { 242 / 255, 211 / 255, 104 / 255 }, outline or 'THICK', 'TOP', 0, -10)
+    F.CreateFS(download, C.Assets.Font.Heavy, 18, outline, L['Download'], 'YELLOW', outline or 'THICK', 'TOP', 0, -10)
 
     GUI:CreateGradientLine(download, 120, -60, -34, 60, -34)
 
-    CreateUrlBox(download, 'CurseForge.com', urls.curse, icons.curse)
-    CreateUrlBox(download, 'Wago.io', urls.wago, icons.wago, { 'TOP', 0, -120 })
-    CreateUrlBox(download, 'WoWInterface.com', urls.wowi, icons.wowi, { 'TOP', 0, -170 })
+    CreateUrlBox(download, GUI.lablesList.curse, GUI.urlsList.curse, { 'TOP', 0, -50 })
+    CreateUrlBox(download, GUI.lablesList.wago, GUI.urlsList.wago, { 'TOP', 0, -80 })
+    CreateUrlBox(download, GUI.lablesList.wowi, GUI.urlsList.wowi, { 'TOP', 0, -110 })
 
     local feedback = CreateFrame('Frame', nil, parent)
-    feedback:SetSize(400, 220)
+    feedback:SetSize(400, 150)
     feedback:SetPoint('TOP', download, 'BOTTOM')
 
-    F.CreateFS(feedback, C.Assets.Font.Heavy, 18, outline, L['Feedback'], { 242 / 255, 211 / 255, 104 / 255 }, outline or 'THICK', 'TOP', 0, -10)
+    F.CreateFS(feedback, C.Assets.Font.Heavy, 18, outline, L['Discussion'], 'YELLOW', outline or 'THICK', 'TOP', 0, -10)
 
     GUI:CreateGradientLine(feedback, 120, -60, -34, 60, -34)
 
-    CreateUrlBox(feedback, 'Discord Channel', urls.discord, icons.discord)
-    CreateUrlBox(feedback, 'QQ Group', urls.qq, icons.qq, { 'TOP', 0, -120 })
+    CreateUrlBox(feedback, GUI.lablesList.discord, GUI.urlsList.discord, { 'TOP', 0, -50 })
+    CreateUrlBox(feedback, GUI.lablesList.qq, GUI.urlsList.qq, { 'TOP', 0, -80 })
 end
