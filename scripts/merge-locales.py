019eb318b446dcb3f7f0332e43a4644c8ad409be
neotpravlennoye <farfaraway@live.jp>
2022-07-16 04:21:11 +0800

    other: update merge locales script

--- scripts/merge-locales.py
+++ scripts/merge-locales.py
@@ -108,10 +108,10 @@ def update_locales(keys, old: {str: str}, del_no_use: bool = True) -> {str: str}
 
 #
 
-addon_path = "../"
+addon_path = "../src/"
 
 # Scan all locale strings
-files = get_files(addon_path, ignore=[".git", ".github", ".vscode", "locales", "libraries", "utils"])
+files = get_files(addon_path, ignore=["locales", "libraries"])
 keys = generate_keys(files)
 keys.sort()
 
