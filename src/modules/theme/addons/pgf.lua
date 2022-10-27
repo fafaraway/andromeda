34adba17c548a158933113183d91b8a7a433ff6f
SlapBetCommissioner <farfaraway@live.jp>
2022-10-27 21:13:25 +0800

    fix(theme): fix PremadeGroupsFilter skin

--- src/modules/theme/addons/pgf.lua
+++ src/modules/theme/addons/pgf.lua
@@ -15,6 +15,7 @@ function THEME:ReskinPGF()
         if tipStyled then
             return
         end
+
         for i = 1, _G.PremadeGroupsFilterDialog:GetNumChildren() do
             local child = select(i, _G.PremadeGroupsFilterDialog:GetChildren())
             if child and child.Shadow then
@@ -44,44 +45,54 @@ function THEME:ReskinPGF()
         F.Reskin(self.ResetButton)
         F.Reskin(self.RefreshButton)
         F.ReskinDropDown(self.Difficulty.DropDown)
-        F.StripTextures(self.Advanced)
         F.ReskinInput(self.Expression)
         F.ReskinInput(self.Sorting.SortingExpression)
+
         if self.MoveableToggle then
             F.ReskinArrow(self.MoveableToggle, 'left')
             self.MoveableToggle:SetPoint('TOPLEFT', 5, -5)
         end
-        if self.MinimizeButton then
-            F.ReskinArrow(self.MinimizeButton, 'down')
-            self.MinimizeButton:ClearAllPoints()
-            self.MinimizeButton:SetPoint('RIGHT', self.CloseButton, 'LEFT', -3, 0)
-            F.ReskinArrow(self.MaximizeButton, 'up')
-            self.MaximizeButton:ClearAllPoints()
-            self.MaximizeButton:SetPoint('RIGHT', self.CloseButton, 'LEFT', -3, 0)
+
+        local button = self.MaxMinButtonFrame
+
+        if button.MinimizeButton then
+            F.ReskinArrow(button.MinimizeButton, 'down')
+            button.MinimizeButton:ClearAllPoints()
+            button.MinimizeButton:SetPoint('RIGHT', self.CloseButton, 'LEFT', -3, 0)
+
+            F.ReskinArrow(button.MaximizeButton, 'up')
+            button.MaximizeButton:ClearAllPoints()
+            button.MaximizeButton:SetPoint('RIGHT', self.CloseButton, 'LEFT', -3, 0)
         end
 
         local names = {
             'Difficulty',
-            'Ilvl',
-            'Noilvl',
             'Defeated',
             'Members',
             'Tanks',
             'Heals',
             'Dps',
+            'MPRating',
+            'PVPRating',
         }
 
         for _, name in pairs(names) do
-            local check = self[name].Act
-            if check then
-                check:SetSize(26, 26)
-                check:SetPoint('TOPLEFT', 5, -3)
-                F.ReskinCheckbox(check)
-            end
-            local input = self[name].Min
-            if input then
-                F.ReskinInput(input)
-                F.ReskinInput(self[name].Max)
+            local frame = self[name]
+            if frame then
+                local check = frame.Act
+                if check then
+                    check:SetSize(26, 26)
+                    check:SetPoint('TOPLEFT', 5, -3)
+                    F.ReskinCheck(check)
+                end
+
+                local input = frame.Min
+                if input then
+                    F.ReskinInput(input)
+                    F.ReskinInput(frame.Max)
+                end
+            else
+                F:Debug(name)
             end
         end
 
@@ -97,6 +108,7 @@ function THEME:ReskinPGF()
     _G.UsePFGButton:ClearAllPoints()
     _G.UsePFGButton:SetPoint('RIGHT', _G.LFGListFrame.SearchPanel.RefreshButton, 'LEFT', -40, 0)
     _G.UsePFGButton:SetSize(20, 20)
+
     F.ReskinCheckbox(_G.UsePFGButton, true)
     _G.UsePFGButton.text:SetWidth(35)
     _G.UsePFGButton.text:SetFontObject(_G.Game12Font)
