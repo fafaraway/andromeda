6a747072e61c3de354a059bc3b4eb37d4b8b48a6
farfaraway <farfaraway@live.jp>
2022-11-25 19:39:07 +0800

    fix(unitframe): fix threat indicator

--- src/modules/unitframe/elements/threat.lua
+++ src/modules/unitframe/elements/threat.lua
@@ -1,27 +1,27 @@
 local F, C = unpack(select(2, ...))
 local UNITFRAME = F:GetModule('UnitFrame')
 
-local function UpdateThreat(self, _, unit)
-    if not self.shadow or self.unit ~= unit then
+local function UpdateThreatBorder(self, _, unit)
+    if not self.__sd or self.unit ~= unit then
         return
     end
 
     local status = UnitThreatSituation(unit)
     if status and status > 0 then
         local r, g, b = GetThreatStatusColor(status)
-        self.shadow:SetBackdropBorderColor(r, g, b, 0.6)
+        self.__sd:SetBackdropBorderColor(r, g, b, 0.6)
     else
-        self.shadow:SetBackdropBorderColor(0, 0, 0, 0.35)
+        self.__sd:SetBackdropBorderColor(0, 0, 0, 0.35)
     end
 end
 
-function UNITFRAME:CreateThreatIndicator(self)
+function UNITFRAME:CreateThreatBorder(self)
     if not C.DB.Unitframe.ThreatIndicator then
         return
     end
 
     self.ThreatIndicator = {
         IsObjectType = function() end,
-        Override = UpdateThreat,
+        Override = UpdateThreatBorder,
     }
 end
