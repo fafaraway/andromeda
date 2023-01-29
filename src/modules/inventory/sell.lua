7215236a3f450e5fd77da2538c3f7c03115f5ad6
fafaraway <farfaraway@live.jp>
2023-01-30 01:15:51 +0800

    improvement: ignore common transmog equipment when auto selling junk

--- src/modules/inventory/sell.lua
+++ src/modules/inventory/sell.lua
@@ -1,14 +1,15 @@
 local F, C = unpack(select(2, ...))
 local INVENTORY = F:GetModule('Inventory')
 
-local stop, cache = true, {}
-local errorText = _G.ERR_VENDOR_DOESNT_BUY
+local stop = true
+local cache = {}
 
 local function startSelling()
     if stop then
         return
     end
-    for bag = 0, 4 do
+
+    for bag = 0, 5 do
         for slot = 1, C_Container.GetContainerNumSlots(bag) do
             if stop then
                 return
@@ -18,12 +19,14 @@ local function startSelling()
             if info then
                 local quality, link, noValue, itemID = info.quality, info.hyperlink, info.hasNoValue, info.itemID
                 local isInSet = C_Container.GetContainerItemEquipmentSetInfo(bag, slot)
+                local hasTransmog = not C_TransmogCollection.GetItemInfo(link) or C_TransmogCollection.PlayerHasTransmogByItemInfo(link)
 
                 if
                     link
                     and not noValue
                     and not isInSet
                     and not INVENTORY:IsPetTrashCurrency(itemID)
+                    and hasTransmog
                     and (quality == 0 or _G.ANDROMEDA_ADB['CustomJunkList'][itemID])
                     and not cache['b' .. bag .. 's' .. slot]
                 then
@@ -52,7 +55,7 @@ local function updateSelling(event, ...)
         wipe(cache)
         startSelling()
         F:RegisterEvent('UI_ERROR_MESSAGE', updateSelling)
-    elseif event == 'UI_ERROR_MESSAGE' and arg == errorText or event == 'MERCHANT_CLOSED' then
+    elseif event == 'UI_ERROR_MESSAGE' and arg == _G.ERR_VENDOR_DOESNT_BUY or event == 'MERCHANT_CLOSED' then
         stop = true
     end
 end
